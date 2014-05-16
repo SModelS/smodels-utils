562d393a531094a17effa6b4cbcfeccd31fe43f2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-05-16 13:03:42 +0200

    turn into module

--- bin/countCitations.py
+++ bin/countCitations.py
@@ -2,21 +2,25 @@
 
 """ a trivial script that counts the citations in inspirehep """
 
-N=1269436
+def count():
+    N=1269436
 
-import urllib2
+    import urllib2
 
-U="http://inspirehep.net/record/%d/citations" % N
+    U="http://inspirehep.net/record/%d/citations" % N
 
-print U
+    # print U
 
-f=urllib2.urlopen( U )
-lines=f.readlines()
-f.close()
+    f=urllib2.urlopen( U )
+    lines=f.readlines()
+    f.close()
 
-for line in lines:
-    pos=line.find("Cited by:")
-    pos2=line.find("records")
-    if pos>-1:
-        T=int(line[pos+9:pos2])
-        print T
+    for line in lines:
+        pos=line.find("Cited by:")
+        pos2=line.find("records")
+        if pos>-1:
+            T=int(line[pos+9:pos2])
+    return T
+
+if __name__ == "__main__":
+    print count()
