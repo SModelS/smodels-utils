008e66db483cd2579b5f84c8f8b5659e53f601f4
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-06-26 15:20:46 +0200

    starting to adapt to example to new database browser

--- bin/exampleDatabaseBrowser.py
+++ bin/exampleDatabaseBrowser.py
@@ -2,7 +2,7 @@
 
 """
 .. module:: exampleDatabaseBrowser
-   :synopsis: Small script to show how the databaseBrowser module can be used to access smodels-database. 
+     :synopsis: Small script to show how the databaseBrowser module can be used to access smodels-database. 
 
 .. moduleauthor:: Veronika Magerl <v.magerl@gmx.at>
 
@@ -11,53 +11,54 @@
 
 import ROOT
 import setPath
-from tools import databaseBrowser
+from smodels_tools.tools import databaseBrowser
 
 def main():
-	# set the level of the logger (default: error ; possible: debug, info, warning, error) 
-	databaseBrowser.setLogLevel('warning')
-	
-	#set the path to the database (default - "/afs/hephy.at/user/w/walten/public/sms/") if the path does not exist or if there is no valid sms-database, databaseBrowser will be stoped
-	databaseBrowser.base = '../../smodels-database/'
+  # set the level of the logger (default: error ; possible: debug, info, warning, error) 
+  # databaseBrowser.setLogLevel('warning')
+  
+  #set the path to the database (default - "/afs/hephy.at/user/w/walten/public/sms/") if the path does not exist or if there is no valid sms-database, databaseBrowser will be stoped
+  # databaseBrowser.base = '../../smodels-database/'
+  browser = databaseBrowser.Browser ( '../../smodels-database/' )
 
-	# to get the structure of the database:
-	database = databaseBrowser.getDatabase()
-	print ''
-	print '\nStructure of database: ',database
-	print ''
-	
-	# get a specified Analysis-object 
-	Analysis = databaseBrowser.Analysis('SUS13002')
-	print 'Analysis-object: ', Analysis
-	# use this object
-	print '\nAnalysis is: ', Analysis.getName()
-	print 'PAS: ', Analysis.getPAS()
-	print 'luminosity: ', Analysis.getLumi()
-	print 'Experiment: ', Analysis.getExperiment()
-	print 'comment: ', Analysis.getComment()
-	print ''
+  # to get the structure of the database:
+  database = browser.database
+  print ''
+  print '\nStructure of database: ',database
+  print ''
+  
+  # get a specified Analysis-object 
+  analysis = browser.expAnalysis('SUS13002')
+  print 'Analysis-object: ', analysis
+  # use this object
+  print '\nAnalysis is: ', analysis.name
+  print 'PAS: ', analysis.pas
+  print 'luminosity: ', analysis.lumi
+  print 'Experiment: ', analysis.experiment
+  print 'comment: ', analysis.comment
+  print ''
 
-	# to get specified Topology-object
-	Topology = databaseBrowser.Topology('T1')
-	print 'Topology-object: ', Topology
-	# use this object
-	print 'Analyses that contain this topology: ', Topology.getAnalysisNames(run = '8TeV')
-	print ''
-	
-	# to get a specified Pair-object
-	Pair = databaseBrowser.Pair(['8TeV','SUS13002', 'T1tttt'])
-	print 'Pair-object is: ', Pair
-	# use this object
-	print '\nResult is checked: ', Pair.checkedBy()
-	print '\nGet all the exclusionlines: ', Pair.getExclusionLines()
-	print '\nSelect a specified exclusionline: ', Pair.selectExclusionLine(expected = True, sigma = 1)
-	
-	# get Analysis belonging to this Pair:
-	Analysis = Pair.getAnalysis()
-	print '\nNow we have an Analysis-object: ', Analysis
-	print '\nFor this Pair the PAS is: ', Analysis.getPAS()
-	print ''
-	
-	
+  # to get specified Topology-object
+  topology = browser.expTopology('T1')
+  #print 'Topology-object: ', topology
+  ## use this object
+  #print 'Analyses that contain this topology: ', topology.getAnalysisNames(run = '8TeV')
+  #print ''
+  
+  # to get a specified Pair-object
+  pair = browser.Pair(['8TeV','SUS13002', 'T1tttt'])
+  print 'Pair-object is: ', pair
+  # use this object
+  print '\nResult is checked: ', pair.checkedBy()
+  print '\nGet all the exclusionlines: ', pair.getExclusionLines()
+  print '\nSelect a specified exclusionline: ', pair.selectExclusionLine(expected = True, sigma = 1)
+  
+  # get Analysis belonging to this Pair:
+  analysis = pair.getAnalysis()
+  print '\nNow we have an Analysis-object: ', analysis
+  print '\nFor this Pair the PAS is: ', analysis.getPAS()
+  print ''
+  
+  
 if __name__ == '__main__':
-    main()	
+  main()  
