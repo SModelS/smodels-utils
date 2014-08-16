6501f3b90fcfc7d957326dc392e959ae1de87b0e
Veronika Magerl <v.magerl@gmx.at>
2014-08-16 15:38:20 +0200

    Changed ordering

--- validation/xsecComparator.py
+++ validation/xsecComparator.py
@@ -26,6 +26,114 @@ import sys
 
 logger = logging.getLogger(__name__)
 
+def motherTendency(masses, xsections):
+    """Produces a root TGraph with mother particle mass on x-axis 
+    and xsec on y-axis.
+    
+    """
+    graph = ROOT.TGraph()
+    if not len(masses) == len(xsections):
+        logger.error('Something is very wrong! Check the grid data!')
+        return None
+    m = float(masses[0])
+    graph.SetPoint(0, m, float(xsections[0]))    
+    for i in range(len(masses)):
+        if m == float(masses[i]): continue
+        if float(xsections[i]) > 4000.: continue
+        m = float(masses[i])
+        n = graph.GetN()
+        #print('Fill in: ', n, m, float(xsections[i]))
+        graph.SetPoint(n, m, float(xsections[i]))
+    graph.SetName('mother')
+    graph.SetTitle('mother-mass vs xsection')
+    graph.SetLineWidth(4)
+    #print ('mother graph', graph)
+    return graph
+
+def readGrid(fileName, targetPath):
+    """Reads the given grid data file and creates python lists.
+    
+    """
+    
+    f = checkFile(targetPath + '/' + fileName)
+    motherM = []
+    lspM = []
+    xsections = []
+    outFile = open(f, 'r')
+    for line in outFile.readlines():
+        line = line.split()
+        if line[0] == '#END': break
+        if not eval(line[2].strip()):
+            continue
+        motherM.append(line[0].strip())
+        lspM.append(line[1].strip())
+        xsections.append(line[2].strip())
+    return [motherM, lspM, xsections]
+            
+
+def lspTendency(motherMasses, lspMasses, xsections, block):
+    """Produces a root TGraph with LSP mass on x-axis and xsec 
+    on y-axis, for several masses of the mother particle.
+    
+    """
+    graph = ROOT.TGraph()
+    if not len(motherMasses) == len(xsections):
+        logger.error('Something is very wrong!')
+        return None
+    m = float(motherMasses[0])
+    mm = None
+    count = 0
+    for i in range(len(motherMasses)):
+        if m != float(motherMasses[i]):
+            count += 1
+            m = float(motherMasses[i])
+        if count == block:
+            logger.debug('block %s and count %s' %(block, count))
+            n = graph.GetN()
+            logger.debug('Fill in: mother: %s, index: %s, lsp: %s and xsec: %s'\
+            %(m,  n, float(lspMasses[i]), float(xsections[i])))
+            graph.SetPoint(n, float(lspMasses[i]), float(xsections[i]))
+            mm = m
+        else: continue
+    graph.SetName('LSP')
+    graph.SetLineWidth(4)
+    return [graph, mm]
+
+def referenceTendency(sqrt, topology):
+    """Produces a root TGraph with mother particle mass on x-axis and xsec 
+    on y-axis using the reference cross sections.
+    
+    """
+    values = referenceXSections.xSecs(sqrt, topology)
+    if not values: return None
+    graph = ROOT.TGraph()
+    for point in values:
+        graph.SetPoint(graph.GetN(), point[0], point[1]*1000.)
+    graph.SetName('reference')
+    graph.SetLineWidth(4)
+    #print ('reference graph', graph)
+    return graph    
+
+def getTarget(path):
+    """Checks if the target directory already exists and raises an error if not.
+    
+    """
+    
+    if not os.path.exists(path):
+        logger.error('Could not find directory %s!' %path)
+        sys.exit()
+    return path
+
+def checkFile(path):
+    """Checks if the data file already exists, raises an error if not. 
+    
+    """
+    if not os.path.exists(path):
+        logger.error('Could not find file %s!' %path)
+        sys.exit()
+    return path
+ 
+
 def main():
     """Handles all command line options, as:
     topology, analysis, directory, particle, ...
@@ -153,114 +261,7 @@ def main():
 
     canvas.Print("./plots/xsecComparator/%s" %name)
     
-def motherTendency(masses, xsections):
-    """Produces a root TGraph with mother particle mass on x-axis 
-    and xsec on y-axis.
-    
-    """
-    graph = ROOT.TGraph()
-    if not len(masses) == len(xsections):
-        logger.error('Something is very wrong! Check the grid data!')
-        return None
-    m = float(masses[0])
-    graph.SetPoint(0, m, float(xsections[0]))    
-    for i in range(len(masses)):
-        if m == float(masses[i]): continue
-        if float(xsections[i]) > 4000.: continue
-        m = float(masses[i])
-        n = graph.GetN()
-        #print('Fill in: ', n, m, float(xsections[i]))
-        graph.SetPoint(n, m, float(xsections[i]))
-    graph.SetName('mother')
-    graph.SetTitle('mother-mass vs xsection')
-    graph.SetLineWidth(4)
-    #print ('mother graph', graph)
-    return graph
 
-def readGrid(fileName, targetPath):
-    """Reads the given grid data file and creates python lists.
-    
-    """
-    
-    f = checkFile(targetPath + '/' + fileName)
-    motherM = []
-    lspM = []
-    xsections = []
-    outFile = open(f, 'r')
-    for line in outFile.readlines():
-        line = line.split()
-        if line[0] == '#END': break
-        if not eval(line[2].strip()):
-            continue
-        motherM.append(line[0].strip())
-        lspM.append(line[1].strip())
-        xsections.append(line[2].strip())
-    return [motherM, lspM, xsections]
-            
-
-def lspTendency(motherMasses, lspMasses, xsections, block):
-    """Produces a root TGraph with LSP mass on x-axis and xsec 
-    on y-axis, for several masses of the mother particle.
-    
-    """
-    graph = ROOT.TGraph()
-    if not len(motherMasses) == len(xsections):
-        logger.error('Something is very wrong!')
-        return None
-    m = float(motherMasses[0])
-    mm = None
-    count = 0
-    for i in range(len(motherMasses)):
-        if m != float(motherMasses[i]):
-            count += 1
-            m = float(motherMasses[i])
-        if count == block:
-            logger.debug('block %s and count %s' %(block, count))
-            n = graph.GetN()
-            logger.debug('Fill in: mother: %s, index: %s, lsp: %s and xsec: %s'\
-            %(m,  n, float(lspMasses[i]), float(xsections[i])))
-            graph.SetPoint(n, float(lspMasses[i]), float(xsections[i]))
-            mm = m
-        else: continue
-    graph.SetName('LSP')
-    graph.SetLineWidth(4)
-    return [graph, mm]
-
-def referenceTendency(sqrt, topology):
-    """Produces a root TGraph with mother particle mass on x-axis and xsec 
-    on y-axis using the reference cross sections.
-    
-    """
-    values = referenceXSections.xSecs(sqrt, topology)
-    if not values: return None
-    graph = ROOT.TGraph()
-    for point in values:
-        graph.SetPoint(graph.GetN(), point[0], point[1]*1000.)
-    graph.SetName('reference')
-    graph.SetLineWidth(4)
-    #print ('reference graph', graph)
-    return graph    
-
-def getTarget(path):
-    """Checks if the target directory already exists and raises an error if not.
-    
-    """
-    
-    if not os.path.exists(path):
-        logger.error('Could not find directory %s!' %path)
-        sys.exit()
-    return path
-
-def checkFile(path):
-    """Checks if the data file already exists, raises an error if not. 
-    
-    """
-    if not os.path.exists(path):
-        logger.error('Could not find file %s!' %path)
-        sys.exit()
-    return path
-
-    
     
 if __name__ == '__main__':
     main()  
