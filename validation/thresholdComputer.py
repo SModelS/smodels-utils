206618a92a669f52e697bcd03cca3d97a3eb0786
traub <michael.traub@gmx.at>
2014-08-21 12:21:51 +0200

    Set min mother mass to 200GeV

--- validation/thresholdComputer.py
+++ validation/thresholdComputer.py
@@ -138,6 +138,8 @@ class Threshold(object):
         if particle == 'mother' and self.topo.name == 'T1tttt':
             if minM < 400: minM = 400
         if particle == 'mother' and self.topo.name == 'T2':
+            if minM < 200: minM = 200#
+        if particle == 'mother' and self.topo.name == 'T1':
             if minM < 200: minM = 200
         return minM
         
@@ -188,7 +190,7 @@ class Threshold(object):
         return massList
 
 def main():
-    threshold = Threshold('T2',Browser('../../smodels-database'))
+    threshold = Threshold('T1',Browser('../../smodels-database'))
     print('motherMasse: %s' %threshold.motherMasses)
     print('lspMasse: %s' %threshold.lspMasses)
     print('minLSB: %s maxLSB %s' %(min(threshold.lspMasses), max(threshold.lspMasses)))
