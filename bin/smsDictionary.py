f4e7f34a094e7a71a337c8813e0b66ac0627bdf0
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-08-28 11:00:12 +0200

    added numbers, a fix small fixes

--- bin/smsDictionary.py
+++ bin/smsDictionary.py
@@ -21,10 +21,11 @@ f=open("SmsDictionary","w")
 
 shortnames={ "directslep": "weakinos", "hadronic": "colored",
              "thirdgen": "third", "eweakino": "weakinos",
-             "hadronic, thirdgen": "colored" }
+             "hadronic, thirdgen": "colored", "None": "none" }
 longnames={ "sleptons": "sleptons", "colored": "colored production",
-            "third": "third generation", "weakinos": "weakinos and sleptons", }
-categoryorder=( "colored", "third", "weakinos" )
+            "third": "third generation", "weakinos": "weakinos and sleptons",
+            "none": "not categorized" }
+categoryorder=( "colored", "third", "weakinos", "none" )
 
 def header():
     f.write ( 
@@ -43,7 +44,7 @@ There is also a ListOfAnalyses.
 The dictionary is split up by production:
 
 [[#colored|colored production]], [[#third|third generation]], 
-[[#weakinos|weakinos and sleptons]]
+[[#weakinos|weakinos and sleptons]], [[#none|uncategorized]]
 
 """ % browser.databaseVersion )
 
@@ -67,10 +68,12 @@ def categoryHeader ( shortname, longname ):
     f.write ( "== %s ==\n" % longname )
     f.write ( "<<Anchor(%s)>>\n" % shortname )
     # f.write ( '||<tableclass="sortable"> Tx Name || Topology || Graph || Results ||\n' )
-    for header in [ "Tx", "Topology", "Graph", "Results" ]:
+    for header in [ "#", "Tx", "Topology", "Graph", "Results" ]:
         f.write ( "||<#EEEEEE:> '''%s''' " % header )
     f.write ( "||\n" )
     ## f.write ( '||<tableclass="sortable"> Tx Name || Topology || Graph || Results ||\n' )
+
+topoCounter=[0]
 
 def writeTopo ( topo ):
     if len(topo.analyses)==0: return
@@ -91,9 +94,13 @@ def writeTopo ( topo ):
     rname=name
     if name in [ "T7ChiSlep", "T8ChiSlep" ]:
         rname="!"+name
+    if name == "T2_OneSq":
+        rname="T2_!OneSq"
     constr="Not yet assigned"
     for c in topo.constraints:
-        if c.find("Not yet assigned")>-1:
+        ##if name=="T1tttt":
+            ## print "c=",c,type(c),len(c)
+        if c.find("Not yet assigned")>-1 or c=="":
             continue
         constr=str ( c ).replace('"',"")
         break
@@ -125,7 +132,8 @@ def writeTopo ( topo ):
     for i in constro[1]:
         i2+="%d," % len(i)
     i2=i2[:-1]+",0)"
-    f.write ( "||<:>'''%s'''<<Anchor(%s)>>" % ( rname, name ) )
+    topoCounter[0]+=1
+    f.write ( "||%d||<:>'''%s'''<<Anchor(%s)>>" % ( topoCounter[0], rname, name ) )
     f.write ( "||vertices: (%d)(%d) <<BR>>  " % ( v1,v2) )
     f.write ( "insertions: %s%s <<BR>> " % ( i1,i2 ) )
     # f.write ( "(jet)(jet) <<BR>>" )
@@ -156,6 +164,19 @@ def writeTopo ( topo ):
             f.write ( "[[%s|%s]]<<BR>>" % ( url, ana.replace("_"," ") ) )
     f.write ( "||\n" )
 
+def topoCmp ( x, y ):
+    """ order topology names canonically, first by length
+        of name, then by alphabet """
+    if x.name == y.name:
+        return 0
+    if len(x.name) < len(y.name):
+        return -1
+    if len(x.name) > len(y.name):
+        return 1
+    if x.name < y.name:
+        return -1
+    return 1
+
 def main():
     header()
     browser.verbosity='error'
@@ -164,34 +185,26 @@ def main():
     categories={}
     for toponame in topos:
         topo = browser.expTopology ( toponame )
-        if topo.category==None or topo.category=="":
-            continue
-        cat=shortnames [ topo.category ]
+        scat=topo.category
+        if scat in [ None, "" ]:
+            scat="None"
+        cat=shortnames [ scat ]
         if not cat in categories: categories[cat]=[]
         categories[cat].append ( topo )
+    #    if toponame=="T1tttt":
+    #        print "category: ",toponame,scat,type(scat),cat
 
-    def topoCmp ( x, y ):
-        """ order topology names canonically, first by length
-            of name, then by alphabet """
-        if x.name == y.name:
-            return 0
-        if len(x.name) < len(y.name):
-            return -1
-        if len(x.name) > len(y.name):
-            return 1
-        if x.name < y.name:
-            return -1
-        return 1
+    #print categories.keys()
 
     for cat in categoryorder:
         topos = categories[cat]
         topos.sort ( topoCmp )
         categoryHeader ( cat, longnames[cat] )
-        #print cat,
+    #    print cat,">>",
         for topo in topos:
-        #    print topo.name," ",
+    #        print topo.name," ",
             writeTopo ( topo )
-        #print
+    #    print
     footer()
     xsel()
 
