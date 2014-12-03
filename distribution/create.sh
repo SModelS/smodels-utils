81db8c06f6403b6694a515545a94cbe81df708dc
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2014-12-03 14:49:45 +0100

    first version of tarball creation script

--- distribution/create.sh
+++ distribution/create.sh
@@ -1,11 +1,37 @@
-#!/bin/zsh
+#!/bin/sh
 
-create()
+# a script that creates from scratch the tarball that we ship
+# Wolfgang Waltenberger, december 2014
+
+VERSION="1.0"
+
+clearTarball()
 {
-    git clone -b public git@smodels.hephy.at:smodels smodels-v1.0
-    cd smodels-v1.0
+  ## remove old tarball, just to be sure
+	rm -rf smodels-v${VERSION}
+	rm -rf smodels-v${VERSION}.tar.gz
 }
-## rm -r **/*.git
 
+createTarball()
+{
+	## create the tarball
+	git clone -b public git@smodels.hephy.at:smodels smodels-v${VERSION}
+	rm -rf smodels-v${VERSION}/.git 
+	tar xvf database.tar
+	mv testCopy smodels-v${VERSION}/smodels-database
+	tar czvf smodels-v${VERSION}.tar.gz smodels-v${VERSION}
+}
 
-create()
+testTarball()
+{
+  ## explode the tarball and perform a simple test
+	cp smodels-v${VERSION}.tar.gz /tmp/
+	cd /tmp/
+	tar xzvf smodels-v${VERSION}.tar.gz
+	cd /tmp/smodels-v${VERSION}
+	/tmp/smodels-v${VERSION}/runSModelS.py -f inputFiles/slha/simplyGluino.slha
+}
+
+clearTarball
+createTarball
+testTarball
