db541b7b4811fb363e679bbbca056c5aceda9e5f
traub <michael.traub@gmx.at>
2015-02-24 17:49:00 +0100

    Added docstrings to preparationHelper.py

--- smodels_utils/dataPreparation/preparationHelper.py
+++ smodels_utils/dataPreparation/preparationHelper.py
@@ -18,7 +18,23 @@ logger.setLevel(level=logging.ERROR)
 
 class Locker(object):
     
+    """Super-class to 'lock' a class.
+    Every child-class of Locker needs 2 class-attributes:
+    infoAttr: list of strings
+    interAttr: list of strings
+    Only attributes with names defined in one of those lists can
+    be added to the child
+    """
+    
+    
     def __setattr__(self, name, attr):
+        
+        """
+        set a attripute if defiened in self.allowedAttr
+        :param name: name of the attribute
+        :param attr: value of the attribute
+        :raise attrError: If name is not in self.allowedAttr
+        """
         
         if name in self.allowedAttr:
             object.__setattr__(self, name, attr)
@@ -27,17 +43,45 @@ class Locker(object):
         
     @property
     def allowedAttr(self):
+        
+        """
+        :return: list containing all entries of
+        infoAttr and internalAttr
+        """
+        
         return self.infoAttr + self.internalAttr
         
         
 class ObjectList(list):
     
+    """
+    list-object with redefined __getitem__ methode
+    An element of ObjectList can be addressed with 
+    an user defined attribute (like the name of the element)
+    instead with an index. The element need to have this 
+    attribute
+    """
+    
     def __init__(self, searchAttr, l = []):
+        
+        """
+        set the attribute for addressing the elements
+        
+        :param searchAttr: name of attribute used to address an element
+        :param l: list of elements to be stored in the object
+        """
     
         self.searchAttr = searchAttr
         list.__init__(self,l)
     
     def __getitem__(self, request):
+        
+        """
+        :raise AttributeError: if no element with value of searchAttr == request 
+        in ObjectList
+        :return: first element with value of  serchAttr equals requested value
+        """
+        
         for obj in self:
             if getattr(obj, self.searchAttr) == request:
                 return obj
