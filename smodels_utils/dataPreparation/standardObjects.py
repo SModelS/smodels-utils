03d6752af983c4a3a037d495075207de7a1ebe59
traub <michael.traub@gmx.at>
2015-02-25 12:36:38 +0100

    Added docstrings to standardObjects.py

--- smodels_utils/dataPreparation/standardObjects.py
+++ smodels_utils/dataPreparation/standardObjects.py
@@ -21,14 +21,44 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.ERROR)
 
 class VertexChecker(object):
+    
+    """
+    This class is designed to compute the off-shell
+    vertices for a given mass array
+    The on-shell constraints of txName are used to define 
+    the SM-particles occurring in every vertex.
+    The differences of the masses of the SUSY-particles related
+    to one vertex are compared with the mass sum of the SM-particles
+    in this vertex
+    """
 
     def __init__(self, txNameObj):
+        
+        """
+        :param txNameObj: inputObjects.TxNameInput-object
+        """
         
         self.txName = txNameObj.name
         self.kinConstraints = self._getKinConstraints(txNameObj)
         
         
     def getOffShellVertices(self, massArray):
+        
+        """
+        compute the mass-difference between two adjacent
+        masses in the mass array. Compare those mass differences
+        with the masses held by self.kinConstraint. If the mass 
+        difference is smaller then the assigned kinConstraint-mass
+        the vertex is added to the list of off-shell vertices
+        
+        :param massArray: list containing two other lists. Each list contains 
+        floats, representing the masses of the particles of each branch in GeV
+        :raise decayChainError: if length of mass array not equal length of kinConstraint
+        :return: list of off-shell vertices, each vertex is represented by a tuple 
+        that contains two integers. The first one of those integers give the branch
+        of the vertex and the second one the position of the vertex in the branch
+        The count starts with 0. eg: (0,1) --> second vertex in first branch
+        """
 
         offShellVertices = []
         massDeltaArray = [[],[]]
@@ -51,6 +81,25 @@ class VertexChecker(object):
         return offShellVertices
         
     def _getKinConstraints(self, txNameObj):
+        
+        """
+        takes the on-shell constraint of a given txName,
+        replace the SM-particles occurring in one Vertex 
+        by the sum of their masses. 
+        The masses of 'Z', 'W', 't' and 'h' are set 
+        to: paricleMass - 2*decaywith
+        The masses of all other SM-particles are set to 0
+        
+        :param txName: inputObjects.TxNameInput-object
+        :raise missingOnConstraintError: if TxNameInput-object
+        have no-shell constraint
+        :raise constraintError: if on-shell constraint can not
+        be interpreted as a list in the required format
+        :return: list of lists, each list holds the mass-sum
+        of the SM-particles for every vertex.
+        or None if on-shell constraint set to:'not yet assigned'
+        
+        """
         
         massDict = {'Z': 86., 'W': 76.,'t': 169.,'h': 118}
         startString = '[[['
@@ -95,33 +144,66 @@ class VertexChecker(object):
         
 class StandardDataList(list):
     
+    """
+    list-object, 
+    holds lists, each list consists of 2 entrys: [massArry, value]
+    The MassArray holds the masses of the SUSY-particles for each branch
+    The masses are represented by a string incl. unit of mass
+    eg: [['5.0*GeV, '10.0*GeV'],[['5.0*GeV, '10.0*GeV']]
+    The value is a string assigned to this  massArray. 
+    example for upper limits: '0.578*pb'
+    """
+    
     def __init__(self, massUnit = '*GeV', valueUnit = '*pb'):
+        
+        """
+        :param massUnit: unit of the mass array
+        :param valueUnit; unit of the value
+        """
         
         self.massUnit = massUnit
         self.valueUnit = valueUnit
         
     
-    def append(self, massArray, limit):
+    def append(self, massArray, value):
+        
+        """
+        append StandardDataList with [massArray, value]
+        if there is no entry with the same massArray
+        allready in StandardDataList
+        
+        :raise valueDifferenceWarning: If there are two entries 
+        with same massArray but different values
+        """
+       
         
         self._checkMasses(massArray)
         array = [[],[]]
         for i in range(len(massArray)):
             array[i] = ['%s%s' %(mass, self.massUnit) for mass in massArray[i]]
-        inLimit = False
+        inList = False
         for point in self:
             if array ==  point[0]:
-                oldlimit = float(point[1][:-3])
-                if abs(limit-oldlimit) > 0.0001:
-                    Errors().limitDifference\
-                    (array, point[1], '%s%s' %(limit,self.valueUnit))
-                inLimit = True
+                oldvalue = float(point[1][:-3])
+                if abs(value-oldvalue) > 0.0001:
+                    Errors().valueDifference\
+                    (array, point[1], '%s%s' %(value,self.valueUnit))
+                inList = True
                 break
-        limit = '%s%s' %(limit,self.valueUnit)
-        if not inLimit:
-            list.append(self, [array, limit])
+        value = '%s%s' %(value,self.valueUnit)
+        if not inList:
+            list.append(self, [array, value])
             
     def _checkMasses(self, massArray):
-    
+        
+        """
+        checks the given mass array
+        
+        :raise negativMassError: If one of the masses is negative
+        :raise massOrderError: if the mass of one particle is bigger
+        then the mass of the previous one
+        """
+        
         for array in massArray:
             for i, mass in enumerate(array):
                 if mass < 0.0: Errors().negativMass(massArray)
@@ -132,6 +214,12 @@ class StandardDataList(list):
     
             
     def __str__(self):
+        
+        """
+        :returns: entries of StandardDataList line by line
+        formatted as string readable by eval as a list of
+        unum-objects
+        """
         
         string = '['
         
@@ -146,12 +234,31 @@ class StandardDataList(list):
         
 class StandardExclusions(list):
     
+    """
+    list-objects
+    holds all exclusion lines for one txName
+    as standardized ROOT.TGraph-objects
+    """
+    
     def __init__(self, name):
+        
+        """
+        initialize a empty StandardExclusions-object
+        :param name: name of related txName
+        """
         
         self.name = name
         list.__init__(self)
         
     def addMassPlane(self, massPlaneObj):
+        
+        """
+        takes all original exclusion line data from given
+        mass plane. Build the standardized ROOT.TGraph and
+        saves those TGraphs in self
+        
+        :param massPlaneObj: inputObjects.MassPlane-object
+        """
         
         for exclusion in massPlaneObj.origExclusions:
             if not exclusion: continue
@@ -171,7 +278,23 @@ class StandardExclusions(list):
             
 class StandardTWiki(object):
     
+    """
+    Holds all information for the creation of
+    twiki.txt 
+    The string representation of this class can 
+    be used to create a new entry for:
+    http://smodels.hephy.at/wiki/DataSource
+    """
+    
     def __init__(self, metaInfo):
+        
+        """
+        Initialize the object with empty lists
+        only the entry for the first column in the twiki 
+        table is build
+        
+        :param metaInfo: inputObjects.MetaInfoInput-object
+        """
         
         self.id = self.idFormat(metaInfo)      
         self.txNames = []
@@ -179,29 +302,59 @@ class StandardTWiki(object):
         self.figures = []
         self.limits = []
         self.exclusions = []
+        self.efficiencies = []
         
     def __str__(self):
         
-        
+        """
+        :return: string holding all informations in a format 
+        readable by MoinMoin Wiki. The string can be used 
+        as a table entry for:
+        http://smodels.hephy.at/wiki/DataSource
+        """
+
         string = '||%s||' %(self.id)
         for attr in \
-        [self.txNames, self.axes, self.figures, self.limits, self.exclusions]:
+        [self.txNames, self.axes, self.figures, self.limits,\
+        self.efficiencies, self.exclusions]:
             string = string + '<<BR>>'.join(attr) + '||'
         return string + '\n'
     
     def addMassPlane(self, txName, plane):
+        
+        """
+        takes all informations needed for the twiki entry
+        from given txName and mass plane.
+        Builds standardized entries for the columns of
+        the twiki table 
+        
+        :param txName: name of the related TxName-object
+        :param plane: inputObjects.MassPlane-object
+        """
             
         self.txNames.append(self.link('smsDictionary#%s' %txName, txName))
         self.axes.append(str(plane.origPlot))
         self.figures.append(self.link(plane.figureUrl, plane.figure))
         self.limits.append(self.objectFormat(plane.origLimits))
         self.exclusions.append(self.objectFormat(plane.origExclusions))
+        self.efficiencies.append(self.objectFormat([plane.efficiencyMap]))
 
     def link(self, url, label):
+        
+        """
+        :param url: url of the link as string
+        :param label: label of the link as string
+        :return: standardized link for the twiki as string
+        """
         
         return '[[%s|%s]]' %(url, label)    
       
     def idFormat(self, metaInfo):
+        
+        """
+        :param metaInfo: inputObjects.MetaInfoInput-object
+        :return: standardized string for the first column of the tWiki table
+        """
         
         ID = self.link(metaInfo.url, metaInfo.id)
         if hasattr(metaInfo, 'superseded_by'):
@@ -210,10 +363,22 @@ class StandardTWiki(object):
     
     def objectFormat(self, objectList):
         
+        """
+        Builds standardized entries for the the following
+        columns of the twiki entry:
+        -upper limits on production cross sections
+        -efficiency maps
+        -exclusion lines
+        
+        :param objectList: list of origDataObjects
+        :return: standardized entries for the twiki table
+        """
+        
         string = ''
         for obj in objectList:
             if not obj.dataUrl: continue
             label = 'OBS'
+            if 'efficiency' in obj.name: label = 'EFF'
             if 'expected' in obj.name: label = 'EXP'
             if 'M1' in obj.name: label = '-'
             if 'P1' in obj.name: label = '+'
@@ -267,11 +432,11 @@ class Errors(object):
         sys.exit()
         
         
-    def limitDifference(self, massArray, oldLimit, limit):
+    def valueDifference(self, massArray, oldValue, value):
         
         m = '------------------------\n'
-        m = m + "WARNING: massPoint: %s allrady in limits \n" %massArray
-        m = m + "but differ in Upperlimit: %s v.s. %s" %(oldLimit, limit)
+        m = m + "WARNING: massPoint: %s allrady in \n" %massArray
+        m = m + "but differ in value: %s v.s. %s" %(oldValue, value)
         m = m + '\n------------------------'
         print(m)
              
