1798cd326dd8b1ec91c24910d9a3546bdb848775
walten <wolfgang.waltenberger@gmail.com>
2015-03-04 14:10:04 -0300

    compute WrongnessFactor added

--- andre_validation/validationObjs.py
+++ andre_validation/validationObjs.py
@@ -26,16 +26,16 @@ class ValidationPlot():
     """
     Encapsulates all the data necessary for creating a single validation plot.
     """
-    
+
     def __init__(self, ExptRes, TxNameStr, Axes, slhadir=None, databasePath=None):
-    
+
         self.expRes = ExptRes
         self.txname = TxNameStr
         self.axes = Axes
         self.slhaDir = None
         self.data = None
         self.officialCurve = self.getOfficialCurve()
-        
+
         if slhadir: self.setSLHAdir(slhadir)
         if databasePath:
             if os.path.isdir(databasePath):
@@ -52,36 +52,71 @@ class ValidationPlot():
             if not os.path.isdir(self.database):
                 logger.error("Could not define database folder")
                 sys.exit()
-        
+
     def __str__(self):
-        
+
         vstr = "Validation plot for\n"
         vstr += 'id: '+self.expRes.getValuesFor('id')+'\n'
         vstr += 'TxName: '+self.txname+'\n'
         vstr += 'Axes: '+self.axes
         return vstr
-    
+
+    def computeWrongnessFactor ( self, looseness=1.2 ):
+        """ computes how 'wrong' the plot is, by cutting the points
+            that are inside/outside the official exclusion curve, and comparing
+            against the points' r value ( upper limit / predict theory cross section )
+            :param looseness: how much do we loosen the criterion?
+        """
+        curve=self.getOfficialCurve()
+        n_points=0
+        pts= { "total": 0, "excluded_inside": 0, "excluded_outside": 0, "not_excluded_inside": 0,
+               "not_excluded_outside": 0, "wrong" : 0 }
+        for point in self.data:
+            x,y=point["axes"][0],point["axes"][1]
+            if y==0: y=1.5 ## to avoid points sitting on the line
+            excluded = point["UL"] < point["signal"]
+            really_excluded = looseness * point["UL"] < point["signal"]
+            really_not_excluded = point["UL"] > looseness * point["signal"]
+            inside = curve.IsInside ( x,y )
+            pts["total"]+=1
+            s=""
+            if excluded:
+                s="excluded"
+            else:
+                s="not_excluded"
+            if inside:
+                s+="_inside"
+            else:
+                s+="_outside"
+            pts[s]+=1
+            if really_excluded and not inside:
+                pts["wrong"]+=1
+            if really_not_excluded and inside:
+                pts["wrong"]+=1
+        logger.debug ( "points in categories %s" % str(pts) )
+        return float(pts["wrong"]) / float(pts["total"])
+
     def setSLHAdir(self,slhadir):
         """
         Defines the folder or tar file containing all the slha files to be
         used to generate the validation plot
-        
+
         :param slhadir: existing folder containing SLHA files
         """
-        
+
         if not os.path.isdir(slhadir) and not os.path.isfile(slhadir):
             logger.error("Folder containing SLHA files not found for "+str(self))
             sys.exit()
         else:
             self.slhaDir = slhadir
-            
+
     def getOfficialCurve(self):
         """
         Reads the root file associated to the ExpRes and
         obtain the experimental exclusion curve for the corresponding TxName and Axes.
         Saves the exclusion curve in self.officialCurve
-        
-        :return: a root TGraph object    
+
+        :return: a root TGraph object
         """
         tgraphDict = getExclusionCurvesFor(self.expRes,txname=self.txname,axes=self.axes)
         if not tgraphDict: return None
@@ -90,54 +125,54 @@ class ValidationPlot():
             logger.warning("More than one exclusion curve found. Using the first one.")
 
         return tgraph[0]
-        
+
     def getData(self):
         """
         Runs SModelS on the SLHA files listed in slhaDir and
         returns the list of excluded and allowed points.
         """
-        
+
         self.data = runSModelSFor(self)
-        
+
     def getPlot(self,silentMode=True):
         """
         Uses the data in self.data and the official exclusion curve
         in self.officialCurve to generate the exclusion plot
         :param silentMode: If True the plot will not be shown on the screen
         """
-        
+
         self.plot,self.base = createPlot(self,silentMode)
-        
+
     def savePlot(self,validationDir=None):
         """
         Saves the plot in .png format in the validationDir folder.
         If the folder does not exist, it will be created.
         If the folder is not defined the plot will be created in the
         analysis/validation/ folder
-        
+
         :param validationDir: Folder where the plot will be saved
         """
-        
-        
+
+
         if not hasattr(self,'plot') or not self.plot:
             logger.warning("No plot found. Nothing will be saved")
             return False
-        
+
         if not validationDir:
             vDir = os.path.join(self.expRes.path,'validation')
         else: vDir = validationDir
-        
+
         if not os.path.isdir(vDir):
             logger.info("Creating validation folder "+vDir)
             os.mkdir(vDir)
-        
+
         filename = self.plot.GetTitle()+'.png'
         filename = filename.replace(self.expRes.getValuesFor('id')+"_","")
         filename = os.path.join(vDir,filename)
         filename = filename.replace("*","").replace(",","").replace("(","").replace(")","")
         self.plot.Print(filename)
         return True
-    
+
     def saveData(self,validationDir=None,datafile=None):
         """
         Saves the data and plot in a text file in the validationDir folder.
@@ -145,21 +180,21 @@ class ValidationPlot():
         If the folder is not defined the plot will be created in the
         analysis/validation/ folder
         If datafile is not define, uses the default naming (Txname_axes.py)
-        
+
         :param validationDir: Folder where the root file will be saved
         :param datafile: Name of the data file
-        """    
-        
+        """
+
         if not hasattr(self,'plot') or not self.plot:
             logger.warning("No plot found. Nothing will be saved")
             return False
         if not hasattr(self,'data') or not self.data:
             logger.warning("No data found. Nothing will be saved")
-            return False        
-        
+            return False
+
         if not validationDir:
             validationDir = os.path.join(self.expRes.path,'validation')
-        
+
         if not os.path.isdir(validationDir):
             logger.info("Creating validation folder "+validationDir)
             os.mkdir(validationDir)
@@ -170,7 +205,7 @@ class ValidationPlot():
             datafile = os.path.join(validationDir,datafile)
             datafile = datafile.replace("*","").replace(",","").replace("(","").replace(")","")
 
-        
+
         #Save data to file
         f = open(datafile,'w')
         dataStr = str(self.data)
@@ -178,11 +213,11 @@ class ValidationPlot():
         dataStr = dataStr.replace('[GeV]','*GeV').replace('[TeV]','*TeV')
         f.write("validationData = "+dataStr)
         f.close()
-        
-        return True        
-            
-        
-        
-        
-        
-    
+
+        return True
+
+
+
+
+
+
