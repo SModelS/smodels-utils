2efa6b2934406b9457d48d9affdb2d430dd0d00b
Andre Lessa <lessa.a.p@gmail.com>
2015-03-13 16:26:34 -0300

    suppressed info msgs

--- validation/validateAllTxNames.py
+++ validation/validateAllTxNames.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
-import sys,os
-
+import sys,os,logging
+home = os.path.expanduser("~")
+sys.path.append(os.path.join(home,'smodels'))
+from smodels.theory.crossSection import logger as cl
+from smodels.theory.slhaDecomposer import logger as dl
+from smodels.experiment.txnameObject import logger as tl
+from gridSModelS import logger as gl
+cl.setLevel(level=logging.ERROR) 
+dl.setLevel(level=logging.ERROR)
+tl.setLevel(level=logging.ERROR)
+gl.setLevel(level=logging.ERROR)
 
 
 from plotProducer import validateTxName
