e47cedff3f928b67cc22780895e1b32591202762
walten <wolfgang.waltenberger@gmail.com>
2015-03-16 17:57:54 -0300

    turned validateTx into an argparsing script

--- validation/scripts/validateTx.py
+++ validation/scripts/validateTx.py
@@ -11,7 +11,7 @@ import logging
 from smodels.theory.crossSection import logger as cl
 from smodels.theory.slhaDecomposer import logger as dl
 from smodels.experiment.txnameObject import logger as tl
-cl.setLevel(level=logging.DEBUG) 
+cl.setLevel(level=logging.DEBUG)
 dl.setLevel(level=logging.DEBUG)
 tl.setLevel(level=logging.DEBUG)
 
@@ -20,15 +20,16 @@ database = DataBase("../../../../")
 #How to validate all plots for all Txnames in one ExpRes:
 expRes = database.getExpResults(analysisIDs=[getExpIdFromPath()],datasetIDs=[None])
 
-## axes="2*Eq(mother,x)_Eq(inter0,106.0)_Eq(lsp,y)"
-axes="2*Eq(mother,x)_Eq(inter0,2.0*y)_Eq(lsp,y)"
+## axes="2*Eq(mother,x)_Eq(lsp,y)"
 slhamain = '../../../../../smodels-utils/slha/'
 ## txname="T6bbWW"
 
-if len (sys.argv)<2:
-    print "please supply txname"
-    sys.exit()
-    
-txname=sys.argv[1]
-print validatePlot(expRes,txname,axes,slhamain+"%s.tar" % txname )
+import argparse, types
+argparser = argparse.ArgumentParser(description="validates one pair of txname / axes")
+argparser.add_argument ( '-T', '--txname', nargs='?', help='txname',
+                type=types.StringType, default='T1' )
+argparser.add_argument ( '-a', '--axes', nargs='?', help='axes description',
+                type=types.StringType, default='2*Eq(mother,x)_Eq(lsp,y)' )
+args=argparser.parse_args() 
+print validatePlot(expRes,args.txname,args.axes,slhamain+"%s.tar" % args.txname )
 
