ebd070584701a382be1afc43d5bac01ce794bb93
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2015-05-19 14:40:54 -0300

    k-factor set to 1.2 after looking at benjamins tables

--- validation/scripts/validate.py
+++ validation/scripts/validate.py
@@ -14,7 +14,7 @@ from smodels.theory.slhaDecomposer import logger as dl
 from smodels.experiment.txnameObject import logger as tl
 cl.setLevel(level=logging.DEBUG) 
 dl.setLevel(level=logging.DEBUG)
-tl.setLevel(level=logging.DEBUG)
+tl.setLevel(level=logging.INFO)
 
 
 print "exp id=",getExpIdFromPath(),"datasetid=",getDatasetIdsFromPath()
@@ -23,8 +23,9 @@ database = Database(os.path.join(home,"smodels-database"))
 #How to validate all plots for all Txnames in one ExpRes:
 expRes = database.getExpResults(analysisIDs=[getExpIdFromPath()],datasetIDs=getDatasetIdsFromPath() )
 slhamain = os.path.join(home,"smodels-utils/slha")
-# kfactorDict = { "TChiWZ": 1.25, "TChiWW": 1.25 }
-kfactorDict= {} 
+kfactorDict = { "TChiWZ": 1.2, "TChiWW": 1.2, "TChiChipmSlepL": 1.2, "TChiChipmSlepStau": 1.2, "TChiChipmStauStau": 1.2, \
+                "TChiSlepSnu": 1.2, "TChiStauSnu": 1.2, "TChiWH": 1.2, "TChiWZoff": 1.2 }
+## kfactorDict= {} 
 for i in expRes:
     validateExpRes(i,slhamain, kfactorDict = kfactorDict )
 
