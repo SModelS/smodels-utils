bd6fb5fd5866c1dcdfebf4f8365099578902ca68
Andre Lessa <lessa.a.p@gmail.com>
2016-06-10 18:10:48 -0300

    fixed listDatabase

--- bin/listDatabase.py
+++ bin/listDatabase.py
@@ -1,116 +1,148 @@
 #!/usr/bin/python
 
-""" This is a simple tool that I (WW) need to work out with the CMS susy group
+""" This is a simple tool that I (WW)need to work out with the CMS susy group
 what digitized results are not yet published """
 
-import setPath
-from smodels.experiment import smsResults, smsHelpers
+import sys
+sys.path.append("../../smodels/")
+from operator import attrgetter
+from smodels.experiment.databaseObj import Database
 from smodels.tools.physicsUnits import TeV
   
-Fields= [ "analysis", "sqrts", "lumi", "topologies", "constraints" ]
-NiceName= { "nick name": "nick name", "analysis": "analysis", "sqrts":"$\sqrt{s}$", "topologies": "topologies", "constraints":"constraints", "lumi": "lumi" }
+Fields= [ "expResult", "sqrts", "lumi","type", "topologies", "constraints" ]
+nextLineFields = ["", "", "", "", "topologies", "constraints"]
+NiceName= { "nick name": "nick name", "expResult": "Experimental Result", "sqrts":"$\sqrt{s}$", "topologies": "topologies", "constraints":"constraints", "lumi": "lumi", "type" : "data type"}
+
+Fields= [ "expResult", "sqrts", "lumi","type"]
+nextLineFields = []
+removeFastlim = True
 
 
-def isATLAS ( analysis ):
-  if analysis.find("ATLAS")==0: return True
-  return False
 
-def begintable ( File ):
-  File.write ( "\\begin{table}\n" )
-  flds="{"
-  for i in range(len(Fields)):
-    flds+="l|"
-  flds=flds[:-1]
-  flds+="}"
-  File.write ( "\\begin{tabular}%s\n" % flds )
-  for (ct,F) in enumerate(Fields):
-    File.write ( "{\\bf %s} " % NiceName[F] )
-    if ct<len(Fields)-1: File.write ( "&" )
+def isATLAS(expResult):
+    if expResult.find("ATLAS")==0: return True
+    return False
 
-  File.write ( "\\\\ \\hline \n" )
+def begintable(File):
+    flds="{"
+    for i in range(len(Fields)):
+        flds+="c|"
+    flds=flds[:-1]
+    flds+="}"
+    File.write("\\begin{longtable}%s\n" % flds)
+    for (ct,F)in enumerate(Fields):
+        File.write("{\\bf %s} " % NiceName[F])
+        if ct<len(Fields)-1: File.write("&")
+
+    File.write("\\\\ \\hline \n")
 
 
-def header ( File ):
-  File.write ( "\\documentclass[8pt]{article}\n" )
-  File.write ( "\\usepackage{multirow}\n" )
-  File.write ( "\\begin{document}\n" )
-  File.write ( "\\thispagestyle{empty}\n" )
-  begintable ( File )
+def header(File):
+    File.write( "\\documentclass[8pt]{article}\n")
+    File.write( "\\usepackage{multirow}\n")
+    File.write( "\\usepackage{longtable}\n")
+    File.write( "\\begin{document}\n")
+    File.write( "\\thispagestyle{empty}\n")
+    begintable( File)
 
-def endtable ( File ):
-  File.write ( "\\end{tabular}\n" )
-  File.write ( "\\end{table}\n" )
+def endtable( File):
+    File.write( "\\end{longtable}\n")
 
-def footer ( File ):
-  endtable ( File )
-  File.write ( "\\end{document}" )
+def footer( File):
+    endtable( File)
+    File.write( "\\end{document}")
 
-def line ( File ):
-  # going from ATLAS to CMS
-  endtable ( File )
-  File.write ( "\\newpage\n \\thispagestyle{empty}\n" )
-  begintable ( File )
+def line(File):
+    # going from ATLAS to CMS
+    endtable(File)
+    File.write("\\newpage\n \\thispagestyle{empty}\n")
+    begintable(File)
 
-def compileTex ( Filename="database.tex" ):
-  import os
-  os.system ( "pdflatex database.tex" )
-  os.system ( "convert database.pdf database.png" )
+def compileTex(Filename="database.tex"):
+    import os
+    os.system("pdflatex database.tex")
+    os.system("convert database.pdf database.png")
 
-def pprint ( constr ):
-  constr=constr.replace("'","")
-  if len(constr)>40:
-    constr=constr[:37]+"..."
-  return constr
+def pprint(constr):
+    constr=constr.replace("'","")
+    if len(constr)>40:
+        constr=constr[:37]+"..."
+    return constr
 
-def printFirstTopo ( File, analysis, topo ):
-  sqrts=int( smsResults.getSqrts ( analysis ) / TeV )
-  lumi=smsResults.getLumi ( analysis ).asNumber()
-  constr=pprint ( smsResults.getConstraints ( analysis, topo ) )
-  pas=smsResults.getPAS ( analysis )
-  File.write ( "%s & %s & %s & %s & %s \\\\ \n" % ( pas, sqrts, lumi, topo, constr ) ) 
+def printFirstTopo(File, expResult, txname):
+    sqrts = int(expResult.globalInfo.sqrts.asNumber(TeV))
+    lumi = expResult.globalInfo.lumi.asNumber()
+    constr = pprint(txname.constraint)
+    expId = expResult.globalInfo.id
+    dataType = expResult.datasets[0].dataInfo.dataType
+    infofields = {"expResult" : expId, "sqrts" : sqrts, "lumi" : lumi,"type" : dataType, 
+			"topologies" : txname.txName, "constraints" : constr}
+    writeStr = ""
+    for i,f in enumerate(Fields):
+        if f:
+            writeStr += str(infofields[f])
+        if i < len(Fields) - 1:
+            writeStr += " &"
+        else:
+            writeStr += " \\\\ \n"
+    File.write(writeStr)
 
-def printNextTopo ( File, analysis, topo ):
-  constr=pprint ( smsResults.getConstraints ( analysis, topo ) )
-  File.write ( " & & & %s & %s \\\\ \n" % ( topo, constr ) ) 
+def printNextTopo(File, expResult, txname):
+    sqrts = int(expResult.globalInfo.sqrts.asNumber(TeV))
+    lumi = expResult.globalInfo.lumi.asNumber()
+    constr = pprint(txname.constraint)
+    expId = expResult.globalInfo.id
+    dataType = expResult.datasets[0].dataInfo.dataType
+    infofields = {"expResult" : expId, "sqrts" : sqrts, "lumi" : lumi,"type" : dataType, 
+			"topologies" : txname.txName, "constraints" : constr}
+    writeStr = ""
+    for i,f in enumerate(nextLineFields):
+        if f:
+            writeStr += str(infofields[f])
+        if i < len(Fields) - 1:
+            writeStr += " &"
+        else:
+            writeStr += " \\\\ \n"
+    File.write(writeStr)
 
-def printAnalysis ( File, analysis, topos ):
-  first=True
-  for topo in topos:
-    constr=smsResults.getConstraints ( analysis, topo )
-    if not constr or constr=="Not yet assigned": continue
-    if first:
-      printFirstTopo ( File, analysis, topo )
-      first=False
-    else:
-      printNextTopo ( File, analysis, topo )
+def printAnalysis(File,expResult,txnames):
+    first=True
+    printedTxnames = []
+    for tx in sorted(txnames, key=lambda tx: tx.txName):
+        if tx.txName in printedTxnames: continue
+        constr = tx.constraint
+        if not constr or constr=="Not yet assigned": continue
+        if first:
+            printFirstTopo(File,expResult,tx)
+            first=False
+            printedTxnames.append(tx.txName)
+        else:
+            printNextTopo(File, expResult,tx)
+            printedTxnames.append(tx.txName)
 
-def run( ):
-  results={}
-  All=smsResults.getAllResults()
-  for analysis in All:
-    for topo in All[analysis]:
-      # if not smsResults.isPublic ( analysis ): continue
-      constr=smsResults.getConstraints ( analysis, topo )
-      if not constr: continue
-      sqrts=smsResults.getSqrts ( analysis )
-      run=smsResults.getRun ( analysis )
-      if not results.has_key ( analysis ): results[analysis]=[]
-      results[analysis].append ( topo )
+def run():
+    database = Database('../../smodels-database/')
+    allResults = database.getExpResults(useSuperseded=True, useNonValidated=True)
+    for expResult in allResults:
+        expResult.dataType = expResult.datasets[0].dataInfo.dataType
+    allResults = sorted(allResults, key = attrgetter('globalInfo.id','globalInfo.sqrts','dataType'))
+    wasATLAS=True
+    File=open("database.tex","w")
+    header(File)
+    for expResult in allResults:
+        expId = expResult.globalInfo.id
+        txnames = expResult.getTxNames()
+        if hasattr(expResult.globalInfo,'contact'):
+            if 'fastlim' in expResult.globalInfo.contact  and removeFastlim:
+                print 'skipping fastlim result:',expId
+                continue
 
-  keys=results.keys()
-  keys.sort()
-  print keys
-  wasATLAS=True
-  File=open("database.tex","w")
-  header ( File )
-  for analysis in keys:
-    topos=results[analysis]
-    if wasATLAS and not isATLAS ( analysis ):
-      line( File )
-      wasATLAS=False
-    printAnalysis ( File, analysis, topos )
-  footer ( File )
-  File.close()
-  compileTex()
+        if wasATLAS and not isATLAS(expId):
+            line(File)
+            wasATLAS=False
+        printAnalysis(File,expResult,txnames)
+    footer(File)
+    File.close()
+    compileTex()
 
 run()
