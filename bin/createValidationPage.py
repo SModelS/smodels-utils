7a8801abb340507b8feda02330314acf4555e2d2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-01 22:31:57 +0200

    validation page

--- bin/createValidationPage.py
+++ bin/createValidationPage.py
@@ -5,36 +5,42 @@
 
 from __future__ import print_function
 import glob
+import time
+import os
+import commands as C
     
-db = "/home/walten/git/smodels-database/"
-outfile = "newFormat"
-
-if False:
-    db = "/home/walten/git/branches/smodels-database/"
-    outfile = "develop"
-
-def copy():
-    import commands
+def copy( db, outfile ):
     www = "/var/www/walten/validation" 
-    commands.getoutput ( "cp %s.html %s" % (outfile, www ) )
-    return
+    a=C.getoutput ( "cp %s.html %s" % (outfile, www ) )
+    print ( a )
+    # return 
     for i in [ "8TeV", "13TeV" ]:
-        commands.getoutput ( "cp -r %s/%s %s/%s" % (db, i, www, outfile ) )
+        C.getoutput ( "cp -r %s/%s %s/%s" % (db, i, www, outfile ) )
 
-def write():
+def write( db, outfile ):
     html=open( outfile + ".html" ,"w")
     html.write ( "<html><body>\n" )
-    html.write ( "<h1>Validation -- %s</h1>\n" % outfile )
+    html.write ( "<h1>Validation -- %s, %s</h1>\n" % (outfile, time.asctime() ) )
     html.write ( "<table>\n" )
-    files = glob.glob ( "%s/*/*/*/validation/*.png" % db )
+    files = glob.glob ( "%s/*/*/*/validation/*_pretty.pdf" % db )
     files.sort()
     for f in files:
-        print f
-        url = f.replace ( db, outfile+"/" )
+        pngname = f.replace( ".pdf",".png" )
+        if not os.path.exists ( pngname ):
+            cmd = "convert %s %s" % (f, pngname ) 
+            C.getoutput ( cmd )
+        print (f)
+        url = pngname.replace ( db, outfile+"/" )
         html.write ( "  <tr><td><img src=%s></img>\n" % url )
     html.write ( "</body></html>\n" )
     html.close()
-    copy()
+    copy( db, outfile )
 
 if __name__ == "__main__":
-    write()
+    db = "/home/walten/git/smodels-database/"
+    outfile = "newFormat"
+    write( db, outfile )
+
+    db = "/home/walten/git/branches/smodels-database/"
+    outfile = "develop"
+    write( db, outfile )
