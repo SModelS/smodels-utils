dc8928c67973b38fafd7ba4543594df138f6cae6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-01 20:19:04 +0200

    python3

--- validation/plottingFuncs.py
+++ validation/plottingFuncs.py
@@ -460,13 +460,13 @@ def createPrettyPlot(validationPlot,silentMode=True, looseness = 1.2 ):
     if tgr.GetYmax() == tgr.GetYmin():
         logger.info("1d data detected, smearing Y values")
         buff = tgr.GetX()
-        buff.SetSize(sys.maxint)
+        buff.SetSize(sys.maxsize)
         xpts = numpy.frombuffer(buff,count=tgr.GetN())
         buff = tgr.GetY()
-        buff.SetSize(sys.maxint)
+        buff.SetSize(sys.maxsize)
         ypts = numpy.frombuffer(buff,count=tgr.GetN())
         buff = tgr.GetZ()
-        buff.SetSize(sys.maxint)
+        buff.SetSize(sys.maxsize)
         zpts = numpy.frombuffer(buff,count=tgr.GetN())
         for i in range(tgr.GetN()):
             tgr.SetPoint(i,xpts[i],ypts[i]+random.uniform(0.,0.001),zpts[i])
@@ -474,13 +474,13 @@ def createPrettyPlot(validationPlot,silentMode=True, looseness = 1.2 ):
     if tgr.GetXmax() == tgr.GetXmin():
         logger.info("1d data detected, smearing X values")
         buff = tgr.GetX()
-        buff.SetSize(sys.maxint)
+        buff.SetSize(sys.maxsize)
         xpts = numpy.frombuffer(buff,count=tgr.GetN())
         buff = tgr.GetY()
-        buff.SetSize(sys.maxint)
+        buff.SetSize(sys.maxsize)
         ypts = numpy.frombuffer(buff,count=tgr.GetN())
         buff = tgr.GetZ()
-        buff.SetSize(sys.maxint)
+        buff.SetSize(sys.maxsize)
         zpts = numpy.frombuffer(buff,count=tgr.GetN())
         for i in range(tgr.GetN()):
             tgr.SetPoint(i,xpts[i]+random.uniform(0.,0.001),ypts[i],zpts[i])
@@ -579,7 +579,7 @@ def createPrettyPlot(validationPlot,silentMode=True, looseness = 1.2 ):
     
     
     #Count the number of entries in legend:
-    nleg = min(2,len(cgraphs)-cgraphs.values().count([])) + min(2,len(official))
+    nleg = min(2,len(cgraphs)-list(cgraphs.values()).count([])) + min(2,len(official))
     #Draw legend: 
     leg = TLegend(0.15,0.83-0.045*nleg,0.495,0.83)
     setOptions(leg)    
