12d53ee92d6ac7a32e119fbb07fc65a93fe54da6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-01 16:18:19 +0200

    small_update

--- bin/createValidationPage.py
+++ bin/createValidationPage.py
@@ -3,18 +3,33 @@
 """ very simple script that collects all validation plots in a simple 
     (local) validation html page """
 
+from __future__ import print_function
 import glob
+import os
+try:
+    import commands as C
+except:
+    import subprocess as C
 
 def write():
-    db = "/home/walten/git/smodels-database/"
-    html=open("index.html","w")
+    home="/home/walten"
+    db = "%s/git/smodels-database/" % home
+    outfile = "newFormat.html"
+    if False:
+        db = "%s/git/branches/smodels-database/" % home
+        outfile = "develop.html"
+    html=open( outfile ,"w")
     html.write ( "<html><body>\n" )
-    html.write ( "<h1>Validation</h1>\n" )
+    html.write ( "<h1>Validation: %s</h1>\n" % db )
     html.write ( "<table>\n" )
-    files = glob.glob ( "%s/*/*/*/validation/*.png" % db )
+    files = glob.glob ( "%s/*/*/*/validation/*_pretty.pdf" % db )
     for f in files:
-        print f
-        html.write ( "  <tr><td><img src=%s></img>\n" % f )
+        pngversion = f.replace(".pdf",".png" )
+        if not os.path.exists ( pngversion ):
+            cmd = "convert %s %s" % ( f, pngversion )
+            C.getoutput ( cmd )
+        print ( f )
+        html.write ( "  <tr><td><img src=%s></img>\n" % pngversion )
     html.write ( "</body></html>\n" )
     html.close()
 
