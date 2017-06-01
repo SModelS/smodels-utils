6e2effea30afa270e409d5691f884859eba8ccdb
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-01 17:46:28 +0200

    move to web server

--- bin/createValidationPage.py
+++ bin/createValidationPage.py
@@ -4,19 +4,36 @@
     (local) validation html page """
 
 import glob
+    
+db = "/home/walten/git/smodels-database/"
+outfile = "newFormat"
+
+if False:
+    db = "/home/walten/git/branches/smodels-database/"
+    outfile = "develop"
+
+def copy():
+    import commands
+    www = "/var/www/walten/validation" 
+    commands.getoutput ( "cp %s.html %s" % (outfile, www ) )
+    return
+    for i in [ "8TeV", "13TeV" ]:
+        commands.getoutput ( "cp -r %s/%s %s/%s" % (db, i, www, outfile ) )
 
 def write():
-    db = "/home/walten/git/smodels-database/"
-    html=open("index.html","w")
+    html=open( outfile + ".html" ,"w")
     html.write ( "<html><body>\n" )
-    html.write ( "<h1>Validation</h1>\n" )
+    html.write ( "<h1>Validation -- %s</h1>\n" % outfile )
     html.write ( "<table>\n" )
     files = glob.glob ( "%s/*/*/*/validation/*.png" % db )
+    files.sort()
     for f in files:
         print f
-        html.write ( "  <tr><td><img src=%s></img>\n" % f )
+        url = f.replace ( db, outfile+"/" )
+        html.write ( "  <tr><td><img src=%s></img>\n" % url )
     html.write ( "</body></html>\n" )
     html.close()
+    copy()
 
 if __name__ == "__main__":
     write()
