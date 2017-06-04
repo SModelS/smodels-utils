71060bbbf52e46bd13d034e3e01204b2e767b682
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-04 19:02:03 +0200

    efficiency maps correct

--- validation/createWikiPage.py
+++ validation/createWikiPage.py
@@ -29,13 +29,28 @@ def writeHeader ( wFile ):
 This page lists validation plots for all analyses and topologies available in the SMS results database of v1.1, including superseded and Fastlim results. The list has been created from the database version 1.1.1 and Fastlim tarball shipped separately.
 
 The validation procedure for upper limit maps used here is explained in [[http://arxiv.org/abs/arXiv:1312.4175|arXiv:1312.4175]][[http://link.springer.com/article/10.1140/epjc/s10052-014-2868-5|EPJC May 2014, 74:2868]], section 4. For validating efficiency maps, a very similar procedure is followed. For every input point, the best signal region is chosen. Experimental upper limit and theoretical prediction is compared for that signal region in order to draw conclusion.
- 
-Individual tables: [[#ATLASupperlimits13|ATLAS upper limits 13TeV]], [[#ATLASefficiencymaps13|ATLAS efficiency maps 13TeV]], [[#CMSupperlimits8|CMS upper limits 13TeV]], [[#CMSefficiencymaps8|CMS efficiency maps 13TeV]].
-[[#ATLASupperlimits8|ATLAS upper limits 8TeV]], [[#ATLASefficiencymaps8|ATLAS efficiency maps 8TeV]], [[#CMSupperlimits8|CMS upper limits 8TeV]], [[#CMSefficiencymaps8|CMS efficiency maps 8TeV]].
 """)
 
-def writeTableHeader ( true_lines ):
+def writeTableList ( wFile, database ):
+    wFile.write ( "== Individual tables ==\n" )
+    for sqrts in [ 13, 8 ]:
+        for exp in [ "ATLAS", "CMS" ]:
+            for tpe in [ "upper limits", "efficiency maps" ]:
+                expResList = getExpList ( sqrts, exp, tpe, database )
+                stpe = tpe.replace ( " ", "" )
+                wFile.write ( " * [[#%s%s%d|%s %s, %d TeV]]: %d analyses\n" % \
+                        ( exp, stpe, sqrts, exp, tpe, sqrts, len(expResList) ) )
+
+def getDatasetName ( txname ):
+    dataset = txname.path [ : txname.path.rfind("/") ]
+    dataset = dataset [ dataset.rfind("/")+1 : ]
+    dataset = dataset.replace ( "HighPt", "!HighPt" )
+    return dataset
+
+def writeTableHeader ( true_lines, tpe ):
     fields = [ "Result", "Txname", "Validated?", "Validation plots", "comment" ]
+    if "efficiency" in tpe:
+        fields.insert(1, "Dataset" )
     ret=""
     for i in fields:
         ret=ret +  ( "||<#EEEEEE:> '''%s''' " % i )
@@ -43,77 +58,116 @@ def writeTableHeader ( true_lines ):
     true_lines.append ( ret )
     # wFile.write ( "||<#EEEEEE:> '''Result''' ||<#EEEEEE:> '''Txname''' ||<#EEEEEE:> '''Validated?''' ||<#EEEEEE:> '''Validation Plots''' ||<#EEEEEE:> '''comment''' ||\n" )
 
-def writeExpRes( expRes, nlines, true_lines, false_lines, databasePath, urldir ):
+def writeExpRes( expRes, nlines, true_lines, false_lines, databasePath, urldir, tpe ):
     valDir = os.path.join(expRes.path,'validation').replace("\n","")
     if not os.path.isdir(valDir): return
-    else:
-        id = expRes.getValuesFor('id')[0]
-        txnames = expRes.getTxNames()
-        line = "||<|%i> [[%s|%s]]" %(len(txnames),\
-                expRes.getValuesFor('url')[0], id )
-        for txname in txnames:
-            validated = txname.getInfo('validated')
-            color=""
-            if validated is True: color = "#32CD32"
-            elif validated in [ None, "n/a" ]: color = "#778899"
-            elif validated in [ False, "tbd" ]: color = "#FF1100"
-            txn = txname.txName
-            txnbrs = txn
-            #if txnbrs == "TChiChipmStauL":
-            #    txnbrs = "TChi-ChipmStauL"
-            sval = str(validated).strip()
-            line += '||[[SmsDictionary#%s|%s]]||<style="color: %s;"> %s ||' \
-                % (txn, txnbrs, color, sval)
-            hasFig=False
-            for fig in glob.glob(valDir+"/"+txname.txName+"_*_pretty.pdf"):
-                pngname = fig.replace(".pdf",".png" )
-                if not os.path.exists ( pngname ):
-                    cmd = "convert -crop 600x420+250+10 %s %s" % ( fig, pngname )
-                    C.getoutput ( cmd )
-                # figName = fig.replace(valDir+"/","")
-                figName = pngname.replace(valDir+"/","").replace ( databasePath, "" )
-                dirPath =  os.path.join( urldir, valDir.replace(databasePath,""))
-                figPath = dirPath+"/"+figName
-                # print ( "figPath=",figPath, "txname=", txname.txName )
-                #line += "[[http://smodels.hephy.at"+figPath+\
-                #        "|"+figName+"]]<<BR>>"
-                figC = "http://smodels.hephy.at"+figPath
-                line += "[[%s|{{%s||width=200}}]]" % ( figC, figC )
-                #line += "{{http://smodels.hephy.at"+figPath+\
-                #        "||width=300}}"
-                line += "<<BR>>"
-                hasFig=True
-            if hasFig:
-                line = line[:-6] ## remove last BR
-            if not "attachment" in line:  #In case there are no plots
-                line += "  ||"
-            else:
-                line = line[:line.rfind("<<BR>>")] + "||"
-            if os.path.isfile(valDir+"/"+txname.txName+".comment"):
-                commentPath = dirPath+"/"+txname.txName+".comment"
-                line += "[[http://smodels.hephy.at"+commentPath+\
-                        "|comment"+"]] ||\n"
-                #        "|"+txname.txName+".comment"+"]] ||\n"
-            else:
-                line += " ||\n" #In case there are no comments
-        if "XXX#778899" in line: none_lines.append(line)
-        # if "#778899" in line: none_lines.append(line)
-        elif "#FF0000" in line: false_lines.append(line)
-        else: true_lines.append(line)
-        nlines += 1
+    id = expRes.getValuesFor('id')[0]
+    txnames = expRes.getTxNames()
+    ltxn = 0 ## len(txnames)
+    for txname in txnames:
+        validated = txname.getInfo('validated')
+        if validated == "n/a": continue
+        ltxn += 1
+    line = "||<|%i> [[%s|%s]]" %( ltxn, expRes.getValuesFor('url')[0], id )
+    hadTxname = False
+    for txname in txnames:
+        validated = txname.getInfo('validated')
+        if validated == "n/a": continue
+        color=""
+        if validated is True: color = "#32CD32"
+        elif validated in [ None, "n/a" ]: color = "#778899"
+        elif validated in [ False, "tbd" ]: color = "#FF1100"
+        txn = txname.txName
+        txnbrs = txn
+        #if txnbrs == "TChiChipmStauL":
+        #    txnbrs = "TChi-ChipmStauL"
+        sval = str(validated).strip()
+        if "efficiency" in tpe:
+            dataset = getDatasetName ( txname )
+            if dataset == "data":
+                continue
+            # print ( "txname=", dataset )
+            line += "|| %s " % dataset
+        hadTxname = True
+        line += '||[[SmsDictionary#%s|%s]]' % ( txn, txnbrs )
+        line += '||<style="color: %s;"> %s ||' % ( color, sval )
+        hasFig=False
+        for fig in glob.glob(valDir+"/"+txname.txName+"_*_pretty.pdf"):
+            pngname = fig.replace(".pdf",".png" )
+            if not os.path.exists ( pngname ):
+                cmd = "convert -crop 600x420+250+10 %s %s" % ( fig, pngname )
+                C.getoutput ( cmd )
+            # figName = fig.replace(valDir+"/","")
+            figName = pngname.replace(valDir+"/","").replace ( databasePath, "" )
+            dirPath =  os.path.join( urldir, valDir.replace(databasePath,""))
+            figPath = dirPath+"/"+figName
+            # print ( "figPath=",figPath, "txname=", txname.txName )
+            #line += "[[http://smodels.hephy.at"+figPath+\
+            #        "|"+figName+"]]<<BR>>"
+            figC = "http://smodels.hephy.at"+figPath
+            line += "[[%s|{{%s||width=200}}]]" % ( figC, figC )
+            #line += "{{http://smodels.hephy.at"+figPath+\
+            #        "||width=300}}"
+            line += "<<BR>>"
+            hasFig=True
+        if hasFig:
+            line = line[:-6] ## remove last BR
+        if not "attachment" in line:  #In case there are no plots
+            line += "  ||"
+        else:
+            line = line[:line.rfind("<<BR>>")] + "||"
+        if os.path.isfile(valDir+"/"+txname.txName+".comment"):
+            commentPath = dirPath+"/"+txname.txName+".comment"
+            line += "[[http://smodels.hephy.at"+commentPath+\
+                    "|comment"+"]] ||\n"
+            #        "|"+txname.txName+".comment"+"]] ||\n"
+        else:
+            line += " ||\n" #In case there are no comments
+    if not hadTxname: return
+    if "XXX#778899" in line: none_lines.append(line)
+    # if "#778899" in line: none_lines.append(line)
+    elif "#FF0000" in line: false_lines.append(line)
+    else: true_lines.append(line)
+    nlines += 1
 #         if nlines > 2: break
 
 def writeExperimentType ( sqrts, exp, tpe, expResList, wFile, nlines, true_lines, false_lines, databasePath, urldir ):
-    print ( "\n\nexp",exp )
+# print ( "\n\nexp",exp )
     stype=tpe.replace(" ","")
-    true_lines.append ( "\n<<Anchor(%s%s%d)>>\n" % ( exp,stype,sqrts ) )
-    true_lines.append ( "== %s %s, %d TeV: %d results ==\n" % (exp,tpe,sqrts, len(expResList)) )
-    writeTableHeader ( true_lines )
+    nres = 0
+    for expRes in expResList:
+        for tn in expRes.getTxNames():
+            validated = tn.getInfo('validated')
+            if validated in [ "n/a" ]: continue
+            if "efficiency" in tpe:
+                dataset = getDatasetName ( tn )
+                if dataset == "data": continue
+            nres += 1
+    if nres == 0:
+            return
+    true_lines.append ( "== %s %s, %d TeV: %d analyses, %d results total ==\n" % (exp,tpe,sqrts, len(expResList), nres ) )
+    true_lines.append ( "<<Anchor(%s%s%d)>>\n\n" % ( exp,stype,sqrts ) )
+    writeTableHeader ( true_lines, tpe )
     expResList.sort()
     for expRes in expResList:
         # print ( "id=",expRes.globalInfo.id )
-        writeExpRes ( expRes, nlines, true_lines, false_lines, databasePath, urldir )
+        writeExpRes ( expRes, nlines, true_lines, false_lines, databasePath, urldir, tpe )
 
+
+def getExpList ( sqrts, exp, tpe, database ):
+    dsids= [ None ]
+    if tpe == "efficiency maps":
+        dsids = [ 'all' ]
+    tmpList = database.getExpResults(datasetIDs=dsids )
+    # tpe = "upper limits"
+    #Load list of experimental results (DOES NOT INCLUDE efficiencies for now)
+    expResList = []
+    for i in tmpList:
+        if not exp in i.globalInfo.id: continue
+        xsqrts=int ( i.globalInfo.sqrts.asNumber(TeV) )
+        if xsqrts != sqrts: continue
+        expResList.append ( i )
+    return expResList
 
 def main():
     localdir = "/var/www/validationWiki"
@@ -137,22 +191,12 @@ def main():
     databasePath = os.path.join(os.path.expanduser("~"),"git/smodels-database/")
     database = Database(databasePath, progressbar=True )
     nlines = 0
-        
+    writeTableList ( wFile, database )
+
     for sqrts in [ 13, 8 ]:
         for exp in [ "ATLAS", "CMS" ]:
             for tpe in [ "upper limits", "efficiency maps" ]:
-                dsids= [ None ]
-                if tpe == "efficiency maps":
-                    dsids = [ 'all' ]
-                tmpList = database.getExpResults(datasetIDs=dsids )
-                # tpe = "upper limits"
-                #Load list of experimental results (DOES NOT INCLUDE efficiencies for now)
-                expResList = []
-                for i in tmpList:
-                    xsqrts=int ( i.globalInfo.sqrts.asNumber(TeV) )
-                    # print ( sqrts )
-                    if exp in i.globalInfo.id and xsqrts == sqrts:
-                        expResList.append ( i )
+                expResList = getExpList ( sqrts, exp, tpe, database )
                 writeExperimentType ( sqrts, exp, tpe, expResList, wFile, nlines, true_lines, false_lines, databasePath, urldir )
 
     #Copy/update the database plots and generate the wiki table
@@ -162,8 +206,10 @@ def main():
     print ( '--->Copy and paste the content to the SModelS wiki page.\n')
     print ( '--->(if xsel is installed, you should find the content in your clipboard.)\n' )
 
-    C.getoutput ( "cat %s | xsel -i" % fName )
+    wFile.write ( "\n\n" )
     wFile.close()
+
+    C.getoutput ( "cat %s | xsel -i" % fName )
 
 if __name__ == "__main__":
     main()
