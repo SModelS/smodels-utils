d6b486c636ffdc566c54b300100e266b1f88d6a1
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-06-21 11:21:22 +0200

    simple script to batch-draw everything

---
+++ smodels_utils/plotting/allFeynGraphs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+OUTPUTDIR="/tmp/feyn"
+
+for i in `ls ../../lhe/*.lhe`; do
+	j=`echo $i | sed -e 's;.*lhe/;;' | sed -e 's/_.\.lhe$//'`;
+	echo $j;
+	./feynmanGraph.py -T $j -s -i -o $OUTPUTDIR/${j}_feyn.pdf
+	convert $OUTPUTDIR/${j}_feyn.pdf $OUTPUTDIR/${j}_feyn.png
+done
