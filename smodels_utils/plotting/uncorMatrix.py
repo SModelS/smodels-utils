46375c335f14c36f6428d0fbfe98f76de3ce04b2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-08-04 16:06:46 +0200

    correlation plot

--- smodels_utils/plotting/uncorMatrix.py
+++ smodels_utils/plotting/uncorMatrix.py
@@ -4,15 +4,15 @@ from __future__ import print_function
 import sys, os, time
 sys.path.insert(0,"../")
 from smodels.experiment.databaseObj import Database
-from smodels.theory.theoryPrediction import theoryPredictionsFor
-from smodels.experiment.exceptions import SModelSExperimentError
 from smodels.tools.smodelsLogging import setLogLevel
 from smodels.tools.colors import colors
-from smodels.tools.physicsUnits import pb, fb, GeV
-from smodels.theory import slhaDecomposer
 import ROOT
+import IPython
 
 ROOT.gStyle.SetOptStat(0000)
+ROOT.gStyle.SetPalette(1)
+ROOT.gStyle.SetNumberContours(2)
+ROOT.gStyle.SetPadLeftMargin(.25)
 
 colors.on = True
 setLogLevel ( "debug" )
@@ -23,7 +23,10 @@ print(d)
 results = d.getExpResults()
 nres = len ( results )
 
-h=ROOT.TH2F ( "Correlations", "Correlations", nres, 0., nres, nres, 0., nres )
+ROOT.c1=ROOT.TCanvas("c1","c1",640,480)
+ROOT.c1.SetLeftMargin(0.19)
+
+h=ROOT.TH2F ( "Correlations", "Correlations between analyses", nres, 0., nres, nres, 0., nres )
 xaxis = h.GetXaxis()
 yaxis = h.GetYaxis()
 
@@ -33,8 +36,13 @@ for x,e in enumerate(results):
     for y,f in enumerate(results):
         isUn = e.isUncorrelatedWith ( f )
         if isUn:
-            h.SetBinContent ( x+1, y+1, 1 )
+            h.SetBinContent ( x+1, y+1, 1e-5 )
+        else:
+            h.SetBinContent ( x+1, y+1, 1. )
         print ( e.globalInfo.id, f.globalInfo.id, isUn )
 
-h.Draw("colz")
+h.Draw("col")
+ROOT.gPad.SetGrid()
 ROOT.c1.Print("matrix.png" )
+ROOT.c1.Print("matrix.pdf" )
+#IPython.embed()
