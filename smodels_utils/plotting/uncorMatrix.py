9abd0da7b7f9f7581d736dcb5cc98e2ac307c521
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-08-04 16:47:42 +0200

    nice plot

--- smodels_utils/plotting/uncorMatrix.py
+++ smodels_utils/plotting/uncorMatrix.py
@@ -5,40 +5,54 @@ import sys, os, time
 sys.path.insert(0,"../")
 from smodels.experiment.databaseObj import Database
 from smodels.tools.smodelsLogging import setLogLevel
+from smodels.tools.physicsUnits import TeV
 from smodels.tools.colors import colors
 import ROOT
 import IPython
+import ctypes
 
 ROOT.gStyle.SetOptStat(0000)
-ROOT.gStyle.SetPalette(1)
-ROOT.gStyle.SetNumberContours(2)
+cols = [ ROOT.kRed, ROOT.kWhite, ROOT.kGreen ]
+ROOT.gStyle.SetPalette(len(cols), (ctypes.c_int * len(cols))(*cols) )
+ROOT.gStyle.SetNumberContours(len(cols))
+
 ROOT.gStyle.SetPadLeftMargin(.25)
 
 colors.on = True
 setLogLevel ( "debug" )
 
-dir = "/home/walten/git/smodels/test/database/"
+#dir = "/home/walten/git/smodels/test/database/"
+dir = "/home/walten/git/smodels-database/"
 d=Database( dir, discard_zeroes = True )
 print(d)
 results = d.getExpResults()
+#results.sort()
 nres = len ( results )
 
-ROOT.c1=ROOT.TCanvas("c1","c1",640,480)
-ROOT.c1.SetLeftMargin(0.19)
+ROOT.c1=ROOT.TCanvas("c1","c1",1600,1500)
+ROOT.c1.SetLeftMargin(0.17)
+ROOT.c1.SetBottomMargin(0.17)
 
-h=ROOT.TH2F ( "Correlations", "Correlations between analyses", nres, 0., nres, nres, 0., nres )
+h=ROOT.TH2F ( "Correlations", "Correlations between analyses (green is uncorrelated)", nres, 0., nres, nres, 0., nres )
 xaxis = h.GetXaxis()
 yaxis = h.GetYaxis()
+xaxis.SetLabelSize(.02)
+yaxis.SetLabelSize(.02)
 
 for x,e in enumerate(results):
-    xaxis.SetBinLabel(x+1,e.globalInfo.id )
-    yaxis.SetBinLabel(x+1,e.globalInfo.id )
+    label = e.globalInfo.id
+    if e.globalInfo.sqrts.asNumber(TeV) > 10.:
+        label = "#color[12]{%s}" % e.globalInfo.id#+ "[%d]" % e.globalInfo.sqrts.asNumber(TeV)
+    xaxis.SetBinLabel(x+1, label )
+    yaxis.SetBinLabel(x+1, label )
     for y,f in enumerate(results):
         isUn = e.isUncorrelatedWith ( f )
         if isUn:
-            h.SetBinContent ( x+1, y+1, 1e-5 )
-        else:
             h.SetBinContent ( x+1, y+1, 1. )
+        else:
+            h.SetBinContent ( x+1, y+1, -1. )
+        if y==x:
+            h.SetBinContent ( x+1, y+1, 0. )
         print ( e.globalInfo.id, f.globalInfo.id, isUn )
 
 h.Draw("col")
