aba2a3d9be5ca634192c75795d2ee55393655147
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-08-16 19:49:30 +0200

    removing more tarballs

--- EM_Creator/Programs_Installation/fetch.sh
+++ EM_Creator/Programs_Installation/fetch.sh
@@ -2,6 +2,8 @@
 
 URL=http://smodels.hephy.at/downloads/tarballs
 
-wget $URL/ma5_v1.4beta.tar 
-wget $URL/MG5_aMC_v2.3.3.tar
-wget $URL/pythia-pgs.tgz
+for i in ma5_v1.4beta.tar MG5_aMC_v2.3.3.tar pythia-pgs.tgz; do
+	echo $i;
+	rm -f $i
+	wget $URL/$i
+done
