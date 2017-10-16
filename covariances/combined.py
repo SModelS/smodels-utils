849e0dd0a39801240f998d12ebef0c8eb8780be8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2017-10-17 00:11:02 +0200

    updates from plane

---
+++ covariances/combined.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" Identify a good example for how to combine results. """
+
+
+import sys
+from smodels.experiment.databaseObj import Database
+
+d=Database( "/home/walten/git/smodels-database" )
+
+txnames={}
+
+results=d.getExpResults()
+for r in results:
+    if hasattr ( r, "contact" ) and "fastlim" in r.contact.lower():
+        continue
+    for t in r.getTxNames():
+        if not t.hasLikelihood():
+            continue
+        txn=t.txName
+        if not txn in txnames.keys():
+            txnames[txn]=[]
+        if not r in txnames[txn]:
+            txnames[txn].append ( r )
+
+
+for txname,analyses in txnames.items():
+    if len(analyses) < 2:
+        continue
+    hasLLHDs=0
+    for ctr,a in enumerate(analyses):
+        for b in analyses[ctr+1:]:
+            if a.isUncorrelatedWith ( b ) == True:
+                print ( "%s: %s <-> %s" % ( txname, a.id(), b.id() ) )
+    # sys.exit()
--- smodels_utils/plotting/uncorMatrix.py
+++ smodels_utils/plotting/uncorMatrix.py
@@ -12,6 +12,8 @@ import IPython
 import ctypes
 
 ROOT.gStyle.SetOptStat(0000)
+
+ROOT.gROOT.SetBatch()
 cols = [ ROOT.kRed, ROOT.kWhite, ROOT.kGreen ]
 ROOT.gStyle.SetPalette(len(cols), (ctypes.c_int * len(cols))(*cols) )
 ROOT.gStyle.SetNumberContours(len(cols))
@@ -33,14 +35,32 @@ ROOT.c1=ROOT.TCanvas("c1","c1",1600,1500)
 ROOT.c1.SetLeftMargin(0.17)
 ROOT.c1.SetBottomMargin(0.17)
 
-h=ROOT.TH2F ( "Correlations", "Correlations between analyses (green is uncorrelated)", nres, 0., nres, nres, 0., nres )
+h=ROOT.TH2F ( "Correlations", 
+              "Correlations between analyses (green is uncorrelated)", 
+              nres, 0., nres, nres, 0., nres )
 xaxis = h.GetXaxis()
 yaxis = h.GetYaxis()
 xaxis.SetLabelSize(.02)
 yaxis.SetLabelSize(.02)
 
+def hasLLHD ( analysis ) : 
+    """ can one create likelihoods from analyses?
+        true for efficiency maps and upper limits with expected values. """
+    if len ( analysis.datasets)>1:
+        return True
+
+    ds=analysis.datasets[0]
+    if ds.dataInfo.dataType=="efficiencyMap":
+        return True
+    for tx in ds.txnameList:
+        if tx.hasLikelihood():
+            return True
+    return False
+
+
 for x,e in enumerate(results):
     label = e.globalInfo.id
+    hasLikelihood = hasLLHD ( e )
     if e.globalInfo.sqrts.asNumber(TeV) > 10.:
         label = "#color[12]{%s}" % e.globalInfo.id#+ "[%d]" % e.globalInfo.sqrts.asNumber(TeV)
     xaxis.SetBinLabel(x+1, label )
@@ -53,7 +73,8 @@ for x,e in enumerate(results):
             h.SetBinContent ( x+1, y+1, -1. )
         if y==x:
             h.SetBinContent ( x+1, y+1, 0. )
-        print ( e.globalInfo.id, f.globalInfo.id, isUn )
+        if not hasLikelihood or not hasLLHD ( f ): ## has no llhd? cannot be combined
+            h.SetBinContent ( x+1, y+1, 0. )
 
 h.Draw("col")
 ROOT.gPad.SetGrid()
