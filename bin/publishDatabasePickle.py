55b0672b7a1eb8813354e89a01b57813cf4cf8c7
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-02-28 10:09:27 +0100

    first working version

--- bin/publishDatabasePickle.py
+++ bin/publishDatabasePickle.py
@@ -9,6 +9,7 @@ import pickle, commands, os, sys, argparse
 def main():
     ap = argparse.ArgumentParser( description="makes a database pickle file publically available (run it on the smodels)" )
     ap.add_argument('-f', '--filename', help='name of pickle file', default="database.pcl" )
+    ap.add_argument('-d', '--dry_run', help='dont copy to final destionation', action="store_true" )
     args = ap.parse_args()
     p=open(args.filename)
     meta=pickle.load(p)
@@ -20,13 +21,21 @@ def main():
     Dict = { "lastchanged": meta.mtime, "size": os.stat(args.filename).st_size, "url": "http://smodels.hephy.at/database/off%s.pcl" % ver }
     f.write ( "%s\n" % Dict )
     f.close()
-    cmd = "cp %s /var/www/database/off%s.pcl" % ( args.filename, ver )
+    # cmd = "cp %s /var/www/database/off%s.pcl" % ( args.filename, ver )
+    cmd = "cp %s /nfsdata/walten/database/off%s.pcl" % ( args.filename, ver )
     print ( cmd )
-    a=commands.getoutput ( cmd )
-    print ( a )
+    if not args.dry_run:
+        a=commands.getoutput ( cmd )
+        print ( a )
+    cmd = "ln -s /nfsdata/walten/database/off%s.pcl /var/www/database/" % ( ver )
+    print ( cmd )
+    if not args.dry_run:
+        a=commands.getoutput ( cmd )
+        print ( a )
     cmd = "cp %s /var/www/database/official%s" % ( infofile, ver )
     print ( cmd )
-    a=commands.getoutput ( cmd )
-    print ( a )
+    if not args.dry_run:
+        a=commands.getoutput ( cmd )
+        print ( a )
 
 main()
