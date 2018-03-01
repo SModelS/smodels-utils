634b69087e5e0a299173a8876cf6f9d9933f698e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-03-01 14:35:20 +0100

    working with ssh

--- bin/publishDatabasePickle.py
+++ bin/publishDatabasePickle.py
@@ -10,29 +10,40 @@ def main():
     ap = argparse.ArgumentParser( description="makes a database pickle file publically available (run it on the smodels)" )
     ap.add_argument('-f', '--filename', help='name of pickle file', default="database.pcl" )
     ap.add_argument('-d', '--dry_run', help='dont copy to final destionation', action="store_true" )
+    ap.add_argument('-s', '--ssh', help='work remotely via ssh', action="store_true" )
     args = ap.parse_args()
     p=open(args.filename)
     meta=pickle.load(p)
-    print ( "meta: %s" % meta )
+    print ( meta )
     ver = meta.databaseVersion.replace(".","") 
     p.close()
     infofile = "official%s" % ver 
+    pclfilename = "off%s.pcl" % ver
+    if ver == "unittest":
+        smodels_ver = "112"
+        infofile = "unittest%s" % smodels_ver
+        pclfilename = "%s.pcl" % infofile
     f=open ( infofile, "w" )
-    Dict = { "lastchanged": meta.mtime, "size": os.stat(args.filename).st_size, "url": "http://smodels.hephy.at/database/off%s.pcl" % ver }
+    Dict = { "lastchanged": meta.mtime, "size": os.stat(args.filename).st_size, "url": "http://smodels.hephy.at/database/%s" % pclfilename }
     f.write ( "%s\n" % str(Dict).replace ( "'", '"' ) )
     f.close()
-    # cmd = "cp %s /var/www/database/off%s.pcl" % ( args.filename, ver )
-    cmd = "cp %s /nfsdata/walten/database/off%s.pcl" % ( args.filename, ver )
+    cmd = "cp %s /nfsdata/walten/database/%s" % ( args.filename, pclfilename )
+    if args.ssh:
+        cmd = "scp %s smodels.hephy.at:/nfsdata/walten/database/%s" % ( args.filename, pclfilename )
     print ( cmd )
     if not args.dry_run:
         a=commands.getoutput ( cmd )
         print ( a )
-    cmd = "ln -s /nfsdata/walten/database/off%s.pcl /var/www/database/" % ( ver )
+    cmd = "ln -s /nfsdata/walten/database/%s /var/www/database/" % ( pclfilename )
+    if args.ssh:
+        cmd = "ssh smodels.hephy.at ln -s /nfsdata/walten/database/%s /var/www/database/" % ( pclfilename )
     print ( cmd )
     if not args.dry_run:
         a=commands.getoutput ( cmd )
         print ( a )
-    cmd = "cp %s /var/www/database/official%s" % ( infofile, ver )
+    cmd = "cp %s /var/www/database/%s" % ( infofile, infofile )
+    if args.ssh:
+        cmd = "scp %s smodels.hephy.at:/var/www/database/%s" % ( infofile, infofile )
     print ( cmd )
     if not args.dry_run:
         a=commands.getoutput ( cmd )
