83075a4a826cb9247ecdda85116ff6c99604ee1a
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-03-22 13:16:57 +0100

    uploads

---
+++ covariances/combine.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+""" Identify a good example for how to combine results. """
+
+
+import sys, numpy
+from smodels.experiment.databaseObj import Database
+from smodels.theory.theoryPrediction import theoryPredictionsFor
+from smodels.theory.slhaDecomposer import decompose
+
+d=Database( "/home/walten/git/smodels-database" )
+# T1bbbb: CMS-PAS-SUS-16-016 <-> CMS-SUS-12-028
+
+slhafile = "./T1bbbb.slha" 
+
+smstoplist = decompose ( slhafile )
+
+print ( "Decomp", len(smstoplist) )
+
+results=d.getExpResults( analysisIDs=['CMS-PAS-SUS-16-016', 'CMS-SUS-12-028' ], 
+                         txnames = [ "T1bbbb" ] )
+
+def combined95UL ( theorypreds ):
+    """ compute the 95% CL upper limit for a combination of 
+    theory predictions. """
+    llhd=0.
+    llhds=[]
+    mu_v = numpy.arange ( 0., 5., .1 )
+    for mu in mu_v
+        tmp=1.
+        for p in theorypreds:
+            tmp=tmp*p.llhd ( mu )
+        llhds.append ( tmp )
+    s=sum(llhds)
+    for ctr,l in enumerate(llhds):
+        llhds[ctr]=l/s
+
+    return llhd
+
+for r in results:
+    preds = theoryPredictionsFor ( r, smstoplist )
+    print ( r )
+    for pred in preds:
+        pred.computeStatistics()
+        print ( pred.likelihood )
---
+++ covariances/list_combined.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" Identify a good example for how to combine results. """
+
+
+import sys
+from smodels.experiment.databaseObj import Database
+
+d=Database( "/home/walten/git/smodels-database" )
+
+txnames,sets={},{}
+
+results=d.getExpResults()
+for r in results:
+    if hasattr ( r, "contact" ) and "fastlim" in r.contact.lower():
+        continue
+    for t in r.getTxNames():
+        if not t.hasLikelihood():
+            continue
+        txn=t.txName
+        if not txn in txnames.keys():
+            txnames[txn]=[]
+        if not r in txnames[txn]:
+            txnames[txn].append ( r )
+
+
+for txname,analyses in txnames.items():
+    if len(analyses) < 2:
+        continue
+    hasLLHDs=0
+    for ctr,a in enumerate(analyses):
+        for b in analyses[ctr+1:]:
+            if a.isUncorrelatedWith ( b ) == True:
+                if not txname in sets.keys():
+                    sets[txname]=set()
+                sets[txname].add ( a.id() + " <-> " + b.id() )
+    #            print ( "%s: %s <-> %s" % ( txname, a.id(), b.id() ) )
+    # sys.exit()
+
+for k,v in sets.items():
+    for pair in v:
+        print ( "%s: %s" % ( k, pair ) )
