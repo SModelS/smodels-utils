03896ac048a7d5893b9b7b004813bfe6344332e3
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-04-05 11:45:50 +0200

    starting with argparse

--- covariances/drawC.py
+++ covariances/drawC.py
@@ -8,12 +8,8 @@ from matplotlib.lines import Line2D
 import sys
 import random
 import numpy
-n=1000
 
-if len(sys.argv)>1:
-    n=int(sys.argv[1])
-
-def addLine ( X, c, legend, style='-', ul=False ):
+def addLine ( ax, X, c, legend, style='-', ul=False ):
     l =Line2D ( list(X.keys()), [ numpy.mean(X[x]) for x in X.keys() ], c=c, linestyle=style, label=legend )
     ax.add_line(l)
     alpha=.5
@@ -27,78 +23,91 @@ def addLine ( X, c, legend, style='-', ul=False ):
 
 def checkN ( fname ):
     f=open(fname+".py")
-    l=f.readlines()[-1]
+    lines=f.readlines()
+    l=lines[-1]
     f.close()
     if not "]" in l[-2:]:
-        f=open(fname+".py","a")
+        f=open(fname+"tmp.py","w")
+        for line in lines:
+            f.write ( line )
         f.write("]\n" )
         f.close()
+        return fname+"tmp"
+    return fname
 
-fname="results%d" % n
-checkN(fname )
-D=__import__( fname )
-data=D.d
+def run ( n ):
+    fname="results%d" % n
+    fname=checkN(fname )
+    D=__import__( fname )
+    data=D.d
 
-#max number of bins
-bmax = max( [ x["nbins"] for x in data ] )
+    #max number of bins
+    bmax = max( [ x["nbins"] for x in data ] )
 
-xv=[]
+    xv=[]
 
-algos={ "nick": "k", "nickl": "k", "marg": "b", "marg10": "cyan", "prof": "r", 
-        "marg100": "g", "margl": "magenta", "profl": "orange" }
-descs={ "nick": "Nick", "nickl": "Linear Nick", "prof": "Profile", "marg": "Margin", 
-        "marg10": "Margin 10K", "marg100": "Margin 100", "margl": "Linear Margin",
-        "profl": "Linear Profile" }
+    algos={ "nick": "k", "nickl": "k", "marg": "b", "marg10": "cyan", "prof": "r", 
+            "marg100": "g", "margl": "magenta", "profl": "orange", "nicka": "k" }
+    descs={ "nick": "Nick", "nickl": "Linear Nick", "prof": "Profile", "marg": "Margin", 
+            "marg10": "Margin 10K", "marg100": "Margin 100", "margl": "Linear Margin",
+            "profl": "Linear Profile", "nicka": "Nick Wide" }
 
-R,T={},{}
-for a in algos.keys():
-    R[a],T[a]={},{}
-    for i in range(2,bmax+1):
-        T[a][i]=[]
-        R[a][i]=[]
-
-skip=[]
-for row in data:
-    nbins =  len(row["bins"]) 
-    xv.append ( nbins )
-
-    denom=row["ul_nick"]
+    R,T={},{}
     for a in algos.keys():
-        T[a][nbins].append( row["t_%s" % a ] )
-        r = row["ul_%s" % a ]
-        if type(r)==float:
-            R[a][nbins].append( row["ul_%s" % a ] / denom )
-        else:
-            print ( "skipping ul: %s" % r )
+        R[a],T[a]={},{}
+        for i in range(2,bmax+1):
+            T[a][i]=[]
+            R[a][i]=[]
 
-def mean ( Rx ):
-    x=[]
-    for k,v in Rx.items():
-        for i in v:
-            x.append ( i )
-    return numpy.mean(x), numpy.std(x)
+    skip=[]
+    for row in data:
+        nbins =  len(row["bins"]) 
+        xv.append ( nbins )
 
-fig,ax=plt.subplots()
-#print ( "skipped points %s" % skip )
-plt.scatter ( [ i - .1 for i in xv ], [ random.uniform(.8,1.2) for x in xv ], c='w' )
-for a,c in algos.items():
-    print ( descs[a], mean ( R[a] ) )
-    if a not in [ "marg100" ]: ##  [ "nickl", "marg100" ]:
-        addLine( R[a], c, descs[a], ul=True )
-plt.legend (  )
-plt.xlabel ( "number of signal regions" )
-plt.ylabel ( "ul / ul(nick)" )
-plt.savefig ( "comp.png" )
-plt.clf()
-fig,ax=plt.subplots()
-M = max([numpy.mean(x+[0]) for x in T["marg10"].values()]) 
-print ( "max", M )
-plt.scatter ( [ i - .1 for i in xv ], [ random.uniform(0,M) for x in xv ], c='w' )
+        denom=row["ul_nick"]
+        for a in algos.keys():
+            T[a][nbins].append( row["t_%s" % a ] )
+            r = row["ul_%s" % a ]
+            if type(r)==float:
+                R[a][nbins].append( row["ul_%s" % a ] / denom )
+            else:
+                print ( "skipping ul: %s" % r )
 
-for a,c in algos.items():
-    addLine( T[a], c, descs[a] )
-plt.xlabel ( "number of signal regions" )
-plt.ylabel ( "time per point [s]" )
-plt.legend (  )
-fig.savefig ( "t.png" )
+    def mean ( Rx ):
+        x=[]
+        for k,v in Rx.items():
+            for i in v:
+                x.append ( i )
+        return numpy.mean(x), numpy.std(x)
 
+    fig,ax=plt.subplots()
+    #print ( "skipped points %s" % skip )
+    plt.scatter ( [ i - .1 for i in xv ], [ random.uniform(.8,1.2) for x in xv ], c='w' )
+    for a,c in algos.items():
+        print ( descs[a], mean ( R[a] ) )
+        if a not in [ "marg100" ]: ##  [ "nickl", "marg100" ]:
+            addLine( ax, R[a], c, descs[a], ul=True )
+    plt.legend (  )
+    plt.xlabel ( "number of signal regions" )
+    plt.ylabel ( "ul / ul(nick)" )
+    plt.savefig ( "comp.png" )
+    plt.clf()
+    fig,ax=plt.subplots()
+    M = max([numpy.mean(x+[0]) for x in T["marg10"].values()]) 
+    print ( "max", M )
+    plt.scatter ( [ i - .1 for i in xv ], [ random.uniform(0,M) for x in xv ], c='w' )
+
+    for a,c in algos.items():
+        addLine( ax, T[a], c, descs[a] )
+    plt.xlabel ( "number of signal regions" )
+    plt.ylabel ( "time per point [s]" )
+    plt.legend (  )
+    fig.savefig ( "t.png" )
+
+if __name__ == "__main__":
+    import argparse
+    ap = argparse.ArgumentParser( description="Draw various validation plots" )
+    ap.add_argument('-n', '--nruns', type=int, default=1000, 
+                    help="which result file to access (result<nruns>.py)" )
+    args=ap.parse_args()
+    run ( args.nruns )
