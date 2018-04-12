52ae886103c9d387797d8af3d5611e1c30cebaad
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-04-12 16:43:33 +0200

    linear regression works

--- ml/train.py
+++ ml/train.py
@@ -15,51 +15,30 @@ import IPython
 #model = Model ( inputs=inputs, outputs=preds )
 ## define the network
 model = Sequential()
-model.add(Dense(2, activation="linear", input_shape=(2,)))
-#model.add(Dense(12, init='uniform', activation='sigmoid'))
-#model.add(Dense(12, init='uniform', activation='sigmoid'))
-##model.add(Dense(1, kernel_initializer="normal" )) ## activation="sigmoid"))
-#model.add(Activation('tanh'))
-model.add(Dense(1,activation='sigmoid'))
+model.add(Dense(4, activation="linear", input_shape=(2,)))
+# model.add(Dense(4, activation="relu"))
+model.add(Dense(1, activation="linear" ))
 model.summary()
 
 model.compile ( loss="mean_squared_error", optimizer="adam", metrics=["mse"] )
 # model.compile ( loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"] )
 
-print ( "Now loading database" )
-from smodels.experiment.databaseObj import Database
-database=Database("database/")
-from smodels.tools.physicsUnits import GeV, fb
+print ( "Now loading data" )
+import pickle
+f=open("data.pcl","rb")
+tr_data=pickle.load ( f )
+tr_labels=pickle.load ( f )
+val_data=pickle.load ( f )
+val_labels=pickle.load ( f )
+f.close()
 
-expres = database.getExpResults( analysisIDs=["CMS-PAS-SUS-12-026"] )[0]
 
-print ( "Done loading database" )
+def hash ( A ):
+    return int(A[0]*10000.+A[1])
 
-tr_data, tr_labels = [], []
-val_data, val_labels=[], []
-
-print ( "Now get upper limits" )
-for mother in np.arange ( 600., 1100., 10. ):
-    for lsp in np.arange ( 0., mother, 10. ):
-        masses=[[ mother*GeV, lsp*GeV], [ mother*GeV, lsp*GeV] ]
-        ul = expres.getUpperLimitFor ( txname="T1tttt", mass=masses )
-        if type(ul) == type(None):
-            continue
-        tr_data.append ( np.array( [ mother, lsp ] ) )
-        # tr_data.append ( np.array( [ mother ] ) )
-        tr_labels.append ( ul.asNumber(fb) )
-        if mother == 700. and lsp==200.:
-            print ( "training data", mother, lsp, ul.asNumber(fb) )
-for mother in np.arange ( 610., 1110., 40. ):
-    for lsp in np.arange ( 10., mother, 40. ):
-        masses=[[ mother*GeV, lsp*GeV], [ mother*GeV, lsp*GeV] ]
-        ul = expres.getUpperLimitFor ( txname="T1tttt", mass=masses )
-        if type(ul) == type(None):
-            continue
-        # val_data.append ( np.array ( [ mother ] ) )
-        val_data.append ( np.array ( [ mother, lsp ] ) )
-        val_labels.append ( ul.asNumber(fb) )
-print ( "Done getting upper limits" )
+X={}
+for d,l in zip (tr_data,tr_labels):
+    X[hash(d)]=l
 
 #cbk=callbacks.TensorBoard ( "logs/log_test_"+str(time.time()), histogram_freq=1, write_graph=True )
 cbk=callbacks.TensorBoard ( "logs/log.me", histogram_freq=1, write_graph=True )
@@ -68,15 +47,14 @@ cbk.set_model ( model )
 print ( "Now fitting ... " )
 history=model.fit ( np.array(tr_data), np.array(tr_labels), \
                     validation_data = ( np.array(val_data), np.array(val_labels) ), 
-                    batch_size=10, epochs=10, callbacks = [ cbk ] )
+                    batch_size=20, epochs=100, callbacks = [ cbk ] )
 print ( "Done fitting" )
-mother,lsp=700,200
-mass = np.array( [ [ 700, 200 ], [ 600, 100 ] ] )
-# mass = np.array( [ mother ] )
+mass = np.array( [ [ 600, 200 ], [ 700, 200 ], [ 800, 200 ], [ 900, 200 ], [ 1000, 200 ] ] )
 preds=model.predict ( mass )
 print ( "Now predict" ) 
 for m,p in zip ( mass,preds ):
-    print ( "%s -> %s" % ( m,p) )
+    print ( "%s -> %s, %s" % ( m,p[0], X[hash(m)] ) )
 
-model.save("first.h5")
+model.save("model.h5")
+model.save_weights("weights.h5")
 # IPython.embed()
