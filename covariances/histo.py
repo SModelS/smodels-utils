a4a8d5962c30326c82b745443376b27a85fc47df
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-05-24 00:14:14 +0200

    yau

--- covariances/histo.py
+++ covariances/histo.py
@@ -20,12 +20,15 @@ while True:
         d=pickle.load(f)
         ctr+=1
         id0,id1,id2,id3,id4,id5=d["n0"],d["n1"],d["n2"],d["n3"],d["n4"],d["n5"]
-        add(id0,11)
-        add(id1,9)
-        add(id2,7)
-        add(id3,5)
-        add(id4,3)
-        add(id5,1)
+        id6,id7=d["n6"],d["n7"]
+        add(id0,21)
+        add(id1,15)
+        add(id2,11)
+        add(id3,9)
+        add(id4,7)
+        add(id5,5)
+        add(id6,3)
+        add(id7,1)
     except EOFError as e:
         break
 
