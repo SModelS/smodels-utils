0ab8e7843729e9e3cf4554d8027e4edf3ccd0200
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-06-18 16:19:14 +0200

    add # datasets subtitle to pretty plots

--- validation/plottingFuncs.py
+++ validation/plottingFuncs.py
@@ -662,6 +662,8 @@ def createPrettyPlot(validationPlot,silentMode=True, looseness = 1.2 ):
     #    subtitle = subtitle[:100] + " ..."
     if len(validationPlot.expRes.datasets) == 1 and type(validationPlot.expRes.datasets[0].dataInfo.dataId)==type(None):
         subtitle = "upper limit"
+    if validationPlot.combine == False and len(validationPlot.expRes.datasets) > 1:
+        subtitle = "best SR"
     lsub=TLatex()
     lsub.SetNDC()
     lsub.SetTextAlign(31)
