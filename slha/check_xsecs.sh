43638d4f3c743cbafd1d4a14956e972a2efbddf1
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-10-17 13:21:27 +0200

    added simple xsec checker script

---
+++ slha/check_xsecs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## super simple script that produces xsecs for slha files, 
+## in case there arent any
+
+for i in `ls T*slha`; do
+	H=`cat $i | grep XSection | wc -l`
+	[ "$H" -eq "0" ] && { echo "$i, $H"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N; }
+done
