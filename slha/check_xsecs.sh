1dec52fb4f26abf8cbea70ad1b94bc95787b8450
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-10-17 14:17:59 +0200

    updated scripts

--- slha/check_xsecs.sh
+++ slha/check_xsecs.sh
@@ -3,8 +3,8 @@
 ## super simple script that produces xsecs for slha files, 
 ## in case there arent any
 
-for i in `ls T*slha`; do
+for i in `ls T*slha | tail -n 500`; do
 	H=`cat $i | grep -i xsection | wc -l`
 #	echo "$i $H"
-	[ "$H" -eq "0" ] && { echo "no xsecs found in $i"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N; }
+	[ "$H" -eq "0" ] && { echo "[check_xsecs.sh] no xsecs found in $i"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N -e 10000; }
 done
