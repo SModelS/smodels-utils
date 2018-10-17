1100b8db94db65b6477c4b823da880d6c1464a99
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-10-17 14:26:39 +0200

    nu_tua is 16 not 18

--- slha/check_xsecs.sh
+++ slha/check_xsecs.sh
@@ -4,6 +4,6 @@
 ## in case there arent any
 
 for i in `ls T*slha`; do
-	H=`cat $i | grep XSection | wc -l`
+	H=`cat $i | grep -i xsection | wc -l`
 	[ "$H" -eq "0" ] && { echo "$i, $H"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N; }
 done
