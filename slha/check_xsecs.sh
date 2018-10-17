7cd3a26f806f80ded7ca7714f376a14ee33d1113
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-10-17 13:38:38 +0200

    fix uppercase

--- slha/check_xsecs.sh
+++ slha/check_xsecs.sh
@@ -4,6 +4,7 @@
 ## in case there arent any
 
 for i in `ls T*slha`; do
-	H=`cat $i | grep XSection | wc -l`
-	[ "$H" -eq "0" ] && { echo "$i, $H"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N; }
+	H=`cat $i | grep -i xsection | wc -l`
+#	echo "$i $H"
+	[ "$H" -eq "0" ] && { echo "no xsecs found in $i"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N; }
 done
