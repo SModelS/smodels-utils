75dcf6de3cb8dff02b707d78c95bf7e46b3e96e2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-10-17 16:06:19 +0200

    shuffle files

--- slha/check_xsecs.sh
+++ slha/check_xsecs.sh
@@ -3,7 +3,7 @@
 ## super simple script that produces xsecs for slha files, 
 ## in case there arent any
 
-for i in `ls T*slha`; do
+for i in `ls T*slha | shuf`; do
 	H=`cat $i | grep -i xsection | wc -l`
 	[ "$H" -eq "0" ] && { echo "[check_xsecs.sh] no xsecs found in $i"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N -e 10000; }
 done
