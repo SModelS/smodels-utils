5b73306e19bcc9dd5963c796fee0d9a5fd4be72d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2018-12-15 23:00:11 +0100

    yau

--- bin/drawDownloads.py
+++ bin/drawDownloads.py
@@ -9,49 +9,64 @@ except:
 a=C.getoutput ( "tac ../log/pip_backup.log > ./.reverse.log" )
 
 ROOT.gStyle.SetOptStat(0000)
+Min,Max = 0.,400.
 
 t=ROOT.TTree()
 
 n=t.ReadFile( "./.reverse.log", "d/C[12]:p/D", ",")
 
-hi = ROOT.TH2F ( "h", "pip downloads", n, 0, n, 10, 0., 500. )
+hi = ROOT.TH2F ( "h", "SModelS: pip downloads", n, 0, n, 10, Min, Max )
 hi.SetYTitle ( "Downloads per day" )
 xa=hi.GetXaxis()
 lastdate=""
-gr = ROOT.TGraph(n)
-gr.SetLineColor(ROOT.kBlue) 
 values=[]
-for i in range(n):
+for i in range(1,n):
     t.GetEntry(i)
     y=t.GetLeaf("p").GetValue()
     values.append ( y )
 
-def smooth ( values, idx ):
-    ret,w=0.,0.
-    r=40
-    for i in range(idx-r,idx+r):
-        if i<0: continue
-        if i>=len(values): continue
-        tmp = scipy.stats.norm.pdf(i,idx,r)
-        w+=tmp
-        ret+=tmp*values[i]
-    return ret/w
+def releases():
+    f=open("../log/releases.log","r")
+    lines=f.readlines()
+    f.close()
+    ret=[]
+    for line in lines:
+        ret.append ( line.split(",") )
+    return ret
 
-
-for i in range(n):
+binlabels={}
+for i in range(1,n):
     t.GetEntry(i)
-    gr.SetPoint(i-1,float(i),smooth(values,i))
+    d = t.GetLeaf("d").GetValueString()
+    date = d.replace("201","'1")
+    binlabels[d]=i
     if i % 5 != 0:
         continue
-    date = (t.GetLeaf("d").GetValueString()[0:12]).replace("201","'1")
     if lastdate != date:
         xa.SetBinLabel( i, date )
         lastdate=date
 
 hi.Draw()
-
+t.SetLineWidth(2)
 t.Draw("p:d","","Lsame" )
-# gr.Draw("Csame")
+for d,ver in releases():
+    if "post" in ver:
+        continue
+    if d in binlabels.keys():
+        Bin = binlabels[d]
+        # print ( "release %s at %s" % ( ver, Bin ) )
+        l=ROOT.TLine ( Bin, Min, Bin, Max )
+        l.SetLineColor ( ROOT.kBlue )
+        l.SetLineWidth( 2 )
+        l.SetLineStyle ( 2 )
+        l.Draw()
+        pos=.8
+        t=ROOT.TText ( Bin, pos*Max, ver )
+        t.SetTextColor ( ROOT.kBlue )
+        t.SetTextSize(.04)
+        t.SetTextAngle(90.)
+        t.Draw()
+        values.append ( (l,t) )
 ROOT.c1.Modified()
 
 ROOT.c1.Print("downloads.png")
