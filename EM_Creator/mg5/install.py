4c1deb91d3a971bfb74aabbecf698f5c699c2d6a
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-04-09 18:17:55 +0200

    installation script

--- EM_Creator/mg5/install.py
+++ EM_Creator/mg5/install.py
@@ -3,23 +3,37 @@
 """ Simple script that handles the installation of MadGraph5.
 FIXME not finished yet. """
 
-import subprocess, os
+import subprocess, os, sys
     
 ver="2_6_5"
 
 def install():
+    if os.path.exists ( "bin/mg5_aMC" ):
+        ## seems like we have an install
+        return
     verdot = ver.replace("_",".")
     url="http://smodels.hephy.at/downloads/tarballs/"
     tarball = "MG5_aMC_v%s.tar.gz" % verdot
     if not os.path.exists ( tarball ):
         cmd = "wget %s/%s" % ( url, tarball )
         subprocess.getoutput ( cmd )
+    cmd = "tar xzvf %s" % tarball
+    subprocess.getoutput ( cmd )
+    cmd = "mv MG5_aMC_v%s/* ."  % ver
+    subprocess.getoutput ( cmd )
+    if not os.path.exists ( "bin/mg5_aMC" ):
+        print ( "something went wrong with the install. please check manually" )
+        sys.exit()
 
 def clean():
-    verdot = ver.replace("_",".")
-    tarball = "MG5_aMC_v%s.tar.gz" % verdot
-    if os.path.exists( tarball ):
-        os.unlink ( tarball )
+    import glob
+    for file in glob.glob ( "*" ):
+        if file not in [ "install.py", "install.txt" ]:
+            cmd = "rm -rf %s" % file
+            subprocess.getoutput ( cmd )
 
 if __name__ == "__main__":
-    install()
+    if len(sys.argv)>1 and sys.argv[1]=="clean":
+        clean()
+    else:
+        install()
