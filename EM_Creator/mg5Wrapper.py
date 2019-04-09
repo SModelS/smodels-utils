7edc3edcb7848a5ad567a3a87ba0d04bd55a9b9e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-04-09 18:06:36 +0200

    trying to restructure em bakery

---
+++ EM_Creator/mg5Wrapper.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+"""
+.. module:: mg5Wrapper
+        :synopsis: code that wraps around MadGraph5. Produces the data cards,
+                   and runs the mg5 executable.
+
+.. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
+"""
+
+import os, sys, colorama, subprocess, shutil
+
+class MG5Wrapper:
+    def __init__ ( self, ver="2_6_5" ):
+        """ 
+        :param mg5install: installation path to mg5
+        """
+        self.mg5install = "./mg5"
+        self.ver = ver
+        if not os.path.isdir ( self.mg5install ):
+            self.exe ( "mkdir %s" % self.mg5install )
+        tarball = self.mg5install + "/MG5_aMC_v%s.tar.gz" % ver.replace( "_", "." )
+        self.tarball = tarball
+        self.executable = self.mg5install + "/bin/mg5_aMC"
+        if not os.path.exists ( tarball ) and os.path.exists ( executable ):
+            self.fetch ()
+        if not os.path.exists(self.executable):
+            self.error ( "cannot find mg5 installation at %s" % mg5install )
+        self.templateDir = "templates/"
+        self.pythiaParams = { 'EBEAM': '6500', # Single Beam Energy expressed in GeV
+                              'NEVENTS': '10', 'MAXJETFLAVOR': '5', 
+                              'PDFLABEL': 'cteq6l1', 'XQCUT': '50', 'qcut': '90' }
+        self.commandfile = "mg5commands.txt"
+        self.info ( "initialised" )
+
+    def fetch ( self ):
+        cmd = "wget -O %s http://smodels.hephy.at/downloads/tarballs/%s" % ( self.tarball, self.tarball )
+        self.exe ( cmd )
+
+    def info ( self, *msg ):
+        print ( "%s[mg5Wrapper] %s%s" % ( colorama.Fore.YELLOW, " ".join ( msg ), \
+                   colorama.Fore.RESET ) )
+
+    def debug( self, *msg ):
+        pass
+
+    def msg ( self, *msg):
+        print ( "[mg5Wrapper] %s" % " ".join ( msg ) )
+
+    def error ( self, *msg ):
+        print ( "%s[mg5Wrapper] %s%s" % ( colorama.Fore.RED, " ".join ( msg ), \
+                   colorama.Fore.RESET ) )
+        sys.exit()
+
+    def writePythiaCard ( self, process="" ):
+        """ this method writes the pythia card for within mg5.
+        :param process: fixme (eg T2_1jet)
+        """
+        filename = "pythia.runcard"
+        self.debug ( "writing pythia run card %s" % filename )
+        if os.path.exists ( filename ):
+            os.unlink ( filename )
+        templatefile = self.templateDir+'/template_run_card.dat'
+        if not os.path.exists ( templatefile ):
+            self.error ( "cannot find %s" % templatefile )
+        tfile = open( templatefile,'r')
+        lines = tfile.readlines()
+        tfile.close()
+        g = open ( filename, "w" )
+        for line in lines:
+            for k,v in self.pythiaParams.items():
+                if k in line:
+                    line = line.replace(k,v)
+            g.write ( line )
+        g.close()
+        self.info ( "wrote pythia run card %s" % filename )
+
+    def writeCommandFile ( self, process = "" ):
+        """ this method writes the commands file for mg5.
+        :param process: fixme (eg T2_11jet)
+        """
+        f = open(self.commandfile,'w')
+        f.write('set automatic_html_opening False\n' )
+        f.write('launch '+ process+'\n')
+        f.write('pythia=ON\n')
+        f.write('madspin=OFF\n')
+        f.write('0\n')
+        f.write('0\n')
+        f.close()
+
+    def run( self ):
+        pass
+
+    def run( self, slhaFile, process ):
+        """ Run MG5 over an slhaFile, specifying the process """
+        # first write pythia card
+        self.writePythiaCard ( process=process )
+        # then write command file
+        self.writeCommandFile( process=process )
+        # then run madgraph5
+        self.execute ( slhaFile, process )
+
+    def install ( self ):
+        """ install the software """
+        pass ## need to write this still
+
+    def exe ( self, cmd ):
+        self.msg ( "now execute: %s" % cmd )
+        ret = subprocess.getoutput ( cmd )
+        if len(ret)==0:
+            return
+        # maxLength=60
+        maxLength=560
+        if len(ret)<maxLength:
+            self.msg ( " `- %s" % ret )
+            return
+        self.msg ( " `- %s" % ( ret[-maxLength:] ) )
+        # self.msg ( " `- %s ... %s" % ( ret[:maxLength/2-1], ret[-maxLength/2-1:] ) )
+
+    def execute ( self, slhaFile, process ):
+        templatefile = self.templateDir + '/MG5_Process_Cards/'+process+'.txt'
+        if not os.path.isfile( templatefile ):
+            self.error ( "The process card %s does not exist." % templatefile )
+        self.info ( "run mg5 for %s" % process )
+        cmd = "%s %s" % ( self.executable, templatefile )
+        self.exe ( cmd )
+        ## copy slha file
+        shutil.copyfile(slhaFile, process+'/Cards/param_card.dat' )
+        if (os.path.isdir(process+'/Events/run_01')):
+            shutil.rmtree(process+'/Events/run_01')
+        cmd = "%s %s" % ( self.executable, self.commandfile )
+        self.exe ( cmd )
+
+
+if __name__ == "__main__":
+    mg5 = MG5Wrapper()
+    process = "T2tt_1jet"
+    mg5.run( "slha/T2tt_997_213_997_213.slha", process )
