16b88b210308d4626133bfe9c7c81a7dd7a4926b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-04-09 23:31:43 +0200

    yup

--- EM_Creator/ma5/make.py
+++ EM_Creator/ma5/make.py
@@ -49,7 +49,7 @@ def install():
 def clean():
     import glob
     for file in glob.glob ( "*" ):
-        if file not in [ "install.py", "install.script" ]:
+        if file not in [ "make.py", "install.script", "Makefile" ]:
             cmd = "rm -rf %s" % file
             subprocess.getoutput ( cmd )
 
