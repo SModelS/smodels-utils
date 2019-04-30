414a6062d0f689fcfc54473fdab42e6b2a29336e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-04-30 12:54:37 +0200

    manipulation scripts for tarballs

---
+++ validation/createValidationPoints.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+"Manually" add points to a tarball. 
+This is opposed to ./runFileCreator.py, does tries
+to automatically determine the points needed for 
+validation.
+"""
+
+from __future__ import print_function
