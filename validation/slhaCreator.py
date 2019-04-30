3683b1dfcb9bb2f2957d21819e560140906a99b4
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-04-30 15:03:04 +0200

    updated slhaCreator

--- validation/slhaCreator.py
+++ validation/slhaCreator.py
@@ -2,7 +2,7 @@
 
 """
 .. module:: slhaCreator
-   :synopsis: Main methods for generating SLHA files for a given Txname from a 
+   :synopsis: Main methods for generating SLHA files for a given Txname from a
               template
 
 .. moduleauthor:: Andre Lessa <lessa.a.p@gmail.com>
@@ -21,13 +21,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.WARNING)
 import tempfile
 import pyslha
-import math
+import math, numpy, subprocess
 try: ## smodels <= 122
     from smodels.theory import slhaDecomposer as decomposer
 except ImportError: ## smodels >= 200
     from smodels.theory import decomposer
 from smodels.tools.physicsUnits import fb, GeV, TeV
-from smodels.tools.xsecComputer import XSecComputer, LO, NLL
+from smodels.tools import xsecComputer, runtime
 from smodels_utils.dataPreparation.massPlaneObjects import MassPlane
 from validation.pythiaCardGen import getPythiaCardFor
 
@@ -40,24 +40,26 @@ class TemplateFile(object):
     def __init__(self,template,axes,tempdir=None,pythiaVersion=6):
         """
         :param template: path to the template file
-        :param axes: string describing the axes for the template file 
+        :param axes: string describing the axes for the template file
                     (i.e. 2*Eq(mother,x)_Eq(inter0,y)_Eq(lsp,x-80.0))
         :param tempdir: Folder to store the SLHA files. If not set,
                         a temporary folder will be created at the current location.
-        :param pythiaVersion: Version of pythia to use (6 or 8). It specifies how 
+        :param pythiaVersion: Version of pythia to use (6 or 8). It specifies how
                               the pythiaCard will be generated.
         """
-        
+
         self.path = template
         self.slhaObj = None
         self.tags = []
         self.axes = axes
         self.motherPDGs = []
         self.pythiaCard = None
+        self.pythiaVersion = pythiaVersion
         if tempdir:
             self.tempdir = tempdir
         else:
             self.tempdir = tempfile.mkdtemp(dir=os.getcwd())
+            print ( "[slhaCreator] tempdir at %s" % self.tempdir )
         #Loads the information from the template file and store the axes labels
         if not os.path.isfile(template):
             logger.error("Template file %s not found." %template)
@@ -76,9 +78,10 @@ class TemplateFile(object):
             self.pythiaCard = getPythiaCardFor(self.motherPDGs,pythiaVersion=pythiaVersion)
         #Define original plot
         self.massPlane = MassPlane.fromString(None,self.axes)
-        
 
-    def createFileFor(self,ptDict,slhaname=None,computeXsecs=False, massesInFileName = False):
+
+    def createFileFor( self,ptDict,slhaname=None,computeXsecs=False,
+                       massesInFileName = False):
         """
         Creates a new SLHA file from the template.
         The entries on the template are replaced by the x,y values in pt.
@@ -87,6 +90,7 @@ class TemplateFile(object):
         :param slhaname: filename for the new file. If None, a random name for the file will be generated,
                      with prefix template and suffix .slha
         :param computeXsecs: if True, will compute NLL cross-sections for the file using 10k events
+        :param massesInFileName: if True, put the masses in the name of the slha file (eg T5WW_2200_1300_60_2200_1300_60.slha)
         :return: SLHA file name if file has been successfully generated, False otherwise.
         """
 
@@ -95,13 +99,13 @@ class TemplateFile(object):
         for ibr,br in enumerate(masses):
             massTag, widthTag = 'M', 'W'
             if ibr == 1: massTag, widthTag = 'm','W'
-            for im,m in enumerate(br): 
+            for im,m in enumerate(br):
                 if type(m)==tuple:
                     massDict[massTag+str(im)] = m[0]
                     massDict[widthTag+str(im)] = m[1] ## coordinateToWidth(m[1])
                 else:
-                    massDict[massTag+str(im)] = m 
-            
+                    massDict[massTag+str(im)] = m
+
         #First check if all the axes labels defined in the template appears in massDict
         if not set(self.tags).issubset(set(massDict.keys())):
             logger.error("Labels do not match the ones defined in %s. keys=%s. tags=%s." % \
@@ -112,8 +116,8 @@ class TemplateFile(object):
         fdata = ftemplate.read()
         ftemplate.close()
         for tag in massDict: fdata = fdata.replace(tag,str(massDict[tag]))
-            
-        #Create SLHA filename (if not defined)        
+
+        #Create SLHA filename (if not defined)
         if not slhaname:
             templateName = self.path[self.path.rfind("/")+1:self.path.rfind(".")]
             if not massesInFileName:
@@ -123,7 +127,7 @@ class TemplateFile(object):
             else:
                 slhaname = "%s" % (templateName)
                 for br in masses:
-                    for m in br: 
+                    for m in br:
                         if type(m)==tuple:
                             slhaname += "_%d_%.2g" % (m[0],m[1] )
                         else:
@@ -132,44 +136,55 @@ class TemplateFile(object):
                 slhaname = os.path.join(self.tempdir,slhaname)
 
         fdata = fdata[:fdata.find('XSECTION')]
-        
+
         #Save file
         fslha = open(slhaname,'w')
         fslha.write(fdata)
         fslha.close()
-                
-        #Compute cross-sections
-        if computeXsecs:
-            if self.pythiaCard:
-                print ( "143 add xsecs" )
-                computerLO = XSecComputer(LO, 1000, 6)
-                xsecsLO = computerLO.compute(8*TeV, slhaname,pythiacard=self.pythiaCard)
-                computerLO.addXSecToFile(xsecsLO,slhaname,comment="1k events (unit = pb)")         
-                computerNLL = XSecComputer(NLL, 1000, 6)
-                xsecsNLL = computerNLL.compute(8*TeV, slhaname,loFromSlha=True,
-                                               pythiacard=self.pythiaCard)
-                computerNLL.addXSecToFile(xsecsNLL,slhaname,comment="(unit = pb)")         
 
-                    
         logger.info("File %s created." %slhaname)
 
         return slhaname
-    
-    def createFilesFor( self, pts, massesInFileName=False ):
+
+    def createFilesFor( self, pts, massesInFileName=False, computeXsecs=False ):
         """
-        Creates new SLHA files from the template for the respective (x,y) values 
+        Creates new SLHA files from the template for the respective (x,y) values
         in pts.
-        For each distinct x value, new cross-sections will be computed.  
-        :param pts: list of dicts with values for the plot in GeV 
+        For each distinct x value, new cross-sections will be computed.
+        :param pts: list of dicts with values for the plot in GeV
                     (e.g. [{'x' : x1, 'y' : y1}, {'x' : x2, 'y' : y2}, ...])
         :return: list of SLHA file names generated.
         """
 
-        slhafiles = []        
+        slhafiles = []
         for pt in pts:
-            slhafile = self.createFileFor(pt,massesInFileName=massesInFileName)
+            slhafile = self.createFileFor( pt, computeXsecs=False,
+                                           massesInFileName=massesInFileName )
             if slhafile:
                 slhafiles.append(slhafile)
+        #Compute cross-sections
+        if computeXsecs:
+            if self.pythiaCard:
+                xargs = argparse.Namespace()
+                xargs.pythia6 = False
+                xargs.pythia8 = True
+                if self.pythiaVersion == 6:
+                    xargs.pythia6 = True
+                    xargs.pythia8 = False
+                xargs.sqrts = [[8, 13]]
+                xargs.ncpus = runtime.nCPUs()
+                xargs.nevents = 10000
+                xargs.pythiacard = self.pythiaCard
+                xargs.NLL = True
+                xargs.tofile = False
+                xargs.alltofile = True
+                xargs.keep=False
+                xargs.LOfromSLHA = False
+                xargs.query = False
+                xargs.colors = None
+                xargs.verbosity = 30
+                xargs.filename = self.tempdir
+                xsecComputer.main(xargs)
 
         return slhafiles
 
@@ -183,102 +198,114 @@ class TemplateFile(object):
         :param y: y value for the plot in GeV (i. e. lsp mass).
         :param z: z value for the plot in GeV (only for 3D grids).
         """
-        
-        
+
+
         inmasses = self.massPlane.getParticleMasses(x=x, y=y, z=z)
         #Add units:
         inmasses = [[m*GeV for m in br] for br in inmasses]
-                
+
         #First create temporary file:
         tempSLHA = self.createFileFor(x,y,z)
         if not tempSLHA: return False
         #Add cross-sections to file running only mother pair production:
         #(to guarantee the mother cross-section value is reliable)
-        if self.pythiaCard:        
-            print ( "208 add xsecs" )
+        if self.pythiaCard:
             computerLO = XSecComputer(LO, 1000, 6)
             xsecsLO = computerLO.compute(8*TeV, tempSLHA,pythiacard=self.pythiaCard)
-            computerLO.addXSecToFile(xsecsLO,tempSLHA,comment="1k events (unit = pb)")         
+            computerLO.addXSecToFile(xsecsLO,tempSLHA,comment="1k events (unit = pb)")
             computerNLL = XSecComputer(NLL, 1000, 6)
             xsecsNLL = computerNLL.compute(8*TeV, tempSLHA,loFromSlha=True,
                                            pythiacard=self.pythiaCard)
-            computerNLL.addXSecToFile(xsecsNLL,tempSLHA,comment="(unit = pb)")        
+            computerNLL.addXSecToFile(xsecsNLL,tempSLHA,comment="(unit = pb)")
         #Run decomposition on the file:
         sigmacut = 0.*fb
         mingap = 2.*GeV
         smstoplist = decomposer.decompose(tempSLHA, sigmacut,\
                         doCompress=True,doInvisible=True, minmassgap=mingap)
-        
+
         #Delete the temporary SLHA file and pythia card
         shutil.rmtree(os.path.dirname(os.path.realpath(tempSLHA)))
-        os.remove(self.pythiaCard)        
-        
+        os.remove(self.pythiaCard)
+
         if not smstoplist or not smstoplist.getElements():
             logger.error("Decomposition produced no results.")
             return False
-                
+
         allEls = smstoplist.getElements()
         goodEl = False
         for el in allEls:
             goodEl = txnameObj.hasElementAs(el)
             if goodEl:
                 break
-        
+
         #Check if a valid element was created:
         if not goodEl:
             logger.warning("No macthing element for %s generated from template" %txnameObj.txName)
             return False
-        
-        #Check if the masses match 
+
+        #Check if the masses match
         #(allow for the case where both branching orders matches the txname):
         if inmasses != goodEl.getMasses():
-            goodElB = txnameObj.hasElementAs(goodEl.switchBranches())            
+            goodElB = txnameObj.hasElementAs(goodEl.switchBranches())
             if (not goodElB) or (goodElB and inmasses != goodElB.getMasses()):
                 logger.warning("Masses do not seem to match")
                 return False
-        
-        
+
+
         return True
-        
+
 if __name__ == "__main__":
     import argparse, types
     argparser = argparse.ArgumentParser(description="creates slha files from template file in given mass ranges")
-    argparser.add_argument ( '-T', '--templatefile', nargs='?', help='path to template file', 
+    argparser.add_argument ( '-T', '--topology', nargs='?', help='topology to create SLHA files for',
         type=str, default='T1' )
-    argparser.add_argument ( '-a', '--axes', nargs='?', help='axes description', 
-        type=str, default='2*Eq(mother,x)_Eq(lsp,y)' )
-    argparser.add_argument ( '--xmin', nargs='?', help='minimum value for x', 
+    argparser.add_argument ( '-a', '--axes', nargs='?', help='axes description',
+        type=str, default='2*[[x, y]]' )
+    argparser.add_argument ( '--xmin', nargs='?', help='minimum value for x',
         type=float, default=100. )
-    argparser.add_argument ( '--xmax', nargs='?', help='maximum value for x', 
+    argparser.add_argument ( '--xmax', nargs='?', help='maximum value for x',
         type=float, default=300. )
-    argparser.add_argument ( '--dx', nargs='?', help='binning in x', 
+    argparser.add_argument ( '--dx', nargs='?', help='binning in x',
         type=float, default=25. )
-    argparser.add_argument ( '--ymin', nargs='?', help='minimum value for y', 
+    argparser.add_argument ( '--ymin', nargs='?', help='minimum value for y',
         type=float, default=100. )
-    argparser.add_argument ( '--ymax', nargs='?', help='maximum value for y', 
+    argparser.add_argument ( '--ymax', nargs='?', help='maximum value for y',
         type=float, default=300. )
-    argparser.add_argument ( '--dy', nargs='?', help='binning in y', 
+    argparser.add_argument ( '--dy', nargs='?', help='binning in y',
         type=float, default=25. )
-    argparser.add_argument('-6', '--pythia6', action='store_true',                 
-        help="use pythia6 for LO cross sections")                                     
-    argparser.add_argument('-8', '--pythia8', action='store_true',                 
-        help="use pythia8 for LO cross sections (default)")    
+    argparser.add_argument('-c', '--clear', action='store_true',
+        help="clear cruft files")
+    argparser.add_argument('-6', '--pythia6', action='store_true',
+        help="use pythia6 for LO cross sections")
+    argparser.add_argument('-8', '--pythia8', action='store_true',
+        help="use pythia8 for LO cross sections (default)")
     args=argparser.parse_args()
+    if args.clear:
+        subprocess.getoutput ( "rm -rf tmp* pythia*card" )
+        sys.exit()
     pythiaVersion = 8
     if args.pythia6:
         pythiaVersion = 6
 
-    templatefile = args.templatefile
+    templatefile="../slha/templates/%s.template" % args.topology
     if not os.path.exists ( templatefile ):
-        templatefile="../slha/templates/%s.template" % args.templatefile
-        if not os.path.exists ( templatefile ):
-            print ( "[slhaCreator] error: templatefile %s not found." % args.templatefile )
-            sys.exit()
+        print ( "[slhaCreator] error: templatefile %s not found." %
+                templatefile )
+        sys.exit()
     tempf = TemplateFile(templatefile,args.axes,pythiaVersion=pythiaVersion)
     masses=[]
-    import numpy
     for mother in numpy.arange(args.xmin,args.xmax+1,args.dx):
         for lsp in numpy.arange(args.ymin,args.ymax+1,args.dy):
             masses.append ( { "x": mother, "y": lsp } )
-    slhafiles = tempf.createFilesFor( masses, massesInFileName=True )
-    print ( slhafiles )
+    slhafiles = tempf.createFilesFor( masses, computeXsecs=True, massesInFileName=True )
+    print ( "Produced %s slha files" % len(slhafiles ) )
+    newtemp = tempfile.mkdtemp(dir="./" )
+    print ( "Now build new tarball in %s" % newtemp )
+    subprocess.getoutput ( "cd %s; tar xzvf ../../slha/%s.tar.gz" % \
+                           ( newtemp, args.topology ) )
+    subprocess.getoutput ( "mv %s/%s*.slha %s" % ( tempf.tempdir, args.topology, newtemp ) )
+    subprocess.getoutput ( "rm -rf %s" % tempf.tempdir )
+    subprocess.getoutput ( "cd %s; tar czvf ../%s.tar.gz %s*slha" % ( newtemp, args.topology, args.topology ) )
+    print ( "New tarball %s.tar.gz" % args.topology )
+    subprocess.getoutput ( "rm -rf %s" % newtemp )
+
