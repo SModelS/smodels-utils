52f73af44ce31f69b614b620eab264c4fc8507f8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-06-01 12:46:47 +0200

    fetch in one go

--- combinations/fetch_hiscore.sh
+++ combinations/fetch_hiscore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "You will need to supply your password on hepgpu"
-scp gpu:/local/wwaltenberger/git/smodels-utils/combinations/hiscore.pcl .
+scp gpu:/local/wwaltenberger/git/smodels-utils/combinations/his????.pcl .
 ./checkHiscores.py -f hiscore.pcl -t 1 -s best.pcl
 mv history.pcl history_old.py
-scp gpu:/local/wwaltenberger/git/smodels-utils/combinations/history.pcl .
+#scp gpu:/local/wwaltenberger/git/smodels-utils/combinations/history.pcl .
