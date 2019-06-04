ce4df269bf69f52e978a9ddd7b51911f9e79a665
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-06-04 22:53:02 +0200

    first movie maker

--- combinations/drawPath.py
+++ combinations/drawPath.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import pickle, subprocess, sys
+import pickle, subprocess, sys, copy
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -8,51 +8,80 @@ from mpl_toolkits.mplot3d import axes3d
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib import cm
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 from multiprocessing import Pool
 import math, copy
 import helpers
 
-# dpi = 200
-# dpi = 100
-dpi = 75
+Writer = animation.writers["ffmpeg"]
 
 class Drawer:
-    def __init__ ( self, picklefile ):
+    def __init__ ( self, picklefile, nmin, nmax ):
+        # self.dpi = 200
+        # self.dpi = 100
+        self.dpi = 75
+        self.nmin = nmin
+        self.nmax = nmax
+        self.counter = nmin
         self.picklefile = picklefile
-        ## the coordinates that we visualise
-        self.coordinates = ( 1000022, 1000001, 1000006 )
+        ## the coordinates that we possibly visualise
+        self.coordinates = ( 1000006, 1000001, 1000005 )
+        ## our current coordinates
+        self.current_coordinates = self.coordinates
+        # the current coordinates, as indices of self.coordinates
+        self.current_indices = ( True, True, True )
         self.getHistory()
+        self.initFigure()
+        self.history = []
 
-    def trim( self, filename ):
-        import subprocess
-        cmd = "convert -trim %s %s" % ( filename, filename.replace(".png","_trimmed.png") )
-        a = subprocess.getoutput ( cmd )
-        cmd = "rm %s" % filename 
-        a = subprocess.getoutput ( cmd )
+    def initFigure ( self ):
+        # plt.style.use("dark_background")
+        self.fig = plt.figure(dpi=self.dpi )
+        self.ax = self.fig.add_subplot ( 111, projection="3d" )
+        self.ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+        self.ax.set_xlim( 0., 2500. )
+        self.ax.set_ylim( 0., 2500. )
+        self.ax.set_zlim( 0., 2500. )
+        self.ax.set_xlabel ( helpers.toLatex(self.coordinates[0],True,True) )
+        self.ax.grid(False)
+        self.ax.set_ylabel ( helpers.toLatex(self.coordinates[1],True,True) )
+        self.ax.set_zlabel ( helpers.toLatex(self.coordinates[2],True,True) )
+        self.ax.zaxis.set_visible(False)
+        return self.fig
+
+    def next ( self ):
+        if self.counter >= self.nmax:
+            raise StopIteration ( "reached step #%d" % self.nmax )
+        self.draw ( self.counter )
+        self.counter+=1
 
     def getHistory( self ):
         """ load the history """
         f=open( self.picklefile, "rb" )
         walker = pickle.load ( f )
         f.close()
-        tmp = []
-        for pt in walker:
-            masses = [ pt["masses"][x] for x in self.coordinates ]
+        tmp = [ [0., 0., 0. ] ]
+        def getFilteredMass ( m ):
+            if m < 5e5:
+                return m
+            return 0.
+        for pt in walker[::10]:
+            masses = [ getFilteredMass(pt["masses"][x]) for x in self.coordinates ]
             tmp.append ( masses )
         self.walk = np.array ( tmp )
 
-    def save ( self, plt, ax, fig, ndim, nsteps, j ):
+    def save ( self, plt, ndim, nsteps, j ):
         filename = "pics/%03d%d.png" % ( nsteps-1, j )
-        #filename = "pics/%dd_%03d%d.png" % ( ndim, nsteps, j )
         #azim = -60 + 10 * math.sin ( nsteps * 2* math.pi / 2000. )
         # ax.view_init( azim= -60 )
-        plt.savefig ( filename, dpi=dpi )
-        # self.trim ( filename )
-        plt.clf()
-        plt.cla()
+        #import IPython
+        #IPython.embed()
+        plt.savefig ( filename, dpi=self.dpi )
+        for t in self.ax.texts + self.ax.lines:
+            t.set_visible(False)
 
     def cutCoords ( self, x, f, n ):
-        """ cut the last line to fraction f """ 
+        """ cut the last line to fraction f """
         if len(x)<2:
             return x
         rx = copy.deepcopy ( x )
@@ -73,124 +102,83 @@ class Drawer:
         return min(coords),max(coords[coords<3000])
 
     def getCoords ( self, j, n ):
-        """ FIXME what exactly does this do? """
+        """ get the coordinates of the current point, in the correct number
+            of dimensions """
         xc, yc, zc = self.walk[::,0], self.walk[::,1], self.walk[::,2]
-        xcoords=self.cutCoords ( xc, (j+1)/10., n )
-        ycoords=self.cutCoords ( yc, (j+1)/10., n )
-        zcoords=self.cutCoords ( zc, (j+1)/10., n )
-        return xcoords,ycoords,zcoords
+        xcoords = self.cutCoords ( xc, (j+1)/10., n )
+        ycoords = self.cutCoords ( yc, (j+1)/10., n )
+        zcoords = self.cutCoords ( zc, (j+1)/10., n )
+        # self.pprint ( "getting coordinates %d/%d: %s" % (j,n,xcoords[0]) )
+        return [ xcoords,ycoords,zcoords ]
 
-    def draw3d( self, n = 80 ):
+    def draw( self, n ):
         for j in range(0,10):
-            fig = plt.figure(dpi=dpi )
-            ax = fig.add_subplot ( 111, projection="3d" )
+            # self.initFigure()
+            title = "MCMC walk, after %d steps" % n
+            t = self.ax.text( 2500., 7., 0., title, horizontalalignment="center",
+                   verticalalignment="bottom", transform = self.ax.transAxes )
+            p = [ t ]
             xcoords,ycoords,zcoords = self.getCoords ( j, n )
             for i in range(n):
-                ax.plot( xcoords[i:i+2], ycoords[i:i+2], zcoords[i:i+2], c=cm.binary(.3+.6*i/n) )
-            ax.plot( [xcoords[-1]], [ycoords[-1]], [zcoords[-1]], "*", color='r' )
-            ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.zaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.set_xlim(self.minMax(xcoords))
-            ax.set_ylim(self.minMax(ycoords))
-            ax.set_zlim(self.minMax(zcoords))
-            ax.set_xlabel ( helpers.toLatex(self.coordinates[0],True,True) )
-            ax.set_ylabel ( helpers.toLatex(self.coordinates[1],True,True) )
-            ax.set_zlabel ( helpers.toLatex(self.coordinates[2],True,True) )
-            ax.grid(False)
-            title = "MCMC walk, after %d steps" % n 
-            ax.text( 10., 7., 0., title, horizontalalignment="center",
-                   verticalalignment="bottom", transform = ax.transAxes )
-            self.save ( plt, ax, fig, 3, n, j )
-            #del fig
-            #del ax
+                p += self.ax.plot( xcoords[i:i+2], ycoords[i:i+2], zcoords[i:i+2], \
+                         c=cm.binary(.3+.6*i/n) )
+            p += self.ax.plot( [xcoords[-1]], [ycoords[-1]], [zcoords[-1]], "*", color='r' )
+            self.ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+            yminmax = self.minMax(ycoords)
+            if abs(yminmax[1])<1e-5:
+                self.ax.yaxis.set_visible(False)
+            else:
+                self.ax.yaxis.set_visible(True)
 
-    def draw2d( self, n = 40 ):
-        fig = plt.figure(dpi=dpi)
-        ax = fig.add_subplot ( 111, projection="3d" )
-        for j in range(10):
-            fig = plt.figure( dpi=dpi )
-            ax = fig.add_subplot ( 111, projection="3d" )
-            xcoords,ycoords,zcoords = self.getCoords ( j, n )
-            for i in range(n-1):
-                ax.plot( xcoords[i:i+2], ycoords[i:i+2], zcoords[i:i+2], c=cm.binary(.3+.6*i/n) )
-            ax.plot( [xcoords[n-1]], [ycoords[n-1]], [zcoords[n-1]], "*", color='r' )
-            ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-            ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-            ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,1)
-            ax.zaxis.set_visible(False)
-            ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.set_xlim(self.minMax(xcoords))
-            ax.set_ylim(self.minMax(ycoords))
-            ax.set_zlim(self.minMax(zcoords))
-            ax.set_xlabel ( helpers.toLatex(self.coordinates[0],True,True) )
-            ax.set_ylabel ( helpers.toLatex(self.coordinates[1],True,True) )
-            ax.w_zaxis.line.set_lw(0.)
-            ax.grid(False)
-            ax.set_zticks([])
-            title = "MCMC walk, after %d steps" % n 
-            ax.text( 10., 7., 0., title, horizontalalignment="center",
-                   verticalalignment="bottom", transform = ax.transAxes )
-            self.save ( plt, ax, fig, 2, n, j )
-            #del fig
-            #del ax
+            # ax.set_ylim( yminmax )
+            zminmax = self.minMax(zcoords)
+            # print ( "zminmax=",zminmax )
+            #ax.set_zlim(zminmax)
+            if False: # abs(zminmax[1])<1e-5:
+                # print ( "make z axis invisible" )
+                self.ax.zaxis.set_visible(False)
+                self.ax.w_zaxis.line.set_lw(0.)
+                self.ax.set_zticks([])
+                self.ax.set_zlabel ( "" )
+                self.ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+                self.ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+            else:
+                self.ax.zaxis.set_visible(True)
+                self.ax.set_zlabel ( helpers.toLatex(self.coordinates[2],True,True) )
+                self.ax.zaxis.set_major_formatter(FormatStrFormatter('%d'))
 
-    def draw1d( self, n = 5 ):
-        fig = plt.figure( dpi=dpi )
-        ax = fig.add_subplot ( 111, projection="3d" )
-        for j in range(10):
-            fig = plt.figure( dpi=dpi )
-            ax = fig.add_subplot ( 111, projection="3d" )
-            xcoords,ycoords,zcoords = self.getCoords ( j, n )
-            for i in range(n):
-                ax.plot( xcoords[i:i+2], ycoords[i:i+2], zcoords[i:i+2], c=cm.binary(.3+.6*i/n) )
-            ax.plot( [xcoords[-1]], [ycoords[-1]], [zcoords[-1]], "*", color='r' )
-            ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-            ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-            ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-            ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,1)
-            ax.zaxis.set_visible(False)
-            ax.yaxis.set_visible(False)
-            ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
-            ax.set_xlim(self.minMax(xcoords))
-            ax.set_ylim(self.minMax(ycoords))
-            ax.set_zlim(self.minMax(zcoords))
-            ax.set_xlabel ( helpers.toLatex(self.coordinates[0],True,True) )
-            ax.w_zaxis.line.set_lw(0.)
-            ax.w_yaxis.line.set_lw(0.)
-            ax.grid(False)
-            ax.set_zticks([])
-            ax.set_yticks([])
-            title = "MCMC walk, after %d steps" % n 
-            ax.text( 10., 7., 0., title, horizontalalignment="center",
-                   verticalalignment="bottom", transform = ax.transAxes )
-            self.save ( plt, ax, fig, 1, n, j )
-            # del fig
-            # del ax
+            self.save ( plt, 3, n, j )
+            self.history.append ( p )
 
-    def draw ( self, n ):
-        cur = self.walk[n]
-        dim = len ( cur [ cur < 3000 ] )
-        # print ( "draw",n,self.walk[n],dim )
-        funcs = { 1:self.draw1d, 2:self.draw2d, 3:self.draw3d }
-        funcs[dim](n)
+    def run ( self ):
+        try:
+            while True:
+                self.next()
+        except StopIteration:
+            pass
+
+    def pprint ( self, *args ):
+        """ logging """
+        print ( "[draw] %s" % (" ".join(map(str,args))) )
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser( description="plot pics for movie" )
-    parser.add_argument("-f", "--file", type=str, 
+    parser.add_argument("-f", "--file", type=str,
             help='history file to use [history.pcl]', default="history.pcl")
-    parser.add_argument("-nmin", "--nmin", type=int, 
+    parser.add_argument("-nmin", "--nmin", type=int,
             help='first step [1]', default=1 )
-    parser.add_argument("-nmax", "--nmax", type=int, 
+    parser.add_argument("-nmax", "--nmax", type=int,
             help='last step [100]', default=100 )
     parser.add_argument("-clear", "--clear", action="store_true",
             help='clear the pics folder before starting' )
     args = parser.parse_args()
     if args.clear:
         subprocess.getoutput ("rm pics/*png" )
-    drawer = Drawer ( args.file )
-    for i in range(args.nmin,args.nmax):
-        drawer.draw ( i )
+    drawer = Drawer ( args.file, args.nmin, args.nmax )
+    print ( "draw the pics" )
+    drawer.run()
+    print ( "now animate the thing" )
+    animator = animation.ArtistAnimation( drawer.fig, drawer.history, interval=50, 
+                                          repeat_delay=3000, blit=True)
+    animator.save("movie.mp4")
