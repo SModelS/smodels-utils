6f2453562f62a52d7710cf6bf798054d744d5460
Wolfgang Waltenberger <wwaltenberger@hepgpu01.hephy.oeaw.ac.at>
2019-06-06 18:27:14 +0200

    starting to have a detailed option

--- combinations/whatsTheHiscore.py
+++ combinations/whatsTheHiscore.py
@@ -8,7 +8,7 @@ def discuss ( model, name ):
     print ( "Currently %7s Z is: %.3f [%d/%d unfrozen particles, %d predictions] " % \
             (name, model.Z, len(model.unFrozenParticles()),len(model.masses.keys()),len(model.bestCombo) ) )
 
-def detailedDiscussion ( model ):
+def discussBest ( model ):
     """ a detailed discussion of number 1 """
     p = 1. - stats.norm.cdf ( model.Z )
     print ( "Current best: %.3f, p=%.2g [%d/%d unfrozen particles, %d predictions] " % \
@@ -21,6 +21,8 @@ def main():
     argparser.add_argument ( '-f', '--picklefile',
             help='pickle file with hiscores [hiscore.pcl]',
             type=str, default="hiscore.pcl" )
+    argparser.add_argument ( '-d', '--detailed',
+            help='detailed descriptions', action="store_true" )
     args = argparser.parse_args()
     f=open(args.picklefile,"rb")
     walkers = pickle.load ( f )
@@ -33,7 +35,7 @@ def main():
         if c in names.keys():
             sc = names[c]
         if c==0:
-            detailedDiscussion ( model )
+            discussBest ( model )
         else:
             discuss ( model, sc )
 
