03e94286ae2f615db581ef91891963194fa31797
Wolfgang Waltenberger <wwaltenberger@hepgpu01.hephy.oeaw.ac.at>
2019-07-16 16:50:36 +0200

    tarball updates

--- slha/check_xsecs.sh
+++ slha/check_xsecs.sh
@@ -5,5 +5,5 @@
 
 for i in `ls T6WW*slha | shuf`; do
 	H=`cat $i | grep -i xsection | wc -l`
-	[ "$H" -eq "0" ] && { echo "[check_xsecs.sh] no xsecs found in $i"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N -e 50000; }
+	[ "$H" -eq "0" ] && { echo "[check_xsecs.sh] no xsecs found in $i"; ~/git/smodels/smodelsTools.py xseccomputer -f $i -8 -P -N -c 10 -e 50000; }
 done
