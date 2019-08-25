47b7e3b2ebbaf0c83697ae59e9472bf5667fe705
Wolfgang Waltenberger <wwaltenberger@hepgpu01.hephy.oeaw.ac.at>
2019-08-25 18:39:28 +0200

    sync

--- combinations/trimmer.py
+++ combinations/trimmer.py
@@ -48,7 +48,7 @@ class Trimmer:
         self.model.Z = -23.
         self.model.predict( strategy=self.strategy, keep_meta = True )
         print ( "[trimmer] Z=%.2f, old=%.2f, %d predictions, experimental=%d" % ( self.model.Z, origZ, len(self.model.bestCombo), runtime._experimental ) )
-        if oriZ > 0. and abs ( origZ - self.model.Z ) / origZ > 0.001:
+        if origZ > 0. and abs ( origZ - self.model.Z ) / origZ > 0.001:
             print  ( "[trimmer] error!! Zs do not match! Will not save" )
             args.save = False
         contributions = {}
