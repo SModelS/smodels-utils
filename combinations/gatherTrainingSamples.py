fa3fae782ea884eaba0dc805a1bbaf0d8fbbc373
Wolfgang Waltenberger <wwaltenberger@hepgpu01.hephy.oeaw.ac.at>
2019-09-10 11:37:55 +0200

    pickler of training samples

---
+++ combinations/gatherTrainingSamples.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import gzip, glob, pickle
+
+def write():
+    files = glob.glob ( "training_*.gz" ) 
+    All = []
+    for fname in files:
+        with gzip.open ( fname, "r" ) as f:
+            lines = f.readlines()
+            for line in lines:
+                All.append ( line )
+    with open ( "training.pcl", "wb" ) as g:
+        for line in All:
+            pickle.dump ( eval(line), g, pickle.HIGHEST_PROTOCOL )
+    print ( "[gatherTrainingSamples] wrote %d lines" % len(All) )
+
+def read():
+    with open ( "training.pcl", "rb" ) as g:
+        ctr=0
+        try:
+            while True:
+                d = pickle.load ( g )
+                if False: # ctr == 0:
+                    print ( "d", d )
+                ctr+=1
+        except EOFError:
+            pass
+        print ( "[gatherTrainingSamples] found %d lines" % ctr )
+
+
+if __name__ == "__main__":
+    import argparse
+    argparser = argparse.ArgumentParser(
+            description='collect the training samples into one big pickle file' )
+    argparser.add_argument ( '-r', '--read',
+            help='read and check training.pcl, instead of producing it', 
+            action="store_true" )
+    args = argparser.parse_args()
+    if args.read:
+        read()
+    else:
+        write()
