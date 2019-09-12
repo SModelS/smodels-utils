7c6654d84796008266c0cb713e4acaffa14e73f8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-09-12 18:22:54 +0200

    score plotter update

--- combinations/plotScores.py
+++ combinations/plotScores.py
@@ -6,13 +6,18 @@ def main():
     with open("scores.csv") as f:
         lines=f.readlines()
     x,y=[],[]
+    epoch = 0
     for l in lines:
         t = list ( map ( float, l.split(",") ) )
         x.append(t[1])
         y.append(t[2])
-    plt.scatter ( x, y )
+        epoch = t[0]
+    plt.scatter ( x, y, s=.1 )
+    plt.plot ( [0,2.8], [0,2.8], 'r--' )
+    plt.title ( "true versus predicted Z scores, epoch %d" % epoch )
     plt.xlabel ( "predicted" )
     plt.ylabel ( "true" )
+    plt.savefig ( "scatter%d.png" % epoch )
     plt.savefig ( "scatter.png" )
 
 
