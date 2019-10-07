2deeee983f8338510289a3e72dcc0e629bc56981
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-10-07 20:48:08 +0200

    t6gamma fix

--- slha/fixT6Gamma.py
+++ slha/fixT6Gamma.py
@@ -11,15 +11,20 @@ def main():
         with open ( fl, "rt" ) as f:
             lines = f.readlines()
         with open ( fl, "wt" ) as f:
+            pastMasses=False
             for line in lines:
-                f.write ( line )
-                if "1000012" in line:
+                if "00000" in line and "# ~" in line and pastMasses and not "DECAY" in line:
+                    pass
+                else:
+                    f.write ( line )
+                if "1000037" in line:
                     f.write ( "   2000012     1.00000000E+05   # ~nu_eR\n" )
                     f.write ( "   2000014     1.00000000E+05   # ~nu_muR\n" )
                     f.write ( "   2000016     1.00000000E+05   # ~nu_tauR\n" )
                     f.write ( "   1000045     1.00000000E+05   # ~chi_50\n" )
                     f.write ( "   45     1.00000000E+05   # ? \n" )
                     f.write ( "   46     1.00000000E+05   # ? \n" )
+                    pastMasses=True
     cmd = "tar czvf T6Gamma.tar.gz T6Gamma*slha" 
     subprocess.getoutput ( cmd )
 
