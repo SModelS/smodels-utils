de065b8a345e2bb2662e017bdf615b900eaa0395
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-10-17 09:31:28 +0200

    import missing

--- combinations/walkingWorker.py
+++ combinations/walkingWorker.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 def main( nmin, nmax, cont, dbpath = "../../smodels-database/" ):
-    import sys
+    import sys, os
     sys.path.insert(0,"/mnt/hephy/pheno/ww/git/smodels/")
     sys.path.insert(0,"/mnt/hephy/pheno/ww/git/smodels-utils/")
     sys.path.insert(0,"/mnt/hephy/pheno/ww/git/smodels-utils/combinations/")
-    # import os
     # os.chdir ( "/mnt/hephy/pheno/ww/git/smodels-utils/combinations/" )
     pfile, states = None, None
     if cont.lower() not in [ "none", "" ]:
