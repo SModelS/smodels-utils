ab68b983ba45ed3f2faba6965227c8e333ffa19b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-11-07 17:50:45 +0100

    updated xsecs

--- slha/removeWronglyOrdered.py
+++ slha/removeWronglyOrdered.py
@@ -2,19 +2,21 @@
 
 """ remove all slha files with the wrong order """
 
-import glob, copy, subprocess
+import glob, copy, subprocess, argparse
 
 def main():
     files = glob.glob("T*slha")
+    pretend = False
     for f in files:
         tokens = f.split("_")
         masses = list ( map ( int, tokens[1:4] ) )
         smasses = copy.deepcopy( masses)
         smasses.sort ( reverse=True )
         issorted = (smasses == masses )
-        print ( masses, issorted )
         if not issorted:
+            print ( "removing", masses )
             cmd = "rm %s" % f
-            subprocess.getoutput ( cmd )
+            if not pretend:
+                subprocess.getoutput ( cmd )
 
 main()
