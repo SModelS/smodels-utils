cb2153e4bc322a13051da345c955196dfdcfb7c5
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-11-28 16:16:54 +0100

    trying to fix slurm invokations

---
+++ clip/cancel_jobs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import subprocess, argparse
+
+def main():
+    import argparse
+    argparser = argparse.ArgumentParser(description="cancel certain jobs")
+    argparser.add_argument ( '-p','--pending', help='cancel pending jobs',
+                             action="store_true" )
+    argparser.add_argument ( '-a','--all', help='cancel all jobs',
+                             action="store_true" )
+    args=argparser.parse_args()
+    grp = "| grep QOSMax"
+    if args.all:
+        grp = ""
+    if args.pending:
+        grp = "PENDING"
+    a= subprocess.getoutput ( "slurm q %s" % grp )
+    print ( "cancelling", end=" " )
+    for line in a.split("\n" ):
+        jobid = line[:8].strip()
+        print ( "%s" % jobid, end=" " )
+        subprocess.getoutput ( "scancel %s" % jobid )
+    print ( "done." )
+
+main()
--- clip/run_walker.sh
+++ clip/run_walker.sh
@@ -4,4 +4,4 @@ ml --latest singularity
 
 cd /mnt/hephy/pheno/ww/git/smodels-utils/clip
 
-singularity shell -c -B /tmp,/run,/scratch -s /mnt/hephy/pheno/ww/git/smodels-utils/clip/walkingWorker.py -H /mnt/hephy/pheno/ww /mnt/hephy/pheno/current.simg
+singularity shell -c -B /tmp,/run,/scratch -s walkingWorker.py -H /mnt/hephy/pheno/ww /mnt/hephy/pheno/current.simg
