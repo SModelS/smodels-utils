2c6f1851ddef9a51b1a74cc1a34a5ba47c5fdb9e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2019-12-02 19:55:41 +0100

    count jobs updated

--- clip/count_jobs.py
+++ clip/count_jobs.py
@@ -3,9 +3,12 @@
 import subprocess
 
 def main():
-    a = subprocess.getoutput ( "slurm q | grep PEND | wc -l" )
-    print ( "pending", a )
-    a = subprocess.getoutput ( "slurm q | grep RUNNING | wc -l" )
-    print ( "running", a )
+    pend = subprocess.getoutput ( "slurm q | grep PEND | wc -l" )
+    print ( "pending", pend )
+    running = subprocess.getoutput ( "slurm q | grep RUNNING | wc -l" )
+    print ( "running", running )
+    remaining = subprocess.getoutput ( "slurm q | grep -v PEND | grep -v RUNNING | grep -v NODELIST | wc -l" )
+    if int(remaining)>0:
+        print ( "remaining", remaining )
 
 main()
