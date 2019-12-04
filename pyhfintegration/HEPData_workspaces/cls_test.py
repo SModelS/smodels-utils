f959a4b4839ab26c6391c7faf6a8e7696cfe59ca
Ga0l <gaelalguero@gmail.com>
2019-12-04 17:28:40 +0100

    testing the json merger (merge.py) that will be used for combining regions A, B and C, seems to work (returns CLs)

--- pyhfintegration/HEPData_workspaces/cls_test.py
+++ pyhfintegration/HEPData_workspaces/cls_test.py
@@ -3,11 +3,11 @@ import json
 
 # Opening main workspace file of region A
 wspec = json.load(open("RegionA/BkgOnly.json"))
-ws = pyhf.Workspace(wspec)
+w = pyhf.Workspace(wspec)
 
 # Opening a random patch to be applied on the previous file
-patch = json.load(open("RegionA/patch.sbottom_900_250_60.json"))
-BSMpatch = json.load(open("patch.bsm.json"))
+patch = json.load(open("RegionA/patch.sbottom_900_250_60.json", "r"))
+BSMpatch = json.load(open("patch.bsm.json", "r"))
 
 # Same modifiers_settings as those use when running the 'pyhf cls' command line
 msettings = {
@@ -17,11 +17,11 @@ msettings = {
 
 patches = [patch, BSMpatch] # list of patches that will be "successively" applied to the main worskpace file
 
-mdl = ws.model(measurement_name=None, patches=patches, modifiers_settings=msettings)
+p = w.model(measurement_name=None, patches=patches, modifiers_settings=msettings)
 
 test_poi = 1.0 # Value of the parameter of interest (POI) which here is the signal strength modifier
 
-result = pyhf.utils.hypotest(test_poi, ws.data(mdl), mdl, qtilde=True, return_expected_set = True)
+result = pyhf.utils.hypotest(test_poi, w.data(p), p, qtilde=True, return_expected_set = True)
 print("CLs_exp")
 for clsexp in result[1].tolist():
     print(clsexp[0])
