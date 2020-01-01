10f8da8efd6e75ad4e49b5de76183a9d9edbc21a
walten <wolfgang.waltenberger@gmail.com>
2020-01-01 15:38:31 +0100

    starting plotting scripts

--- combinations/Zofp.py
+++ combinations/Zofp.py
@@ -19,13 +19,11 @@ def setup():
     return rundir
 
 
-def produce():
+def produce( pid=1000022, nevents = 100000 ):
     import hiscore
     rundir = setup()
     picklefile =rundir + "hiscore.pcl" 
     hi = hiscore.Hiscore( 0, False, picklefile = picklefile )
-    pid = 1000022
-    nevents = 100000
     model = hi.trimmed[0]
     mass = model.masses[pid]
     Zs = {}
@@ -37,17 +35,46 @@ def produce():
     import pickle
     with open ( "scan.pcl", "wb" ) as f:
         pickle.dump ( Zs, f )
+        pickle.dump ( mass, f )
+        pickle.dump ( nevents, f )
         f.close()
 
-def draw():
+def draw( pid= 1000022 ):
     from matplotlib import pyplot as plt
+    import helpers
     import pickle
-    with open ( "saved.pcl", "rb" ) as f:
+    with open ( "scanM%s.pcl" % pid, "rb" ) as f:
         Zs = pickle.load( f )
-    plt.plot ( Zs.keys(), Zs.values() )
-    plt.savefig ( "irst.png" )
+    x = list(Zs.keys())
+    y = list(Zs.values())
+    pname = helpers.toLatex ( pid, addDollars=True )
+    plt.plot ( x, y )
+    plt.ylabel ( "Z" )
+    plt.title ( "Significance Z=Z(%s)" % pname )
+    plt.xlabel ( "m(%s) [GeV]" % pname )
+    plt.savefig ( "M%d.png" % pid )
 
 if __name__ == "__main__":
     import argparse
-    produce()
-    # draw()
+    argparser = argparse.ArgumentParser(
+            description='script that takes care of the Z(m) plots' )
+    argparser.add_argument ( '-p', '--pid',
+            help='pid to consider. If zero, then consider a predefined list [1000022]',
+            type=int, default=1000022 )
+    argparser.add_argument ( '-P', '--produce',
+            help='produce the pickle file',
+            action="store_true" )
+    argparser.add_argument ( '-d', '--draw',
+            help='produce the plot',
+            action="store_true" )
+    args = argparser.parse_args()
+    pids = args.pid
+    if args.produce:
+        if pids > 0:
+            produce( pids )
+        else:
+            pids = [ 1000022, 1000021, 1000006, 2000006, 1000024 ]
+            for pid in pids:
+                produce ( pid )
+    if args.draw:
+        draw( args.pid )
