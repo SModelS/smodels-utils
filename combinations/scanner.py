d79c9ce3d37a2c8c6c84d029bf90245c9a138cdf
walten <wolfgang.waltenberger@gmail.com>
2020-01-15 12:28:54 +0100

    plotting updated

--- combinations/scanner.py
+++ combinations/scanner.py
@@ -133,17 +133,17 @@ def draw( pid= 1000022, interactive=False ):
     for i in x:
         y.append ( Zs[i] )
     pname = helpers.toLatex ( pid, addDollars=True )
-    plt.plot ( x, y )
+    plt.plot ( x, y, label="Z(%s), %d events" % ( pname, nevents ) )
     ymax = max(y)
     imax = y.index ( ymax )
     xmax = x[imax]
-    plt.plot ( [ cmass, cmass ], [ 0.98*ymax, 1.01*ymax ], label="protomodel, Z(%d GeV)=%.2f" % (cmass, Zs[cmass] ) )
-    plt.plot ( [ xmax, xmax ], [ 0.98*ymax, 1.01*ymax ], label="max(x), Z(%d GeV)=%.2f" % (xmax, ymax ) )
+    plt.scatter ( [ xmax ], [ ymax ], label="max(x), Z(%d GeV)=%.2f" % (xmax, ymax ), s=100, c="g", marker="+", zorder=1 )
+    plt.scatter ( [ cmass ], [ Zs[cmass] ], label="protomodel, Z(%d GeV)=%.2f" % (cmass, Zs[cmass] ), marker="*", s=100, c="r", zorder=2 )
     plt.ylabel ( "Z" )
     plt.title ( "Significance Z=Z(%s)" % pname )
     plt.legend()
     plt.xlabel ( "m(%s) [GeV]" % pname )
-    plt.text ( .9*min(x)+.1*(max(x)-min(x)), 1.*max(y), "%d events" % nevents )
+    # plt.text ( .9*min(x)+.1*(max(x)-min(x)), 1.*max(y), "%d events" % nevents )
     figname = "M%d.png" % pid 
     print ( "[scanner] creating %s" % figname )
     plt.savefig ( figname )
