d6d82a7e193f624cbda7d3e23f539d998d44730b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2020-01-22 15:31:20 +0100

    /neutr added

--- combinations/fetch_hiscore.py
+++ combinations/fetch_hiscore.py
@@ -7,6 +7,8 @@ if len(sys.argv)>1 and "scan" in sys.argv[1]:
 	f= [ "scanM\*.pcl", "mp\*.pcl", "ssm\*.pcl" ]
 if len(sys.argv)>1 and "copy" in sys.argv[1]:
 	f= [ "hiscoreCopy.pcl" ]
+if len(sys.argv)>1 and "ssm" in sys.argv[1]:
+	f= [ "ssm\*.pcl" ]
 for i in f:
     cmd="scp wolfgan.waltenberger@clip-login-1:/mnt/hephy/pheno/ww/rundir/%s ." % i
     print ( cmd )
