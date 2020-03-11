6cca121a41170e4415e6c61d69fb814274ad42a6
Wolfgang Waltenberger <wwaltenberger@hepgpu01.hephy.oeaw.ac.at>
2020-03-11 11:54:17 +0100

    bakery update

--- EM_Creator/mg5Wrapper.py
+++ EM_Creator/mg5Wrapper.py
@@ -215,11 +215,14 @@ class MG5Wrapper:
         files = glob.glob ( ".lock*" )
         t = time.time()
         for f in files:
-            ts = os.stat(f).st_mtime
-            dt = ( t - ts ) / 60. / 60.
-            if dt > hours:
-                self.msg ( "removing old lock %s [%d hrs old]" % ( f, int(dt) ) )
-                subprocess.getoutput ( "rm -f %s" % f )
+            try:
+                ts = os.stat(f).st_mtime
+                dt = ( t - ts ) / 60. / 60.
+                if dt > hours:
+                    self.msg ( "removing old lock %s [%d hrs old]" % ( f, int(dt) ) )
+                    subprocess.getoutput ( "rm -f %s" % f )
+            except:
+                pass
 
     def exe ( self, cmd, masses="" ):
         sm = ""
