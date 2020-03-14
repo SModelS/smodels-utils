ce278963220ef5c51343f9c06fd73d99c129c5c6
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2020-03-14 12:47:51 +0100

    move to scratch

---
+++ clip/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -rf old*pcl .old* .cur*slha .tri*slha .scan*slha RUN* walker*log .s*slha
---
+++ clip/run_bakery_template.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ml --latest singularity
+
+cd /users/wolfgan.waltenberger/git/projects/singularity/
+
+singularity shell -c -B /tmp,/run,/scratch -s /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/clip/@@SCRIPT@@ -H /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/EM_Creator /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
+#singularity shell -c -B /tmp,/run,/scratch -s /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/clip/@@SCRIPT@@ -H /mnt/hephy/pheno/ww/git/smodels-utils/EM_Creator /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
