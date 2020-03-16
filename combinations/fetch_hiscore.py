5eee7bdd235d4b24d8c1f07f40d1821690aa3851
walten <wolfgang.waltenberger@gmail.com>
2020-03-16 21:28:48 +0100

    check sysargv1

--- combinations/fetch_hiscore.py
+++ combinations/fetch_hiscore.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
-import subprocess, sys
+import subprocess, sys, copy
 
 f= [ "hiscore.pcl" ]
-if len(sys.argv)>1 and "scan" in sys.argv[1]:
-	f= [ "scanM\*.pcl", "mp\*.pcl", "ssm\*.pcl" ]
-if len(sys.argv)>1 and "copy" in sys.argv[1]:
-	f= [ "hiscoreCopy.pcl" ]
-if len(sys.argv)>1 and "ssm" in sys.argv[1]:
-	f= [ "ssm\*.pcl" ]
+if len(sys.argv)>1:
+    oldf = copy.deepcopy(f)
+    if "scan" in sys.argv[1]:
+	    f= [ "scanM\*.pcl", "mp\*.pcl", "ssm\*.pcl" ]
+    if "states" in sys.argv[1]:
+	    f= [ "states.pcl" ]
+    if "copy" in sys.argv[1]:
+	    f= [ "hiscoreCopy.pcl" ]
+    if "ssm" in sys.argv[1]:
+	    f= [ "ssm\*.pcl" ]
+    if f == oldf:
+        print ( "[fetch_hiscore] do not understand what you mean with '%s'. I only know of 'scan', 'states', 'copy', 'ssm'." % sys.argv[1] )
+        sys.exit()
 for i in f:
     cmd="scp wolfgan.waltenberger@clip-login-1:/scratch-cbe/users/wolfgan.waltenberger/rundir/%s ." % i
     print ( cmd )
