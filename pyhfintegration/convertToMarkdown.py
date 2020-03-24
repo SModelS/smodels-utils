bbaef2baa374e4c9a6bddac6436829d7e7c7e634
Ga0l <gaelalguero@gmail.com>
2020-03-24 11:36:23 +0100

    updated README with grid scan

---
+++ pyhfintegration/convertToMarkdown.py
@@ -0,0 +1,15 @@
+from outputGridScan import outputDataDict
+# Keys definition
+stau = "m(stau)"
+chi = "m(chi1)"
+off = "officialUL"
+pyhf = "pyhfUL"
+dis = "discrepancy"
+dash = "---"
+markdownTab = "|{:^15s}|{:^15s}|{:^22s}|{:^18s}|{:^18s}|\n".format("m(stau) [GeV]", "m(chi1) [GeV]", "official 95% UL [pb]", "pyhf 95% UL [pb]", "discrepancy [%]")
+markdownTab += "|{0:^15s}|{0:^15s}|{0:^22s}|{0:^18s}|{0:^18s}|\n".format(dash)
+formatter = "|{:<15.0f}|{:<15.0f}|{:<22.4f}|{:<18.4f}|{:<18.1f}|\n"
+for d in outputDataDict:
+    markdownTab += formatter.format(d[stau], d[chi], d[off], d[pyhf], d[dis]*100)
+with open("markdownTab.txt", "w") as output:
+    output.write(markdownTab)
