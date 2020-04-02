0526d9a53ca484bbfb642af46827c86897495b6e
Ga0l <gaelalguero@gmail.com>
2020-04-02 16:34:37 +0200

    results of grid scan with aux table in ULtab-aux.txt

--- pyhfintegration/convertToMarkdown.py
+++ pyhfintegration/convertToMarkdown.py
@@ -1,4 +1,4 @@
-from outputGridScan import outputDataDict
+from outputGridScan_aux import outputDataDict
 import sys
 # Keys definition
 stau = "m(stau)"
@@ -12,5 +12,5 @@ markdownTab += "|{0:^15s}|{0:^15s}|{0:^22s}|{0:^18s}|{0:^18s}|\n".format(dash)
 formatter = "|{:<15.0f}|{:<15.0f}|{:<22.5f}|{:<18.5f}|{:<18.1f}|\n"
 for d in outputDataDict:
     markdownTab += formatter.format(d[stau], d[chi], d[off], d[pyhf], d[dis]*100)
-with open("markdownTab.txt", "w") as output:
+with open("ULtab-aux.txt", "w") as output:
     output.write(markdownTab)
