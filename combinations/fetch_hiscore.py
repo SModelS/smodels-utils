0d70c24eeec3c5c19b6faec3da8ba7ed81549fa8
walten <wolfgang.waltenberger@gmail.com>
2020-05-09 14:05:24 +0200

    argparse

--- combinations/fetch_hiscore.py
+++ combinations/fetch_hiscore.py
@@ -1,34 +1,50 @@
 #!/usr/bin/env python3
 
-import subprocess, sys, copy
+""" simple tool to fetch all sorts of files from clip """
 
-f= [ "hiscore.pcl" ]
-if len(sys.argv)>1:
-    oldf = copy.deepcopy(f)
-    if "scan" in sys.argv[1]:
-	    f= [ "scanM*.pcl", "mp*.pcl", "ssm*.pcl" ]
-    if "mp" in sys.argv[1]:
-	    f= [ "mp*.pcl" ]
-    if "png" in sys.argv[1]:
-	    f= [ "*.png" ]
-    if "states" in sys.argv[1]:
-	    f= [ "states.pcl" ]
-    if "copy" in sys.argv[1]:
-	    f= [ "hiscoreCopy.pcl" ]
-    if "2" in sys.argv[1]:
-	    f= [ "hiscore2.pcl" ]
-    if "ssm" in sys.argv[1]:
-	    f= [ "ssm*.pcl" ]
-    if "pmodel" in sys.argv[1]:
-	    f= [ "pmodel?.py" ]
-    if "all" in sys.argv[1]:
-	    f= [ "scanM*.pcl", "mp*.pcl", "ssm*.pcl", "*png", "ssm*.pcl", "pmodel?.py"  ]
-    if f == oldf:
-        print ( "[fetch_hiscore] do not understand what you mean with '%s'. I only know of 'scan', 'states', 'copy', 'ssm', 'png', 'pmodel', 'all'." % sys.argv[1] )
-        sys.exit()
-for i in f:
-    cmd="scp wolfgan.waltenberger@clip-login-1:/scratch-cbe/users/wolfgan.waltenberger/rundir/%s ." % i
-    print ( cmd )
-    subprocess.run(cmd.split(" "), stderr=sys.stderr, stdout=sys.stdout)
-    #out = subprocess.getoutput ( cmd )
-    #print ( out )
+import subprocess, sys, copy, argparse
+
+def fetch ( files ):
+    """ fetch the files in list """
+    files = set ( files ) ## remove dupes
+    for i in files:
+        cmd="scp wolfgan.waltenberger@clip-login-1:/scratch-cbe/users/wolfgan.waltenberger/rundir/%s ." % i
+        print ( cmd )
+        ret = subprocess.run(cmd.split(" "), stderr=sys.stderr, stdout=sys.stdout)
+
+def main():
+    import argparse
+    argparser = argparse.ArgumentParser(
+            description='file fetching utility, fetches from clip' )
+    argparser.add_argument ( '-a', '--all', help='all files', action="store_true" )
+    argparser.add_argument ( '--scan', help='files from scan', action="store_true" )
+    argparser.add_argument ( '--states', help='the states', action="store_true" )
+    argparser.add_argument ( '--pmodels', help='the pmodels', action="store_true" )
+    argparser.add_argument ( '--png', help='the png files', action="store_true" )
+    argparser.add_argument ( '--ssms', help='the ssm files', action="store_true" )
+    argparser.add_argument ( '--copy', help='the copy of the hiscore file', 
+                             action="store_true" )
+    argparser.add_argument ( '-2', '--two', help='the second hiscore file', 
+                             action="store_true" )
+    args = argparser.parse_args()
+    files= [ "hiscore.pcl" ]
+    if args.scan:
+        files = [ "scanM*.pcl", "mp*.pcl", "ssm*.pcl" ]
+    if args.states:
+        files = [ "states.pcl" ]
+    if args.two:
+        files = [ "hiscore2.pcl" ]
+    if args.copy:
+        files = [ "hiscoreCopy.pcl" ]
+    if args.pmodels:
+        files = [ "pmodel?.py" ]
+    if args.png:
+        files = [ "*.png" ]
+    if args.ssms:
+        files = [ "ssm*.pcl" ]
+    if args.all:
+        files = [ "scanM*.pcl", "mp*.pcl", "ssm*.pcl", "*png", "ssm*.pcl", "pmodel?.py"  ]
+    fetch ( files )
+
+if __name__ == "__main__":
+    main()
