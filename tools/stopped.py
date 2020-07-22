63e982f16d39a08e571f1fd1ac9dce7d2f1850fd
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2020-07-22 14:36:36 +0200

    adding counting script

---
+++ tools/stopped.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def main():
+    import glob
+    files = glob.glob("walker*log")
+    steps = {}
+    for f in files:
+        nr = int ( f.replace("walker","").replace(".log","") )
+        h = open ( f, "rt" )
+        lines = h.readlines()
+        h.close()
+        for line in lines[::-1]:
+            if "Step" in line:
+                laststep = line[line.find("Step")+5:]
+                for c in [ "/", ":", " has", " " ]:
+                    if c in laststep:
+                        laststep = laststep[:laststep.find(c)]
+                laststep = int ( laststep.strip() )
+                #print ( nr, laststep )
+                steps[nr]=laststep
+                break
+    keys = list ( steps.keys() )
+    keys.sort()
+    for k in keys:
+        print ( k, steps[k] )
+    print ( f"we have {len(keys)} entries" )
+
+main()
