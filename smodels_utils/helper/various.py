3f0aa402898872fb2c496d0895f28ba8ddbec7e9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2020-08-20 12:57:23 +0200

    adding removeFastlim function

--- smodels_utils/helper/various.py
+++ smodels_utils/helper/various.py
@@ -10,8 +10,19 @@
 
 import os, sys
 
+def removeFastlim ( database ):
+    """ given a "Database" object, remove the fastlim results """
+    newRes = []
+    for expRes in database.expResultList:
+        contact = "unknown"
+        if hasattr ( expRes.globalInfo, "contact" ):
+            contact = expRes.globalInfo.contact
+        if not "fastlim" in contact:
+                newRes.append ( expRes )
+    database.expResultList = newRes
+
 def getPathName ( dbpath, analysis, valfile ):
-    """ get the path name, given a dbpath, an analysis id, and a valfile name 
+    """ get the path name, given a dbpath, an analysis id, and a valfile name
         potentially with wildcards """
     import glob
     if not valfile.endswith(".py"): valfile += ".py"
