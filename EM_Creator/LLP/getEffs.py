bd9a832c9c7c696f1ba79b4269f7f2a95d1d0a03
Andre Lessa <lessa.a.p@gmail.com>
2020-08-20 23:09:17 -0300

    Changing getEffs

--- EM_Creator/LLP/getEffs.py
+++ EM_Creator/LLP/getEffs.py
@@ -4,13 +4,13 @@
 #and extract an efficiency map for all the points, including the effect of finite width.
 
 import sys
-from configparser import SafeConfigParser,ExtendedInterpolation
+from configParserWrapper import ConfigParserExt
 import logging
 import time
 import os
 import multiprocessing
 import numpy as np
-import math,glob
+import math,glob,tarfile
 
 
 
@@ -62,7 +62,7 @@ class Particle(object):
             mass = np.sqrt(np.inner(self.fourMom(),self.fourMom()))
             return mass
 
-def  getEffForEvent(event,xEffs=None):
+def getEffForEvent(event,widths=[0.0],detectorLength=1.0):
     """
     Compute the efficiency for the event for a given
     list of x-values (xEffs), where x = L/ctau.
@@ -72,7 +72,7 @@ def  getEffForEvent(event,xEffs=None):
     :param xEffs: list of float with the effective values for L/ctau to rescale the events for.
                   If None, will return the unrescaled efficiency (zero width).
 
-    :return: numpy array with a list of rescaled efficiencies for each value of x in xEff.
+    :return: numpy array with a
     """
 
 
@@ -85,57 +85,45 @@ def  getEffForEvent(event,xEffs=None):
         logger.error("Can not handle more than 2 particles per event")
         raise ValueError("%s" %str(event))
 
-
-    #Trigger probabilities and error:
-    ProbTrigger = [0.]*2
-    #Online probabilities and error for each signal region/mass cut:
-    ProbOnline = [0.]*2
+    #Signal region definitions and min reconstructed mass
+    SRs = {'c000' : 0,'c100' : 100.0,'c200' : 200.0 ,'c300' :300.0}
+    #Trigger probabilities for each signal region/particle:
+    ProbTrigger = np.array([0.]*npart)
+    #Online probabilities and error for each signal region/particle:
+    ProbOnline = dict([[sr,np.array([0.]*npart)] for sr in SRs])
     #Compute zero-width efficiencies:
-    mrec = [0,100.,200.,300.]     #Use only maximum mass cut
-    for i,p in enumerate(event):
-        irec = int(math.floor(p.mass*0.6/100.))
-        irec = min(irec,len(mrec)-1)
-        ProbTrigger[i] = p.effs[0]
-        ProbOnline[i] = p.effs[irec+1]
+    for i,hscp in enumerate(event):
+        ProbTrigger[i] = hscp.effs['trigger'] #Same trigger eff for all SRs
+        for sr,mrec in SRs.items():
+            if hscp.mass*0.6 < mrec:
+                continue
+            ProbOnline[sr][i] = hscp.effs[sr]
 
-    #Efficiency if both particles decay outside:
-    eff12 = ProbTrigger[0]*(1-ProbTrigger[1]) + ProbTrigger[1]*(1-ProbTrigger[0]) + ProbTrigger[0]*ProbTrigger[1]
-    eff12 *= ProbOnline[0]*(1-ProbOnline[1]) + ProbOnline[1]*(1-ProbOnline[0]) + ProbOnline[0]*ProbOnline[1]
+    #Create array to store efficiencies for each x (width) value
+    effs = np.zeros(len(widths),dtype=[('width',float)]+[(sr,float) for sr in SRs])
 
-    #Efficiency if only the first one decays outside:
-    eff1only = ProbTrigger[0]*ProbOnline[0]
-    #Efficiency if only the second one decays outside:
-    eff2only = ProbTrigger[1]*ProbOnline[1]
+    #Loop over x values and compute the probability of reconstructing at least one HSCP:
+    for i,w in enumerate(widths):
+        effs['width'][i] = w
+        x = detectorLength*w/1.975e-16 #Effective length
+        #Compute fraction of HSCPs which decay outside the detector:
+        probDecay = np.array([math.exp(-hscp.mass*x/hscp.P()) for hscp in event])
+        for sr in SRs:
+            #Compute probability for reconstructing each HSCP:
+            probTag = probDecay*ProbTrigger*ProbOnline[sr]
+            #Probability for reconstructing at least one HSCP (1 - probability for missing all):
+            probFinal = 1.0 - np.prod(1.0-probTag)
+            #Store result:
+            effs[sr][i] = probFinal
 
-    if xEffs is None:
-        return eff12
+    return effs
 
-    #Now comput the fraction of particles which survive each part of the detector.
-    #For each particle, get fraction which crossed a distance L:
-    FL = []
-    for x in xEffs:
-        evtFL = [0.]*2
-        for ip,particle in enumerate(event):
-            evtFL[ip] = math.exp(-particle.mass*x/particle.P()) #Compute fraction of long-lived
-        FL.append(tuple(evtFL))
-
-    FL = np.array(FL,dtype=[('F1',float),('F2',float)])
-    F12 = FL['F1']*FL['F2']
-    F1only = FL['F1']*(1-FL['F2'])
-    F2only = FL['F2']*(1-FL['F1'])
-
-
-    eventEff = F12*eff12 + F1only*eff1only + F2only*eff2only
-
-    return eventEff
-
-def getEventsFrom(infile,useEffs=False):
+def getEventsFrom(infile, effLabels = None):
     """
     Reads a simplified LHE file and returns a list of events.
     Each event is simply a list of TParticle objects.
 
     :param infile: Path to the input file
-    :param useEffs: If useEffs = True, includes the event efficiencies. If False, set efficiencies to 1.
 
     :return: list of events (e.g. [ [ TParticle1, TParticle2,..], [ TParticle1,...]  ])
     """
@@ -143,6 +131,10 @@ def getEventsFrom(infile,useEffs=False):
     if not os.path.isfile(infile):
         logger.error("File %s not found" %infile)
         return []
+
+    #Define labels for efficiencies is the order appearing in the event file:
+    if effLabels is None:
+        effLabels = ['trigger','c000','c000_err','c100','c100_err','c200','c200_err','c300','c300_err']
 
     f = open(infile,'r')
     events = f.read()
@@ -169,12 +161,7 @@ def getEventsFrom(infile,useEffs=False):
                 name = "~hscp"
                 charge = -1.
             particle = Particle(pdg=pdg,triMomentum=triMomentum,energy=energy,mass=mass,name=name,charge=charge)
-            if useEffs:
-                particle.effs = [l[i] for i in range(6,15,2)]
-                particle.effErrors = [l[i] for i in range(7,16,2)]
-            else:
-                particle.effs = [1.,1.] #Offline and Online probabilities
-                particle.effErrors = [0.,0.] #Offline and Online probabilities errors
+            particle.effs = dict([[label,l[i]] for i,label in enumerate(effLabels)])
 
             particles.append(particle)
 
@@ -182,7 +169,7 @@ def getEventsFrom(infile,useEffs=False):
 
     return eventList
 
-def getEffsFor(lheFile,widths,detectorLength,outFolder):
+def getEffsFor(lheFile,nHSCP,widths,detectorLength,outFolder):
     """
     Compute the efficiencies for a list of widths
     using the events and efficiencies stored in the lheFile (for zero width).
@@ -201,51 +188,48 @@ def getEffsFor(lheFile,widths,detectorLength,outFolder):
 
     inputFile = lheFile
     if lheFile.endswith(".tar.gz"):
-        tar = tarfile.open(lheFile, "r:gz")
-        tar.extractall()
-        tar.close()
-        inputFile = lheFile.replace('.tar.gz','')
-    elif fname.endswith(".tar"):
-        tar = tarfile.open(fname, "r:")
-        tar.extractall()
-        tar.close()
-        inputFile = lheFile.replace('.tar','')
+        with tarfile.open(lheFile, "r:gz") as tar:
+            inputFile = lheFile.replace('.tar.gz','')
+            tar.extract(os.path.basename(inputFile),
+                            path=os.path.dirname(lheFile))
 
-    events = getEventsFrom(inputFile, useEffs=True)
-    invCtau = np.array(widths)/1.975e-16
-    xEffs = invCtau*detectorLength
+    events = getEventsFrom(inputFile)
 
-    zeroEff = 0.
-    rescaledEff = np.array([0.]*len(xEffs))
-    zeroEffErr = 0.
-    rescaledEffErr = np.array([0.]*len(xEffs))
+    columns = ['width','c000','c100','c200','c300','c000_err','c100_err','c200_err','c300_err']
+    effs = np.zeros(len(widths),dtype=[(c,float) for c in columns])
     for event in events:
         if not event:
             continue  #Skip events without HSCPs
 
-
-        #Get efficiency for zero width
-        zeff = getEffForEvent(event,None)
-        zeroEff += zeff
-        zeroEffErr += zeff**2
+        #Limit the maximum number of HSCPs per event:
+        event = event[:nHSCP]
         #Get rescaled efficiency
-        reff = getEffForEvent(event,xEffs)
-        rescaledEff += reff
-        rescaledEffErr += reff**2
+        evEffs = getEffForEvent(event,widths,detectorLength)
+        for sr in evEffs.dtype.names:
+            if sr == 'width': continue
+            effs[sr] += evEffs[sr] #Add up efficiencies
+            effs[sr+'_err'] += evEffs[sr]**2 #Add up error squared
 
-    zeroEffErr = np.sqrt(zeroEffErr)
-    rescaledEffErr = np.sqrt(rescaledEffErr)
-    LLPfraction = rescaledEff/zeroEff
-    LLPfractionErr = LLPfraction*np.sqrt((rescaledEffErr/rescaledEff)**2 + (zeroEffErr/zeroEff)**2)
 
-    res = np.array(zip(widths,LLPfractions,LLPfractionsErr),dtype=[('width',float),('eff',float),('effError',float)])
-    res = np.sort(res,order='width')
 
-    outFile = os.path.join(outFolder,inputFile.replace('.lhe','')+'.eff')
+    #Compute average efficiency and error
+    for label in effs.dtype.names:
+        if label == 'width': continue
+        if '_err' in label:
+            effs[label] = np.sqrt(effs[label])/len(events)
+        else:
+            effs[sr] = effs[sr]/len(events)
+
+    res = np.sort(effs,order='width')
+
+    outFile = os.path.join(outFolder,os.path.basename(inputFile).replace('.lhe','')+'.eff')
     #Save results to file:
     header = '%19s'*len(res.dtype.names) %res.dtype.names
     header = header[3:]
     np.savetxt(outFile,res,header=header,fmt = ['     %1.7e']*len(res.dtype.names))
+
+    if inputFile != lheFile and os.path.isfile(inputFile):
+        os.remove(inputFile)
 
     return True
 
@@ -255,7 +239,7 @@ if __name__ == "__main__":
     import argparse
     ap = argparse.ArgumentParser( description=
             "Compute effective fraction of long-lived particles" )
-    ap.add_argument('-p', '--parfile', default='fraction_parameters.ini',
+    ap.add_argument('-p', '--parfile', default='map_parameters.ini',
             help='path to the parameters file.')
     ap.add_argument('-v', '--verbose', default='error',
             help='verbose level (debug, info, warning or error). Default is error')
@@ -274,19 +258,18 @@ if __name__ == "__main__":
         sys.exit()
     logger.setLevel(level = levels[level])
 
-    parser = SafeConfigParser( inline_comment_prefixes=( ';', ))
-    parser.optionxform = str
-    parser._interpolation = ExtendedInterpolation()
+    parser = ConfigParserExt()
     ret = parser.read(args.parfile)
     if ret == []:
         logger.error( "No such file or directory: '%s'" % args.parfile)
         sys.exit()
 
     lheFiles = eval(parser.get("options","lheFiles"))
-    outFile = parser.get("options","outFile")
-    widths = np.array(eval(parser.get("options","widths")))
-    detectorLength = eval(parser.get("options","detectorLength"))
+    effFolder = parser.get("options","effFolder")
+    widths = np.array(parser.get("options","widths"))
+    detectorLength = parser.get("options","detectorLength")
     ncpus = parser.getint("options","ncpu")
+    nHSCP = parser.getint("options","nHSCP")
     if ncpus  < 0:
         ncpus =  multiprocessing.cpu_count()
 
@@ -295,7 +278,7 @@ if __name__ == "__main__":
     children = []
     #Loop over model parameters and submit jobs
     for lheFile in lheFiles:
-        p = pool.apply_async(getLLPFraction, args=(lheFile,widths,detectorLength,))
+        p = pool.apply_async(getEffsFor, args=(lheFile,nHSCP,widths,detectorLength,effFolder,))
         children.append(p)
 
     #Wait for jobs to finish:
