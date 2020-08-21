bc943ad8942a69f13e7ff35d7b5e406d764a27a3
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2020-08-21 13:36:02 +0200

    slurm -U mode for short term

--- tools/updateHiscores.py
+++ tools/updateHiscores.py
@@ -25,6 +25,34 @@ def setup( rundir = None ):
     os.chdir ( rundir )
     return rundir
 
+def countSteps():
+    """ count the number of steps taken accoring to walker logs """
+    import glob
+    files = glob.glob("walker*log")
+    steps = {}
+    for f in files:
+        nr = int ( f.replace("walker","").replace(".log","") )
+        h = open ( f, "rt" )
+        lines = h.readlines()
+        h.close()
+        for line in lines[::-1]:
+            if "Step" in line:
+                laststep = line[line.find("Step")+5:]
+                for c in [ "/", ":", " has", " " ]:
+                    if c in laststep:
+                        laststep = laststep[:laststep.find(c)]
+                laststep = int ( laststep.strip() )
+                #print ( nr, laststep )
+                steps[nr]=laststep
+                break
+    keys = list ( steps.keys() )
+    keys.sort()
+    tots = 0
+    for k in keys:
+        tots += steps[k]
+        print ( k, steps[k] )
+    print ( f"we have {len(keys)} entries, total of {tots} steps." )
+    return tots
 
 def updateHiscores( rundir=None ):
     args = types.SimpleNamespace()
