4dd9a1b953bfa83391705c9ac6ea3a6b7540ee79
Andre Lessa <lessa.a.p@gmail.com>
2020-11-18 17:38:20 -0300

    Removed EM_Creator

--- EM_Creator/bake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-  
-./bake.py -p 5 -t TGQ -m "[(50,4500,200),(50,4500,200),(0.)]" -a --analyses "atlas_susy_2016_07,cms_sus_16_033" -n 10000 --mingap2 0. --mingap13 0. &
-./bake.py -p 5 -t TGQ -m "[(750,4500,200),(750,4500,200),(695.)]" -a --analyses "atlas_susy_2016_07,cms_sus_16_033" -n 10000 --mingap2 0. --mingap13 0. &
-./bake.py -p 5 -t TGQ -m "[(1050,4500,200),(1050,4500,200),(995.)]" -a --analyses "atlas_susy_2016_07,cms_sus_16_033" -n 10000 --mingap2 0. --mingap13 0. &
-./bake.py -a -p 5 -t TGQ --analyses "cms_sus_16_033,atlas_susy_2016_07" -m "[(100,2000,200),(100,2000,200),(50,2000,200)]" -n 10000 --mingap2 0. --mingap13 0. &
-./bake.py -p 5 -t TGQ -m "[(50,4500,400),(50,4500,400),(50,3000,400.)]" -a --analyses "atlas_susy_2016_07,cms_sus_16_033" -n 10000 --mingap2 0. --mingap13 0. &
--- EM_Creator/rmExo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm -r cms_b2g* cms_exo* cms_top* cms_sus_16_039* cms_sus_16_052* atlas_susy_2015_06* atlas_conf* atlas_exo* cms_sus_17*
