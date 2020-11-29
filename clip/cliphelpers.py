8218a6922d3ea04700d360894bc99d175a2916fe
walten <wolfgang.waltenberger@gmail.com>
2020-11-29 15:22:08 +0100

    cliphelpers

---
+++ clip/cliphelpers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" various helper functions around the clip cluster that do not fit in any of
+the more specific modules """
+
+def describeSet( s: set ):
+    """ describe a given set of indices in compact form.
+    returns: a string
+    """
+    return str( s )
