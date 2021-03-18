20d0939837b3427e9555206906d5d02d7cfc0223
walten <wolfgang.waltenberger@gmail.com>
2021-03-18 23:13:20 +0100

    inverse

--- bin/createSuperseded.py
+++ bin/createSuperseded.py
@@ -6,10 +6,13 @@ results only. """
 import argparse
 from smodels.experiment.databaseObj import Database
 
-def create ( infile, outfile ):
+def create ( infile, outfile, filtered ):
+    """
+    :param filtered: if true, then remove superseded entries instead of the other way round
+    """
     db = Database ( infile )
     olders = db.expResultList
-    newers = []
+    newers, supers = [], []
     superseded = []
     for er in olders:
         gI = er.globalInfo
@@ -20,17 +23,30 @@ def create ( infile, outfile ):
         gI = er.globalInfo
         if hasattr ( gI, "supersededBy" ) or gI.id in superseded:
             newers.append ( er )
-    db.subs[0].expResultList = newers
+        else:
+            if not hasattr ( gI, "contact" ) or not "fastlim" in gI.contact:
+                supers.append ( er )
+    if filtered:
+        db.subs[0].expResultList = supers
+    else:
+        db.subs[0].expResultList = newers
     db.subs = [ db.subs[0] ]
-    db.subs[0].txt_meta.databaseVersion = db.databaseVersion + "superseded"
+    sstring = "superseded"
+    if filtered:
+        sstring = "nosuperseded"
+    db.subs[0].txt_meta.databaseVersion = db.databaseVersion + sstring
     db.createBinaryFile ( outfile )
 
 def main( ):
     ap = argparse.ArgumentParser( description="script that creates a database pickle file with superseded results only" )
     ap.add_argument('-i', '--infile', help='name of input database -- pickle file or path [../../smodels-database]', default="../../smodels-database" )
     ap.add_argument('-o', '--outfile', help='name of output pickle file [superseded.pcl]', default="superseded.pcl" )
+    ap.add_argument('-f', '--filter', help='invert the selection, remove superseded entries',
+                     action="store_true" )
     args = ap.parse_args()
 
-    create ( args.infile, args.outfile )
+    if args.filter and args.outfile == "superseded.pcl":
+        args.outfile = "filtered.pcl"
+    create ( args.infile, args.outfile, args.filter )
 
 main()
