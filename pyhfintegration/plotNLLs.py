058bf004715961aff595e4bf94ac34b0c604d15d
walten <wolfgang.waltenberger@gmail.com>
2021-06-17 17:24:34 +0200

    setter

--- pyhfintegration/plotNLLs.py
+++ pyhfintegration/plotNLLs.py
@@ -25,21 +25,36 @@ def main():
     import argparse
     argparser = argparse.ArgumentParser( description = "plot NLLs for various approximations of the likelihood" )
     argparser.add_argument ( "-a", "--analysis",
-            help="analysis [ATLAS-SUSY-2018-14]",
-            type=str, default="ATLAS-SUSY-2018-14" )
+            help="analysis [ATLAS-SUSY-2019-08]",
+            type=str, default="ATLAS-SUSY-2019-08" )
     argparser.add_argument ( "-l", "--lifetime",
-            help="lifetime string [3.4e-16]",
-            type=str, default="3.4e-16" )
+            help="lifetime string [None]",
+            type=str, default=None )
+    argparser.add_argument ( "-t", "--txname",
+            help="txname [TChiWH]",
+            type=str, default="TChiWH" )
+    argparser.add_argument ( "-m", "--mlsp",
+            help="mass of lsp [None]",
+            type=int, default=None )
     args = argparser.parse_args()
     db = Database ( "../../smodels-database" )
-    model = Model(BSMparticles=BSMList, SMparticles=SMList)
-    slhafile = "../slha/TSelSelDisp_1000_1.4e-14_0_1000_1.4e-14_0.slha"
-    model.updateParticles(inputFile=slhafile)
+    mlsp = args.mlsp
+    if mlsp == None and args.lifetime!= None:
+        mlsp = 1
 
-    for txname in [ "TSelSelDisp" ]: # , "TSmuSmuDisp" ]:
+    for txname in [ args.txname ]: # , "TSmuSmuDisp" ]:
         tarball="../slha/%s.tar.gz" % txname
         tar = tarfile.open(tarball,"r:gz")
-        files = [f for f in tar.getnames() if '.slha' in f and args.lifetime in f ]
+        def matches ( f ):
+            if not f.endswith ( ".slha" ):
+                return False
+            if args.lifetime != None and not args.lifetime in f:
+                return False
+            if args.mlsp != None and not "_%s_" % args.mlsp in f:
+                return False
+            return True
+
+        files = [f for f in tar.getnames() if matches ( f ) ]
         erf = db.getExpResults ( analysisIDs = [ args.analysis ], useNonValidated=True,
                                  dataTypes = [ "efficiencyMap" ], txnames = [ txname ] )[0]
         #era = db.getExpResults ( analysisIDs = [ args.analysis ], useNonValidated=True,
@@ -49,10 +64,12 @@ def main():
         for f in files:
             fobj = tar.extractfile(f)
             txt = fobj.read()# .replace("\\n","\n" )
+            model = Model(BSMparticles=BSMList, SMparticles=SMList)
             model.updateParticles ( inputFile="../slha/" + f )
             pyfile=pyslha.readSLHA( txt.decode("ascii") )
             masses=pyfile.blocks["MASS"]
-            mslep = masses[1000011]
+            mmother = masses[1000023]
+            reallsp = masses[1000022]
             xsec = 0.
             for k,xsecs in pyfile.xsections.items():
                 for xs in xsecs.xsecs:
@@ -64,13 +81,19 @@ def main():
                         continue
                     xsec += xs.value
             fobj.close()
-            massvec = [[ (mslep*GeV, 3.4e-16*GeV), 1*GeV ] ] * 2
+            mothervec = mmother*GeV
+            if args.lifetime != None:
+                mothervec = ( mmother*GeV, args.lifetime*GeV )
+            if mlsp == None:
+                mlsp = reallsp
+            # massvec = [[ (mmother*GeV, 3.4e-16*GeV), 1*GeV ] ] * 2
+            massvec = [[ mothervec, mlsp*GeV ] ] * 2
             toplist = decomposer.decompose ( model, 0.01*fb, True, True, 5.*GeV )
             preds = theoryPredictionsFor ( erf, toplist, combinedResults=False,
                                            useBestDataset=True )
             if type(preds) != type(None):
                 # llhd = preds[0].llhd
-                x.append ( mslep )
+                x.append ( mmother )
                 lumi = preds[0].dataset.globalInfo.lumi
                 llhd = preds[0].getLikelihood()
                 oul = float ( preds[0].getUpperLimit()*lumi )
@@ -80,12 +103,12 @@ def main():
                 if type(llhd) == type(None):
                     yBestSR.append ( float("nan") )
                 else:
-                    print ( "best SR mass", mslep, "nll", - math.log ( llhd ) )
+                    print ( "best SR mass", mmother, "nll", - math.log ( llhd ) )
                     yBestSR.append ( - math.log ( llhd ) * random.uniform(.9,1.1 ) )
                 if type(ulllhd) == type(None):
                     yUL.append ( float("nan") )
                 else:
-                    # print ( "UL-based mass", mslep, "nll", ulllhd )
+                    # print ( "UL-based mass", mmother , "nll", ulllhd )
                     if ulllhd > 500.:
                         ulllhd = 500.
                     yUL.append ( ulllhd )
@@ -100,7 +123,7 @@ def main():
                 if type(cllhd) == type(None):
                     yPyhf.append( float ("nan" ) )
                 else:
-                    print ( "comb mass", mslep, "nll", - math.log ( cllhd ) )
+                    print ( "comb mass", mmother , "nll", - math.log ( cllhd ) )
                     yPyhf.append ( - math.log ( cllhd ) )
             # oul = eru.getUpperLimitFor ( None, expected=False, txname=txname, mass = massvec )
             # print ( oul )
