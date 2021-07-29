2b66cb4fc3aab1cac5b2da18ff0de1eda4fd6b30
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2021-07-29 12:46:19 +0200

    adapted parameters

--- clip/slurm.py
+++ clip/slurm.py
@@ -473,9 +473,9 @@ def bake ( recipe, analyses, mass, topo, dry_run, nproc, rundir, cutlang,
             qos = "c_medium"
         cmd += [ "--qos", qos ]
         cmd += [ "--time", "%s" % ( time*60-1 ) ]
-    ram = 3 * nproc
+    ram = 3.5 * nproc
     cmd += [ "--mem", "%dG" % ram ]
-    cmd += [ "-c", "%d" % (int(nproc)*3) ]
+    cmd += [ "-c", "%d" % (int(nproc)*2) ]
     cmd += [ tmpfile ]
     # cmd += [ "./run_bakery.sh" ]
     print ("[slurm.py] baking %s" % " ".join ( cmd ) )
