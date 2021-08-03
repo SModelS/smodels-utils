e924f7eddc0cf90397c89aa1340f781946b86bf9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2021-08-03 11:11:50 +0200

    copy environment to hpc file system

---
+++ clip/copyEnv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import subprocess, os
+
+def copy():
+    files = [ ".bashrc", ".bash.aliases", ".vimrc", ".tmux.conf" ]
+    for f in files:
+        source = "/users/wolfgan.waltenberger/"
+        dest = "/scratch-cbe/users/wolfgan.waltenberger/"
+        if os.path.exists ( f"{dest}{f}" ):
+            continue
+        cmd = f"cp -rf {source}{f} {dest}{f}"
+        subprocess.getoutput ( cmd )
+
+def copyContainers():
+    destdir = "/scratch-cbe/users/wolfgan.waltenberger/container/"
+    sourcedir = "/mnt/hephy/pheno/"
+    simg = "ubuntu2104sing38a.simg"
+    if not os.path.exists ( f"{destdir}{simg}" ):
+        cmd = f"cp {sourcedir}{simg} {destdir}"
+        subprocess.getoutput ( cmd )
+    if not os.path.exists ( f"{destdir}current.simg" ):
+        cmd = f"ln -s {destdir}{simg} {destdir}current.simg"
+        subprocess.getoutput ( cmd )
+
+if __name__ == "__main__":
+    copy()
+    copyContainers()
