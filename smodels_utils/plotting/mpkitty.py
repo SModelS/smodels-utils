6b11bc7588cfeed0ade9d0b34fdd8a88a6ccf113
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2021-09-09 12:28:47 +0200

    copy if needed

--- smodels_utils/plotting/mpkitty.py
+++ smodels_utils/plotting/mpkitty.py
@@ -1,7 +1,22 @@
 #import sys
 #sys.path.insert(0,".")
 # import matplotlib-backend-kitty
+def importBackend():
+    import matplotlib, os, sys, subprocess
+    from smodels_utils import SModelSUtils
+    home = os.environ["HOME"]
+    ver = sys.version[:3]
+    path = f"{home}/.local/lib/python{ver}/site-packages/"
+    name = "matplotlib-backend-kitty"
+    if not os.path.exists ( os.path.join ( path, name ) ):
+        cmd = f"mkdir -p {path}"
+        subprocess.getoutput ( cmd )
+        sourcedir = os.path.join ( SModelSUtils.installDirectory(), "smodels_utils", "plotting" )
+        o = cmd = f"cp -r {sourcedir}/{name} {path}"
+        subprocess.getoutput ( cmd )
 
-import matplotlib
-matplotlib.use('module://matplotlib-backend-kitty')
+    matplotlib.use('module://matplotlib-backend-kitty')
+
+importBackend()
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
