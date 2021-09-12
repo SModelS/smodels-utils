0e1a2c303b3e628beb90d5ade03073f8c020f3e4
walten <wolfgang.waltenberger@gmail.com>
2021-09-12 11:27:21 +0200

    fallback

--- smodels_utils/plotting/mpkitty.py
+++ smodels_utils/plotting/mpkitty.py
@@ -1,21 +1,25 @@
-#import sys
-#sys.path.insert(0,".")
-# import matplotlib-backend-kitty
+""" a bit of code to try to import the matplotlib-kitty backend,
+    if that fails then fall back to ordinary matplotlib """
+
 def importBackend():
-    import matplotlib, os, sys, subprocess
-    from smodels_utils import SModelSUtils
-    home = os.environ["HOME"]
-    ver = sys.version[:3]
-    path = f"{home}/.local/lib/python{ver}/site-packages/"
-    name = "matplotlib-backend-kitty"
-    if not os.path.exists ( os.path.join ( path, name ) ):
-        cmd = f"mkdir -p {path}"
-        subprocess.getoutput ( cmd )
-        sourcedir = os.path.join ( SModelSUtils.installDirectory(), "smodels_utils", "plotting" )
-        o = cmd = f"cp -r {sourcedir}/{name} {path}"
-        subprocess.getoutput ( cmd )
+    try:
+        import matplotlib, os, sys, subprocess
+        from smodels_utils import SModelSUtils
+        home = os.environ["HOME"]
+        ver = sys.version[:3]
+        path = f"{home}/.local/lib/python{ver}/site-packages/"
+        name = "matplotlib-backend-kitty"
+        if not os.path.exists ( os.path.join ( path, name ) ):
+            cmd = f"mkdir -p {path}"
+            subprocess.getoutput ( cmd )
+            sourcedir = os.path.join ( SModelSUtils.installDirectory(), "smodels_utils", "plotting" )
+            cmd = f"cp -r {sourcedir}/{name} {path}"
+            o = subprocess.getoutput ( cmd )
 
     matplotlib.use('module://matplotlib-backend-kitty')
+
+    except Exception as e:
+        pass
 
 importBackend()
 import matplotlib.pyplot as plt
