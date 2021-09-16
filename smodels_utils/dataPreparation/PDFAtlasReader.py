179574db68a28c61a7890811bbdfeef4a7de92e1
walten <wolfgang.waltenberger@gmail.com>
2021-09-16 14:31:37 +0200

    lines

--- smodels_utils/dataPreparation/PDFAtlasReader.py
+++ smodels_utils/dataPreparation/PDFAtlasReader.py
@@ -4,6 +4,7 @@
 """
 
 import minecart
+import sys
 
 class PDFAtlasReader(): 
     def __init__( self, config : dict ): 
@@ -19,7 +20,18 @@ class PDFAtlasReader():
         print ( f"[PDFAtlasReader] Error: {line}" )
         raise RuntimeError( line )
 
-    def computeXY ( self, letter ):
+    def pprint ( self, line ):
+        print ( f"[PDFAtlasReader] {line}" )
+
+    def pdfToMasses ( self, pdfx, pdfy ):
+        """ convert pdfx and pdfy to masses """
+        rx = ( pdfx - self.pdf_x0 ) / ( self.pdf_xmax - self.pdf_x0 )
+        ry = ( pdfy - self.pdf_y0 ) / ( self.pdf_ymax - self.pdf_y0 )
+        x = self.config["x"]["limits"][0] + rx * ( self.config["x"]["delta"] )
+        y = self.config["y"]["limits"][0] + ry * ( self.config["y"]["delta"] )
+        return x,y
+
+    def computeXYLetter ( self, letter ):
         """ compute X and Y coordinates for letter """
         ## '0.39' bbox (365.7701221858726, 245.44099671225973,
         # 373.6441329278273, 257.6138040553271) sits at 150,150
@@ -29,17 +41,25 @@ class PDFAtlasReader():
         # pdf_xavg = is the x coordinate in pdf coord system
         pdf_yavg = ( bbox[1] + bbox[1] ) / 2.
         pdf_xavg = ( bbox[0] + bbox[0] ) / 2.
-        ## rx, ry should be the relative coordinates within the plot
-        rx = ( pdf_xavg - self.pdf_x0 ) / ( self.pdf_xmax - self.pdf_x0 )
-        ry = ( pdf_yavg - self.pdf_y0 ) / ( self.pdf_ymax - self.pdf_y0 )
-        x = self.config["x"]["limits"][0] + rx * ( self.config["x"]["delta"] )
-        y = self.config["y"]["limits"][0] + ry * ( self.config["y"]["delta"] )
-        #y = yavg - self.config["y"]["limits"][0]
-        return x,y
+        return self.pdfToMasses ( pdf_xavg, pdf_yavg )
+
+    def computeXYShape ( self, shape ):
+        """ compute X and Y coordinates for shape """
+        # pdf_xavg = is the x coordinate in pdf coord system
+        path = shape.path
+        ret = []
+        for pt in path:
+            if pt[0] not in [ "m", "l" ]:
+                self.pprint ( f"for shape we have '{pt[0]}'" )
+            ret.append ( self.pdfToMasses ( pt[1], pt[2] ) )
+        return ret
+        
 
     def read ( self ):
         """ read in the file """
-        fname = self.config["name"]+".pdf"
+        fname = self.config["name"]
+        if not fname.endswith ( ".pdf" ):
+            fname += ".pdf"
         f = open ( fname, "rb" )
         pdffile = open(fname, 'rb')
         doc = minecart.Document(pdffile)
@@ -54,15 +74,59 @@ class PDFAtlasReader():
         # pdf_xmax and pdf_ymax are the pdf coordinates of our largest xs and ys
         self.pdf_xmax = self.page.width - 20.
         self.pdf_ymax = self.page.height - 30.
-        f = open ( "values.csv", "wt" )
-        for l,lettering in enumerate(page.letterings):
-            x, y = self.computeXY ( lettering )
-            if lettering.title() in [ "0.39", "0.079" ]:
-                print ( "letter", l, lettering, "xy", self.computeXY ( lettering ) )
+        self.ulValues()
+        self.exclusionLine()
+
+    def ulValues( self ):
+        f = open ( "ul.csv", "wt" )
+        for l,lettering in enumerate(self.page.letterings):
+            x, y = self.computeXYLetter ( lettering )
+            #if lettering.title() in [ "0.39", "0.079" ]:
+            #    print ( "letter", l, lettering, "xy", self.computeXYLetter ( lettering ) )
             if "." in lettering.title():
-                f.write ( f"{x},{y},{lettering.title()}\n" )
+                line = f"{x},{y},{lettering.title()}\n"
+                if self.yIsDiff():
+                    line = f"{x},{x-y},{lettering.title()}\n"
+                f.write ( line )
         f.close()
-        self.interact()
+
+    def isDarkRed ( self, c ):
+        """ check if it is the dark red that ATLAS uses for observed limits """
+        if abs ( c[0]-0.501961 ) < 1e-5 and abs ( c[1] ) < 1e-5 and \
+           abs ( c[2] ) < 1e-5:
+                return True
+        return False
+
+    def exclusionLine ( self ):
+        line=[]
+        ct = 0
+        for s in self.page.shapes:
+            if s.stroke == None:
+                continue
+            c = s.stroke.color.as_rgb()
+            dash = s.stroke.dash
+            if self.isDarkRed ( c ) and s.stroke.linewidth > 2.1 and dash == ([], 0):
+                ct += 1
+                cur = self.computeXYShape ( s ) 
+                for pt in cur:
+                    if pt[1] < pt[0]:
+                        line.append ( pt )
+        f=open ( "excl.csv", "wt" )
+        for l in line:
+            line = f"{l[0]},{l[1]}\n"
+            if self.yIsDiff():
+                line = f"{l[0]},{l[0]-l[1]}\n"
+            f.write ( line )
+        f.close()
+
+    def yIsDiff ( self ):
+        if "axes" in self.config:
+            a = self.config["axes"].strip()
+            a = a.replace(" ","")
+            if a == "[[x-y],[x-y]]":
+                # self.pprint ( "using x-y" )
+                return True
+        return False
 
     def interact ( self ):
         import IPython
@@ -70,10 +134,20 @@ class PDFAtlasReader():
 
  
 if __name__ == "__main__":
+    import argparse
+    ap = argparse.ArgumentParser(description="PDF Reader for ATLAS files")
+    ap.add_argument('-f', '--file',
+            help='pdf file to read [figaux_08a.pdf]', default='figaux_08a.pdf')
+    ap.add_argument('-i', '--interactive',
+            help="interactive shell", action="store_true" )
+    args = ap.parse_args()
     config =  {
-        'name': 'figaux_08a',
+        'name': args.file,
         'x':{'limits': (100, 850)},
         'y':{'limits': (100, 850)},
+#        'axes': '[[x-y],[x-y]]',
         }
 
     r = PDFAtlasReader( config )
+    if args.interactive:
+        r.interact()
