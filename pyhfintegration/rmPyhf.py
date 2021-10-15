47e83df4cc399e3bc70482d9118719e5b9585b0a
walten <wolfgang.waltenberger@gmail.com>
2021-10-15 14:14:25 +0200

    added script

---
+++ pyhfintegration/rmPyhf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+""" simple script that removes all pyhf info from a database folder """
+    
+import glob, os
+
+def cleanGlobalInfo ( f ):
+    """ clean globalInfo.txt file """
+    print ( f"cleaning global info {f}" )
+    h=open ( f, "rt" )
+    txt = h.read()
+    h.close()
+    if not "json" in txt:
+        return
+    lines= txt.split("\n")
+    h=open( f, "wt" )
+    for line in lines:
+        if line.startswith("jsonFiles"):
+            continue
+        if line.startswith("datasetOrder"):
+            continue
+        if line.startswith("covariance" ):
+            continue
+        h.write ( line+"\n" )
+    h.close()
+    
+
+def rm ( path ):
+    files = glob.glob ( path + "/*" )
+    # print ( f"{len(files)} files in {path}" )
+    for f in files:
+        if f.endswith ( ".json" ):
+            print ( f"unlinking {f}" )
+            os.unlink ( f )
+        if f.endswith ( "globalInfo.txt" ):
+            cleanGlobalInfo ( f )
+        if os.path.isdir ( f ) and f != path:
+            rm ( f )
+
+rm ( "../../smodels-database" )
