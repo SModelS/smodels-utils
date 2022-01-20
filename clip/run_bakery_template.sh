49db051fbe22b7e0597730a0c39d3722b0f911eb
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2022-01-20 18:35:57 +0100

    keephepmc flag

--- clip/run_bakery_template.sh
+++ clip/run_bakery_template.sh
@@ -2,7 +2,7 @@
 
 ## ml --latest singularity
 
-cd /users/wolfgan.waltenberger/git/projects/singularity/
+# cd /users/wolfgan.waltenberger/git/projects/singularity/
 
 singularity shell -c -B /tmp,/run,/scratch -s /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/clip/@@SCRIPT@@ -H /scratch-cbe/users/wolfgan.waltenberger/git/em-creator /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
 #singularity shell -c -B /tmp,/run,/scratch -s /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/clip/@@SCRIPT@@ -H /groups/hephy/pheno/ww/git/em-creator /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
