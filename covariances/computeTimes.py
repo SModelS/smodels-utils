1bf71e978b0b5fdea511b5853139ffdd0506242d
walten <wolfgang.waltenberger@gmail.com>
2022-02-08 19:13:06 +0100

    argparse

--- covariances/computeTimes.py
+++ covariances/computeTimes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import numpy
+""" get the average runtimes out of validation files """
+
+import numpy, os
 
 def computeTimes ( validationfile ):
     f=open(validationfile,"rt")
@@ -14,9 +16,14 @@ def computeTimes ( validationfile ):
             ts.append ( t )
             if t > 0.:
                 ts0.append ( t )
-    nts = numpy.array(ts)
     print ( f"I have {len(ts)} entries. t={numpy.mean(ts):.2f}+-{numpy.std(ts):.2f} seconds" )
-    print ( f"{len(nts[nts==0.])} entries are zero. without them we have: t={numpy.mean(ts0):.2f}+-{numpy.std(ts0):.2f} seconds" )
+    print ( f"{len(ts)-len(ts0)} entries are zero. without them we have: t={numpy.mean(ts0):.2f}+-{numpy.std(ts0):.2f} seconds" )
 
 if __name__ == "__main__":
-    computeTimes ( "/home/walten/git/smodels-database/13TeV/CMS/CMS-SUS-16-048-agg/validation/TChiWZoff_2EqMassAx_EqMassBy.py" )
+    import argparse
+    ap = argparse.ArgumentParser( description= "very simple facility to compute average runtimes from validation files" )
+    ap.add_argument('-f','--validationfile',help="path to validation file [~/git/smodels-database/13TeV/CMS/CMS-SUS-16-048-agg/validation/TChiWZoff_2EqMassAx_EqMassBy.py]",
+                    default = "~/git/smodels-database/13TeV/CMS/CMS-SUS-16-048-agg/validation/TChiWZoff_2EqMassAx_EqMassBy.py", type=str )
+    args = ap.parse_args()
+    filename = os.path.expanduser ( args.validationfile )
+    computeTimes ( filename )
