541ffc3ccbc495de45911a90a5c7bc3dd9f86031
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2022-02-16 00:16:33 +0100

    cleanout

--- clip/bake_template.sh
+++ clip/bake_template.sh
@@ -4,6 +4,7 @@ SCRIPT=$(readlink -f $0)
 
 source /groups/hephy/pheno/opt/root/bin/thisroot.sh
 cd /scratch-cbe/users/wolfgan.waltenberger/git/em-creator
+test -e ./rmOld.py && ./rmOld.py # clean out regularly
 ./bake.py @@ARGS@@
 sleep 5
 rm $SCRIPT
