528ed7a0aab6baf549001c6097bdcef977df2af2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2022-02-15 15:31:47 +0100

    rm

--- clip/validate_template.sh
+++ clip/validate_template.sh
@@ -6,4 +6,5 @@ source /groups/hephy/pheno/opt/root/bin/thisroot.sh
 cd /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/validation
 ./runValidation.py -p @@INIFILE@@
 sleep 5
+rm @@INIFILE@@
 rm $SCRIPT
