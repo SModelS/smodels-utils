00164fb67a1ea1dafdedafedfb6e58adb1855f98
walten <wolfgang.waltenberger@gmail.com>
2022-02-21 16:07:56 +0100

    plotting combined llhds

--- covariances/testAnalysisCombinations.py
+++ covariances/testAnalysisCombinations.py
@@ -23,23 +23,29 @@ import os
 from smodels_utils.plotting import mpkitty as plt
 from covariances.cov_helpers import getSensibleMuRange, computeLlhdHisto
 
-def testConstruction():
-    """ this method should simply test if the fake result and the
-        covariance matrix are constructed appropriately """
+def getExpResults():
+    """ collect the experimental results """
     database = Database("official+../../smodels-database/+../../branches/smodels-database/" )
     # database = Database("official+../smodels-database/" )
     dTypes = ["efficiencyMap"]
-    anaids = ["CMS-SUS-16-033", "CMS-SUS-13-012", "ATLAS-CONF-2013-037", "CMS-PAS-SUS-16-052-agg", "ATLAS-SUSY-2018-22", "CMS-SUS-19-006-agg", "ATLAS-SUSY-2019-09-eff", "ATLAS-SUSY-2019-09" ]
-    anaids = [ "CMS-SUS-16-048", "CMS-SUS-16-050-agg", "CMS-PAS-SUS-16-052-agg", "ATLAS-SUSY-2018-22", "CMS-SUS-19-006-agg", "ATLAS-SUSY-2019-09-eff", "ATLAS-SUSY-2019-09" ]
-    # anaids = ["ATLAS-SUSY-2018-22" ]
-    dsids = [ "SR1_Njet2_Nb0_HT500_MHT500", "SR2_Njet3_Nb0_HT1500_MHT750", "3NJet6_1250HT1500_300MHT450", "SRtN2", "SR3_Njet5_Nb0_HT500_MHT_500" ]
+    # anaids = ["CMS-SUS-16-033", "CMS-SUS-13-012", "ATLAS-CONF-2013-037", "CMS-PAS-SUS-16-052-agg", "ATLAS-SUSY-2018-22", "CMS-SUS-19-006-agg", "ATLAS-SUSY-2019-09-eff", "ATLAS-SUSY-2019-09" ]
+    # anaids = [ "CMS-SUS-16-048", "CMS-SUS-16-050-agg", "CMS-PAS-SUS-16-052-agg", "ATLAS-SUSY-2018-22", "CMS-SUS-19-006-agg", "ATLAS-SUSY-2019-09-eff" ]
+    anaids = ["ATLAS-SUSY-2019-09", "CMS-SUS-16-039-agg", "ATLAS-SUSY-2018-06" ]
+    # dsids = [ "SR1_Njet2_Nb0_HT500_MHT500", "SR2_Njet3_Nb0_HT1500_MHT750", "3NJet6_1250HT1500_300MHT450", "SRtN2", "SR3_Njet5_Nb0_HT500_MHT_500" ]
     dsids = [ "all" ]
-    # slhafile = "test/testFiles/slha/T1tttt.slha"
-    slhafile = "T2_1233_1007_1233_1007.slha"
-    slhafile = "T2tt_880_300_880_300.slha"
-    # slhafile = "TChiWZ_820_680_820_680.slha"
     exp_results = database.getExpResults(analysisIDs=anaids,
                                          datasetIDs=dsids, dataTypes=dTypes)
+    return exp_results
+
+def testConstruction():
+    """ this method should simply test if the fake result and the
+        covariance matrix are constructed appropriately """
+    exp_results = getExpResults()
+    # slhafile = "T2_1233_1007_1233_1007.slha"
+    # slhafile = "T2tt_1130_650_1130_650.slha"
+    # slhafile = "test/testFiles/slha/T1tttt.slha"
+    slhafile = "TChiWZ_820_680_820_680.slha"
+    slhafile = "TChiWZ_460_230_460_230.slha"
     model = Model(BSMparticles=BSMList, SMparticles=SMList)
     model.updateParticles(inputFile=slhafile)
     smstopos = decomposer.decompose(model)
@@ -47,7 +53,8 @@ def testConstruction():
     llhds = {}
     totllhd = {}
     llmin,llmax = float("inf"), 0.
-    print ( f"i see {len(exp_results)} expresults" )
+    ernames = set ( [ x.globalInfo.id for x in exp_results ] )
+    print ( f"[testAnalysisCombinations] {len(exp_results)} results:", ", ".join(ernames)  )
     for er in exp_results:
         ts = theoryPredictionsFor(er, smstopos,
             combinedResults=False, useBestDataset=False, marginalize=False)
@@ -55,9 +62,9 @@ def testConstruction():
             continue
         tsc = theoryPredictionsFor(er, smstopos,
             combinedResults=True, useBestDataset=False, marginalize=False)
-        print ( f"{len(ts)} SR results, {len(tsc)} comb results" )
+        print ( f"   --- {er.globalInfo.id}: {len(ts)} SR results, {len(tsc)} comb results" )
         # ts += tsc
-        ts = tsc
+        # ts = tsc
         for t in ts:
             tpreds.append(t)
     xmin, xmax = getSensibleMuRange ( tpreds )
@@ -97,9 +104,8 @@ def testConstruction():
    # plt.plot ( [ mu_hat-sigma_mu, mu_hat-sigma_mu ], [ llmin, .7*llmax ], linestyle=":", c="k" )
     #plt.plot ( [ mu_hat+sigma_mu, mu_hat+sigma_mu ], [ llmin, .7*llmax ], linestyle=":", c="k" )
     # plt.yscale ( "log" )
-    print ( "mu_hat", mu_hat, "lmax", lmax )
     ulmu = combiner.getUpperLimitOnMu()
-    print ( "ulmu", ulmu )
+    print ( f"[testAnalysisCombinations] mu_hat {mu_hat:.2g} lmax {lmax:.2g} ul_mu {ulmu:.2f}" )
     plt.plot ( [ ulmu, ulmu ], [ llmin, llmax*.25 ], linestyle="dotted", c="k", label=r"ul$_\mu$" )
 
     slha = slhafile 
@@ -108,6 +114,7 @@ def testConstruction():
         slha = slha[:p]
     plt.title ( f"likelihoods for {slha}" )
     plt.legend()
+    # plt.legend(bbox_to_anchor=(1.1, 1.05)) # place outside
     plt.xlabel ( r"$\mu$" )
     plt.savefig ( "combo.png" )
     plt.kittyPlot()
