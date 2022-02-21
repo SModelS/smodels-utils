a2e03c7c47d647883d6c35b753915eb50ea15821
walten <wolfgang.waltenberger@gmail.com>
2022-02-21 11:51:54 +0100

    sensible range

--- covariances/cov_helpers.py
+++ covariances/cov_helpers.py
@@ -4,7 +4,15 @@
 
 import math
 
-def _computeLlhds ( tpred, xmin, xmax, nbins = 10 ):
+def computeLlhdHisto ( tpred, xmin, xmax, nbins = 10 ):
+    """ compute the likelhoods for theory prediction
+    :param tpred: a theory prediction
+    :param xmin: minimum mu
+    :param xmax: maximum mu
+    :param nbins: the number of bins
+    :returns dictionary of normalized likelihoods and normalization constant
+    """
+
     import numpy as np
     dx = ( xmax - xmin ) / nbins
     S = 0.
@@ -16,38 +24,57 @@ def _computeLlhds ( tpred, xmin, xmax, nbins = 10 ):
         ret[mu]=l
     for k,v in ret.items():
         ret[k]=ret[k]/S
-    return ret
+    return ret, S
 
-def getSensibleMuRange ( tpred, nfinalbins=None ):
+def getSensibleMuRange ( tpred ):
     """ given a theory prediction, get a sensible range for mu.
         sensible meaning, smallest interval that covers 99% of the llhd 
-    :param tpred: the theory prediction
-    :param nfinalbins: if integer, then run with this as final number of bins
+    :param tpred: the theory prediction or list of theory predictions
     """
+    if type ( tpred ) in [ tuple, list ]:
+        minx, maxx = [], []
+        minxtot, maxxtot = float("inf"), -float("inf")
+        for t in tpred:
+            xmin, xmax = getSensibleMuRange ( t )
+            if xmin < minxtot:
+                minxtot = xmin
+            if xmax > maxxtot:
+                maxxtot = xmax
+        return minxtot, maxxtot
+            
     xmin, xmax = -1., 5. ## first guess
     hasConverged = False
     ctIt = 0
     while not hasConverged:
+        if ctIt > 10:
+            break
         hasConverged = True
         ctIt += 1
-        llhds = _computeLlhds ( tpred, xmin, xmax )
-        # print ( f"iteration {ctIt}: xmin={xmin}, xmax={xmax} llhds={llhds}" )
+        factor = ( 1. + 1. / ctIt )
+        llhds, S = computeLlhdHisto ( tpred, xmin, xmax, 10 )
         v = list ( llhds.values() )
+        if False:
+        print ( f"[cov_helpers] iteration {ctIt}: xmin={xmin}, xmax={xmax} llhds={v[0],v[-1]}" )
         if v[0] > .01 and xmin < 0.:
-            xmin = xmin * 2.
+            xmin = xmin * factor
             hasConverged = False
         if v[0] > .01 and xmin > 0.:
-            xmin = xmin - 1.
+            xmin = xmin - factor
             hasConverged = False
         if v[-1] > .01:
-            xmax = xmax * 1.5
+            xmax = xmax * factor
             hasConverged = False
-        if v[0] < 1e-9: ## too far
-            xmin = xmin * 1.3
-    if nfinalbins != None:
-        llhds = _computeLlhds ( tpred, xmin, xmax, nfinalbins )
-    return llhds
-    
+        if v[0] < 1e-3 and xmin < 0.: ## too far
+            xmin = xmin / factor
+            hasConverged = False
+        if v[0] < 1e-3 and xmin > 0.: ## too far
+            xmin = xmin * factor
+            hasConverged = False
+        if v[-1] < 1e-4: ## too far
+            xmax = xmax / factor
+            hasConverged = False
+    k = list ( llhds.keys() )
+    return min(k), max(k)
 
 def computeCorrelationMatrix ( cov : list ) -> list:
     """ given a covariance matrix, compute a correlation matrix
