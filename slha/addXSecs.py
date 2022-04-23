acd7c3d30836c6c5a5fca4ae469c17d6fc4eb22e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2022-04-23 15:30:39 +0200

    simple script to parallelize manually

---
+++ slha/addXSecs.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import glob, os, subprocess, sys
+
+def hasXSecs( filename ):
+    with open ( filename, "rt" ) as f:
+        lines=f.readlines()
+        f.close()
+    for line in lines:
+        if line.startswith ( "XSEC" ):
+            return True
+    return False
+
+def lock ( f ):
+    if not os.path.exists ( f ):
+        return
+    if "/" in f:
+        p1 = f.rfind("/")
+        f = f[p1+1:]
+    cmd = f"touch .{f}.lock"
+    subprocess.getoutput ( cmd )
+
+def unlock ( f ):
+    if "/" in f:
+        p1 = f.rfind("/")
+        f = f[p1+1:]
+    cmd = f"rm -rf .{f}.lock"
+    subprocess.getoutput ( cmd )
+    
+def isLocked ( f ):
+    if "/" in f:
+        p1 = f.rfind("/")
+        f = f[p1+1:]
+    return os.path.exists ( f".{f}.lock" )
+
+def count( dirname ):
+    files = glob.glob( dirname+"T*slha" )
+    ctr=0
+    for f in files:
+        if not hasXSecs ( f ) and not isLocked ( f ):
+            ctr+=1
+    print ( f"{ctr}/{len(files)} processed" )
+
+def addXSecs( dirname ):
+    files = glob.glob( dirname+"T*slha" )
+    ctr=0
+    hasXS = 0
+    locked = 0
+    for f in files:
+        if hasXSecs ( f ):
+            hasXS += 1
+            continue
+        if isLocked ( f ):
+            locked += 1
+            continue
+        else:
+            ctr+=1
+            if ctr < 40:
+                cmd = f"../../smodels/smodelsTools.py xseccomputer -f {f} -8 -N -P -e 200000 -v info -c 1 "
+                pid = os.fork()
+                if pid == 0:
+                    lock ( f )
+                    print ( "cmd", cmd )
+                    o = subprocess.getoutput ( cmd )
+                    print ( "o", o )
+                    unlock ( f )
+                    sys.exit()
+    print ( f"{ctr}/{len(files)} processing, {hasXS} have xsecs, {locked} locked" )
+
+if __name__ == "__main__":
+    addXSecs( "tmpcsz2ttbh/" )
+    # count( "tmpcsz2ttbh/" )
