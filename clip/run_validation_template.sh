8a90f51dfba71fb26668ca15f0d6f9928b6e74f9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2022-06-16 16:45:54 +0200

    different home dir

--- clip/run_validation_template.sh
+++ clip/run_validation_template.sh
@@ -5,7 +5,7 @@ SCRIPT=$(readlink -f $0)
 
 # cd /users/wolfgan.waltenberger/git/projects/singularity/
 
-singularity shell -c -B /tmp,/run,/scratch -s /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/clip/temp/@@SCRIPT@@ -H /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/validation /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
+singularity shell -c -B /tmp,/run,/scratch -s /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/clip/temp/@@SCRIPT@@ -H /scratch-cbe/users/wolfgan.waltenberger/ /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
 
 # -H /groups/hephy/pheno/ww/git/em-creator
 
