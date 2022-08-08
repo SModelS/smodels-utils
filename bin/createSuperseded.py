661f744b82e39c161906c019725e4d53bea5f4f5
Andre Lessa <lessa.a.p@gmail.com>
2022-08-08 11:47:38 -0300

    Path fixes for createSuperseded

--- bin/createSuperseded.py
+++ bin/createSuperseded.py
@@ -4,6 +4,7 @@
 results only. """
 
 import argparse
+import sys,os
 
 def main( ):
     ap = argparse.ArgumentParser( description="script that creates a database pickle file with superseded results only" )
@@ -11,12 +12,23 @@ def main( ):
     ap.add_argument('-o', '--outfile', help='name of output pickle file [superseded.pcl]', default="superseded.pcl" )
     ap.add_argument('-f', '--filter', help='invert the selection, remove superseded entries',
                      action="store_true" )
+    ap.add_argument('-P', '--smodelsPath', help='path to the SModelS folder [None]', default=None)                     
     args = ap.parse_args()
 
     if args.filter and args.outfile == "superseded.pcl":
         args.outfile = "filtered.pcl"
-    from smodels_utils.helper.databaseManipulations import createSuperseded
-    createSuperseded ( args.infile, args.outfile, args.filter )
+
+    if args.smodelsPath:
+        sys.path.append(os.path.abspath(args.smodelsPath))
+
+
+    try:
+        from smodels_utils.helper.databaseManipulations import createSupersededPickle
+    except ModuleNotFoundError:
+        sys.path.append('../')
+        from smodels_utils.helper.databaseManipulations import createSupersededPickle
+    
+    createSupersededPickle ( args.infile, args.outfile, args.filter )
 
 if __name__ == "__main__":
     main()
