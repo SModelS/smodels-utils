9adbf2eb03b2b1290399ed0b190e70484f96a7b8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-02-14 17:39:18 +0100

    degenerate

--- slha/slhaCreator.py
+++ slha/slhaCreator.py
@@ -496,7 +496,7 @@ if __name__ == "__main__":
         type=str, default='@@topo@@.tar.gz' )
     argparser.add_argument ( '-C', '--comment', nargs='?', help='add a comment to all files [None]',
         type=str, default=None )
-    argparser.add_argument ( '-e', '--ewk', help='type of ewk process, wino or hino [wino]',
+    argparser.add_argument ( '-e', '--ewk', help='type of ewk process, wino, hino, or degenerate [wino]',
         type=str, default="wino" )
     argparser.add_argument ( '-a', '--axes', nargs='?', help='axes description 2*[[x, y]]',
         type=str, default='2*[[x, y]]' )
