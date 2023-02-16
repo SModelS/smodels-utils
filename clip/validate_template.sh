e8a712e92dd8df951730e1793d1de4ff8174ceeb
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-02-16 15:13:22 +0100

    validate

--- clip/validate_template.sh
+++ clip/validate_template.sh
@@ -2,7 +2,13 @@
 
 SCRIPT=$(readlink -f $0)
 
+ml unload gcc
+ml unload anaconda3
+ml unload build-env
+
+export PYTHONPATH="/scratch-cbe/users/wolfgan.waltenberger/git/smodels:/scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils"
 source /groups/hephy/pheno/opt/root/bin/thisroot.sh
+
 cd /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/validation
 ./runValidation.py -p @@INIFILE@@
 sleep 5
