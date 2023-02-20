016d411f2ceed97d50a747a6476ced929a098b6a
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-02-20 10:53:16 +0100

    corrections

--- smodels_utils/dataPreparation/pdfconfigs/CMS-SUS-19-010-aux-018b.py
+++ smodels_utils/dataPreparation/pdfconfigs/CMS-SUS-19-010-aux-018b.py
@@ -1,6 +1,6 @@
 {
   'name': 'CMS-SUS-19-010_Figure-aux_018-b',
   'x':{'limits': (800, 2501, 50) },
-  'y':{'limits': (0, 2101, 50) },
-  'z':{'limits': ( 0, .35 + .04 ), 'log': False },
+  'y':{'limits': (0, 1551, 50) },
+  'z':{'limits': ( 0, .35 + 1e-6 ), 'log': False },
 }
