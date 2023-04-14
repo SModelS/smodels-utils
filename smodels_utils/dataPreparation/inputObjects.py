28991648690ae0df57e932a56773dbc721174568
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-04-14 16:26:11 +0200

    a priori

--- smodels_utils/dataPreparation/inputObjects.py
+++ smodels_utils/dataPreparation/inputObjects.py
@@ -410,6 +410,7 @@ class DataSetInput(Locker):
                 analysis = "x", xsection = 1. )
         ulspey = statModel.poi_upper_limit ( expected = ExpectationType.observed )/lumi
         ulspeyE = statModel.poi_upper_limit ( expected = ExpectationType.apriori )/lumi
+        # ulspeyE = statModel.poi_upper_limit ( expected = ExpectationType.aposteriori )/lumi
         #Round numbers:
         ulspey = round_list(ulspey, 3)
         ulspeyE = round_list(ulspeyE, 3)
