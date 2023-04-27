e42f2c6183a3e2a74d6fbcd150fde0f4a01e3b72
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-04-27 12:02:37 +0200

    new interface

--- truncated_gaussians/plotCorrectedLlhds.py
+++ truncated_gaussians/plotCorrectedLlhds.py
@@ -2,7 +2,7 @@
 
 from smodels.tools import runtime
 runtime._experimental = True
-from smodels.theory.theoryPrediction import theoryPredictionsFor
+from smodels.theory.theoryPrediction import theoryPredictionsFor, fiveValuesFromLimits
 from smodels.experiment.databaseObj import Database
 from smodels.theory import decomposer
 from smodels.theory.model import Model
@@ -14,7 +14,8 @@ def run():
     db = Database ( "debug" )
     anaid = "CMS-SUS-14-021"
     slhafile = "T2bbWW_111_34_111_34.slha"
-    mus = np.arange ( -1.5, 2.01, .1 )
+    mus = np.arange ( -1.5, 2.01, .03 )
+    # mus = np.arange ( -1.5, 2.01, .1 )
 
     #anaid = "ATLAS-SUSY-2013-16"
     #slhafile = "T2tt_748_271_748_271.slha"
@@ -27,17 +28,18 @@ def run():
     model = Model(BSMparticles=BSMList, SMparticles=SMList)
     model.updateParticles(inputFile=slhafile)
     toplist = decomposer.decompose(model, doCompress=True, doInvisible=True )
-    prUL = theoryPredictionsFor(erUL, toplist, combinedResults=False, marginalize=False)
-    prEff = theoryPredictionsFor(erEff, toplist, combinedResults=False, marginalize=False)
+    prUL = theoryPredictionsFor(erUL, toplist, combinedResults=False )
+    prEff = theoryPredictionsFor(erEff, toplist, combinedResults=False )
     uls, ul0s, effs = [], [], []
     for mu in mus:
-        ul = prUL[0].getLikelihood ( mu=mu )
+        ul = prUL[0].likelihood ( mu=mu )
         if ul == None:
             print ( "warning ul is none for", mu )
         uls.append ( ul )
-        ul0 = prUL[0].likelihoodFromLimits ( mu=mu, corr = 0. )
+        ret = fiveValuesFromLimits ( prUL[0], mu=mu, corr = 0. )
+        ul0 = ret["lbsm"]
         ul0s.append ( ul0 )
-        eff = prEff[0].getLikelihood ( mu=mu )
+        eff = prEff[0].likelihood ( mu=mu )
         # print ( mu, eff, ul )
         effs.append ( eff )
     suls,sul0s, seffs = sum(uls), sum(ul0s), sum(effs)
