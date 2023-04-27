b4bba1880b06e598b45c97485235a8c46aca8bb4
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-04-27 13:12:06 +0200

    adapted

--- truncated_gaussians/plotCorrectedLlhds.py
+++ truncated_gaussians/plotCorrectedLlhds.py
@@ -9,6 +9,7 @@ from smodels.theory.model import Model
 from smodels.particlesLoader import BSMList
 from smodels.share.models.SMparticles import SMList
 import numpy as np
+import sys
 
 def setup14021():
     anaid = "CMS-SUS-14-021"
@@ -20,36 +21,55 @@ def setup1316():
     anaid = "ATLAS-SUSY-2013-16"
     slhafile = "T2tt_720_240_720_240.slha"
     mus = np.arange ( -1.5, 2.01, .1 )
+    print ( "we dont have expected upper limits for that one!" )
+    return { "anaid": anaid, "slhafile": slhafile, "mus": mus }
+
+def setup16039():
+    anaid = "CMS-SUS-16-039"
+    slhafile = "TChiWZ_500_215_500_215.slha"
+    mus = np.arange ( -1.5, 2.01, .1 )
+    print ( "we dont have expected upper limits for that one!" )
+    return { "anaid": anaid, "slhafile": slhafile, "mus": mus }
+
+def setup16033():
+    anaid = "CMS-SUS-16-033"
+    slhafile = "T2tt_720_80_720_80.slha"
+    mus = np.arange ( -1., 1.51, .018 )
     return { "anaid": anaid, "slhafile": slhafile, "mus": mus }
 
 def run():
-    db = Database ( "debug+fastlim" )
+    db = Database ( "debug" )
     mus = np.arange ( -1.5, 2.01, .03 )
+    ret = setup16033()
     ret = setup14021()
-#    ret = setup1316()
     anaid, slhafile, mus = ret["anaid"], ret["slhafile"], ret["mus"]
 
     er = db.getExpResults ( analysisIDs = [ anaid ], dataTypes = [ "upperLimit" ] )
+    if er == []:
+        print ( f"could not find an upperLimit result for {anaid}" )
+        sys.exit()
     erUL = er[0]
     er = db.getExpResults ( analysisIDs = [ anaid ], dataTypes = [ "efficiencyMap" ] )
+    if er == []:
+        print ( f"could not find an efficiencyMap result for {anaid}" )
+        sys.exit()
     erEff = er[0]
     model = Model(BSMparticles=BSMList, SMparticles=SMList)
     model.updateParticles(inputFile=slhafile)
     toplist = decomposer.decompose(model, doCompress=True, doInvisible=True )
     prUL = theoryPredictionsFor(erUL, toplist, combinedResults=False )
     prEff = theoryPredictionsFor(erEff, toplist, combinedResults=False )
-    print ( "prUL", prUL, "prEff", prEff )
     uls, ul0s, effs = [], [], []
     for mu in mus:
         ul = prUL[0].likelihood ( mu=mu )
         if ul == None:
-            print ( "warning ul is none for", mu )
+            print ( f"warning: ul is None for mu={mu}. (do we have euls?)" )
         uls.append ( ul )
-        ret = fiveValuesFromLimits ( prUL[0], mu=mu, corr = 0. )
+        ret = fiveValuesFromLimits ( prUL[0], mu=mu, corr = 0.01 )
         ul0 = ret["lbsm"]
         ul0s.append ( ul0 )
         eff = prEff[0].likelihood ( mu=mu )
-        # print ( mu, eff, ul )
+        # print ( mu, eff, ul, ul0 )
         effs.append ( eff )
     suls,sul0s, seffs = sum(uls), sum(ul0s), sum(effs)
     for i in range(len(uls)):
@@ -59,11 +79,12 @@ def run():
     for i in range(len(effs)):
         effs[i] = effs[i] / seffs
     from smodels_utils.plotting import mpkitty as plt
+    print ( "mus", max(uls), max(ul0s) )
     plt.plot ( mus, uls, label = "from limits, corr=0.6" )
     plt.plot ( mus, ul0s, label = "from limits, no corr" )
     plt.plot ( mus, effs, label = "from efficiencies" )
     plt.xlabel ( r"$\mu$" )
-    plt.title ( "comparison of likelihoods" )
+    plt.title ( f"comparison of likelihoods, {anaid}" )
     plt.legend()
     plt.savefig ( "llhds.png" )
     plt.show()
