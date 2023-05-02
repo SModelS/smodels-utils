7864c2ecdf9164ef5b2804c1f31041af5dffddbd
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-05-02 15:44:15 +0200

    working on 19-006

--- truncated_gaussians/plotCorrectedLlhds.py
+++ truncated_gaussians/plotCorrectedLlhds.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from smodels.tools import runtime
+import random
 runtime._experimental = True
 from smodels.theory.theoryPrediction import theoryPredictionsFor
 from smodels.tools.statsTools import StatsComputer
@@ -40,30 +41,43 @@ def setup16033():
 def setup19006():
     anaid = "CMS-SUS-19-006"
     slhafile = "T2tt_720_80_720_80.slha"
-    mus = np.arange ( -1., 1.51, .018 )
+    mus = np.arange ( -.5, .5, .018 )
     # the signal region is SR6_Njet2_Nb2_HT500_MHT500
     # which has oUL = 2.46*fb, eUL = 1.85*fb  
     # however, we get oUL_mu = .218, eUL_mu = .24
     # we cannot combine
-    return { "anaid": anaid, "slhafile": slhafile, "mus": mus, "combined" : True }
+    combined = True
+    return { "anaid": anaid, "slhafile": slhafile, "mus": mus, "combined": combined }
 
 def normalize ( container : list ):
-    T = sum(container)
+    T = np.nansum(container)
+    if T == 0.:
+        return
     for i,c in enumerate(container):
         container[i]=c/T
     return container
 
-def run():
-    # db = Database ( "official" )
-    db = Database ( "debug" )
-    mus = np.arange ( -1.5, 2.01, .03 )
-    #ret = setup16033()
-    ret = setup14021()
-    # ret = setup19006()
-    combined = ret["combined"]
-    anaid, slhafile, mus = ret["anaid"], ret["slhafile"], ret["mus"]
+def wiggle ( container : list , r : float = .05 ):
+    T = np.nansum(container)
+    if T == 0.:
+        return
+    for i,c in enumerate(container):
+        container[i]=c*random.uniform(1-r,1+r)
+    return container
 
-    er = db.getExpResults ( analysisIDs = [ anaid ], dataTypes = [ "upperLimit" ] )
+def run():
+    dbpath = "debug" # "official"
+    dbpath = "../../smodels-database" 
+    db = Database ( dbpath )
+    #ret = setup16033()
+    # ret = setup14021()
+    ret = setup19006()
+    combined = ret["combined"]
+    mus = ret["mus"]
+    anaid, slhafile, mus = ret["anaid"], ret["slhafile"], ret["mus"]
+    anaidUL = anaid.replace("-agg","").replace("-adl","")
+
+    er = db.getExpResults ( analysisIDs = [ anaidUL ], dataTypes = [ "upperLimit" ] )
     if er == []:
         print ( f"could not find an upperLimit result for {anaid}" )
         sys.exit()
@@ -78,34 +92,38 @@ def run():
     toplist = decomposer.decompose(model, doCompress=True, doInvisible=True )
     prUL = theoryPredictionsFor(erUL, toplist, combinedResults=False )
     prEff = theoryPredictionsFor(erEff, toplist, combinedResults=combined )
-    pprint ( f"prEff is {prEff[0]} {prEff[0].dataset}, {len(prEff)}" )
+    # prEff = theoryPredictionsFor(erEff, toplist, useBestDataset = not combined, combinedResults=combined )
+    #pprint ( f"prEff is {prEff[0]} {prEff[0].dataset}, {len(prEff)} predictions" )
     uls, ul0s, effs = [], [], []
     ulsE, ul0sE, effsE = [], [], []
     computer = StatsComputer.forTruncatedGaussian ( prUL[0], corr = 0. )
-    ret = computer.get_five_values ( prUL[0] )
-    pprint ( f"truncated gaussian returned {ret}" )
+    ret = computer.get_five_values ( False )
+    # pprint ( f"truncated gaussian returned {ret}" )
+    doEfficiencies = False
     for mu in mus:
         ul = prUL[0].likelihood ( mu=mu )
         if ul == None:
             print ( f"warning: ul is None for mu={mu}. (do we have euls?)" )
         uls.append ( ul )
-        ulE = prUL[0].likelihood ( mu=mu, expected=True )
-        ulsE.append ( ulE )
         ul0 = computer.likelihood ( poi_test=mu, expected=False, return_nll=False )
         ul0s.append ( ul0 )
-        ul0E = computer.likelihood ( poi_test=mu, expected=True, return_nll=False )
-        ul0sE.append ( ul0E )
-        eff = prEff[0].likelihood ( mu=mu )
+        eff = prEff[0].likelihood ( mu=mu, nll=False )
+        print ( f"llhd for {mu} is {eff}" )
         effs.append ( eff )
-        effE = prEff[0].likelihood ( mu=mu, expected=True )
-        effsE.append ( effE )
+        if doEfficiencies:
+            ulE = prUL[0].likelihood ( mu=mu, expected=True )
+            ulsE.append ( ulE )
+            ul0E = computer.likelihood ( poi_test=mu, expected=True, return_nll=False )
+            ul0sE.append ( ul0E )
+            effE = prEff[0].likelihood ( mu=mu, expected=True )
+            effsE.append ( effE )
     for x in [ uls, ul0s, effs, ulsE, ul0sE, effsE  ]:
         normalize ( x )
     from smodels_utils.plotting import mpkitty as plt
     plt.plot ( mus, uls, label = "from limits, corr=0.6", c="r" )
     plt.plot ( mus, ul0s, label = "from limits, no corr", c="g" )
     plt.plot ( mus, effs, label = "from efficiencies", c="k" )
-    if True:
+    if doEfficiencies:
         plt.plot ( mus, ulsE, label = "from limits, corr=0.6, expected", c="r", ls="dotted" )
         plt.plot ( mus, ul0sE, label = "from limits, no corr, expected", c="g", ls="dotted" )
         plt.plot ( mus, effsE, label = "from efficiencies, expected", ls="dotted", c="k" )
