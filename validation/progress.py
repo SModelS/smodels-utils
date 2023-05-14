9cc5d64a36eb45a59a21591b86986a43146ea49a
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-05-14 16:34:17 +0200

    list turned dict

--- validation/progress.py
+++ validation/progress.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+.. module:: progress.py
+   :synopsis: a progress report of all validation jobs
+
+"""
+
 import glob, time
 from tqdm import tqdm
 
@@ -24,16 +30,16 @@ class Progress:
     def show( self ):
         self.parse()
         # self.pprint()
-        self.tqdms = []
+        self.tqdms = {}
         for i,(k,v) in enumerate ( self.stats.items() ):
             n = tqdm ( desc=k, total = v["npoints"], position = i,
                        unit = "pt", bar_format="{desc}: {percentage:3.0f}%|{bar:46}| {n_fmt}/{total_fmt} [{remaining}] {postfix}", colour = "green" )
-            self.tqdms.append ( n )
+            self.tqdms[k]=n
         while True:
             time.sleep(.3)
             self.parse()
             for i,(k,v) in enumerate ( self.stats.items() ):
-                self.tqdms[i].update ( v["ndone"] - self.tqdms[i].n )
+                self.tqdms[k].update ( v["ndone"] - self.tqdms[k].n )
 
 
 if __name__ == "__main__":
