8ed8ada5dd3e10b7d5cb86960e696ae9c670dc93
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-05-14 22:51:32 +0200

    pretty or ugly

--- validation/runValidation.py
+++ validation/runValidation.py
@@ -245,10 +245,10 @@ def run ( expResList, options : dict, keep, db ):
         if not txnames:
             logger.warning("No valid txnames found for %s (not assigned constraints?)" %str(expRes))
             continue
-        pretty = options["prettyPlots"]
-        if pretty.lower() in [ "false", "no", "0" ]:
+        pretty = str(options["prettyPlots"]).lower()
+        if pretty in [ "false", "no", "0" ]:
             pretty = False
-        elif pretty.lower() in [ "true", "yes", "1" ]:
+        elif pretty in [ "true", "yes", "1" ]:
             pretty = True
         prettyorugly = [ pretty ]
         if pretty=="both":
