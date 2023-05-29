380e530fcda31a12846e500cc06c6f636c157831
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-05-29 14:36:17 +0200

    updated ml loads

--- clip/container.sh
+++ clip/container.sh
@@ -9,10 +9,15 @@ unset LD_LIBRARY_PATH
 
 /users/wolfgan.waltenberger/copyEnv.py
 
-ml unload gcc/10.2.0 > /dev/null 2>&1
-ml unload anaconda3/2021.11 > /dev/null 2>&1
-ml unload build-env/f2021 > /dev/null 2>&1
-ml unload gcc/10.2.0 > /dev/null 2>&1
+ml unload gcc/12.2.0 > /dev/null 2>&1
+ml unload anaconda3/2022.05 > /dev/null 2>&1
+ml unload build-env/f2022 > /dev/null 2>&1
+ml unload gcc/12.2.0 > /dev/null 2>&1
 
 singularity shell --env-file /users/wolfgan.waltenberger/.containerrc -c -B /groups/hephy,/scratch-cbe/users/wolfgan.waltenberger:/local/wwaltenberger,/scratch-cbe/users/wolfgan.waltenberger:/home/walten,/scratch-cbe/users/wolfgan.waltenberger/tmp:/tmp,/run,/scratch -s /bin/bash -H /scratch-cbe/users/wolfgan.waltenberger /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
 # singularity shell --env 'PS1="[sing] \[\e[32;11m\]\h \[\e[0;33;11m\]\w> \[\033k\033\134\033 \e[37;0m\]"' -c -B /groups/hephy,/scratch-cbe/users/wolfgan.waltenberger:/local/wwaltenberger,/scratch-cbe/users/wolfgan.waltenberger:/home/walten,/scratch-cbe/users/wolfgan.waltenberger/tmp:/tmp,/run,/scratch -s /bin/bash -H /scratch-cbe/users/wolfgan.waltenberger /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
+
+ml load build-env/f2022 > /dev/null 2>&1
+ml load gcc/12.2.0 > /dev/null 2>&1
+ml load anaconda3/2022.05 > /dev/null 2>&1
+ml load gcc/12.2.0 > /dev/null 2>&1
