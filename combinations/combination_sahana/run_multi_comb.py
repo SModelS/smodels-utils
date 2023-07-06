42a23dbc93926b2f614fa05bbde8008f78683512
Sahana Narasimha <Sahana.Narasimha@oeaw.ac.at>
2023-07-06 23:05:03 +0200

    updated file

--- combinations/combination_sahana/run_multi_comb.py
+++ combinations/combination_sahana/run_multi_comb.py
@@ -24,18 +24,26 @@ import csv
 import multiprocessing
 from multiprocessing import Process
 from multiprocessing import Queue
+import time
 
 '''Note : Before running the program, make sure to enter the path of the parameter.ini file and the outputdir according to the local computer path (in runSmodels function). The program is configured for specific paths only'''
 
 
 class SModelsOutput(object):
-    def __init__(self, inputfile):
+    def __init__(self, inputfiles, queue):
         '''inputfile: slha file for which an smodels output for the best combination is needed'''
         
-        self.file = inputfile
+        self.inputfiles = inputfiles
         self.database = Database('official')
         self.expresults = self.database.getExpResults(analysisIDs='all', dataTypes=['efficiencyMap','combined'])
         self.combinationMatrix()
+        self.summary_output = []
+        
+        for file in self.inputfiles:
+            self.file = file
+            self.getBestCombination(queue)
+            
+        #queue.put(self.summary_output)
 
         
     def combinationMatrix(self):
@@ -79,10 +87,11 @@ class SModelsOutput(object):
             else:
                 self.m_nlsp = abs(d.blocks['MASS'].get(1000025))     #neutralino_3
         '''
-    def getBestCombination(self, queue):
+    def getBestCombination(self,queue):
     
         '''main function where you compute the theory pred for the model point and get best combination'''
         
+        process_st = time.process_time()
         sigmacut = 0.005*fb
         mingap = 5.*GeV
         
@@ -105,7 +114,10 @@ class SModelsOutput(object):
         if bestThPred == []:
             print("M_C1: ", self.m_c1, "\t M_N1: ", self.m_n1, "\t Combination: None")
             print("Not running SModelS on file as no tp available")
-            queue.put([filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2] + ['N/A']*7)
+            process_et = time.process_time()
+            time_process = time.strftime("%H:%M:%S", time.gmtime(process_et - process_st))
+            #self.summary_output.append([filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2] + ['N/A']*7 + [time_process])
+            queue.put([filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2] + ['N/A']*7 + [time_process])
             #print("Queue worked!")
             #return out
             #self.out.write('\n {}, \t\t {}, \t {}, \t {}, \t {}, \t {}, \t {},  \t N/A, \t \t \t N/A, \t\t\t N/A, \t N/A, \t N/A, \t N/A, \t N/A'.format(filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2))
@@ -115,7 +127,13 @@ class SModelsOutput(object):
             self.runSmodels(bestThPred, self.file)
             #self.readSModelSFile(self.file)
             self.readPythonFile(self.file)
-            queue.put([filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2, self.output_r[2], self.output_r[3], self.output_ana[-1], self.output_r[0], self.output_ana[0], self.output_r[1] ,self.output_ana[1]])
+            
+            process_et = time.process_time()
+            time_process = time.strftime("%H:%M:%S", time.gmtime(process_et - process_st))
+            #self.summary_output.append([filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2, self.output_r[2], self.output_r[3], self.output_ana[-1], self.output_r[0], self.output_ana[0], self.output_r[1] ,self.output_ana[1], time_process])
+            
+            queue.put([filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2, self.output_r[2], self.output_r[3], self.output_ana[-1], self.output_r[0], self.output_ana[0], self.output_r[1] ,self.output_ana[1], time_process])
+            
             #print("Queue worked!")
             #return out
             #self.out.write('\n {}, \t\t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t\t\t {}, \t\t\t {}, \t {}, \t {}, \t {}'.format(filename, self.m_n1, self.m_n2,self.m_c1, self.m_n3, self.m_n4, self.m_c2, self.output_r[2], self.output_r[3], self.output_ana[-1], self.output_r[0], self.output_ana[0], self.output_r[1] ,self.output_ana[1]))
@@ -138,7 +156,7 @@ class SModelsOutput(object):
         print("Running SModelS on model point for the combination")
         
         filename = file
-        outputDir = '%s/combinations/results'%(os.path.expanduser('~/smodels-utils'))
+        outputDir = '%s/combinations/combination_sahana/results'%(os.path.expanduser('~/smodels-utils'))
         #run SModelS with input file:
         output = modelTester.testPoint(filename, outputDir, parser, '2.3.0', listOfExpRes)
         
@@ -211,32 +229,35 @@ class SModelsOutput(object):
 if __name__ == "__main__":
 
     files = glob.glob ( "filter_slha/ew*slha" )
-    files = files[2]
+    files = [files[:2],files[2:4],files[4:6],files[6:8],files[8:10]]
     queue = Queue()
 
     #sms = SModelsOutput(files)
     #$sms.readPythonFile('results/ew_yzyxds4m.slha')
 
-    sms = [SModelsOutput(file) for file in files]
+    #SMS = [[SModelsOutput(file) for file in files[:10],[SModelsOutput(file) for file in files[10:20],[SModelsOutput(file) for file in files[20:30],[SModelsOutput(file) for file in files[30:]]
  
-    processes = [Process(target=sm.getBestCombination, args=(queue,)) for sm in smreas]
+   
+    processes = [Process(target=SModelsOutput, args=(file,queue)) for file in files]
     for process in processes:
         process.start()
         print("process started")
         
-    for process in processes:]
+    for process in processes:
         process.join()
         if process.exitcode !=0 : print("Error for file: " ,files[processes.index(process)])
     
     #size = queue.qsize()
     #print('\n qs ', size)
-    name = 'summary.csv'
+    name = 'summary_2.csv'
     with open('results/summary.csv','w') as out:
-        out.write('#SLHA_file \t\t M_N1 \t M_N2 \t M_C1 \t M_N3 \t M_N4 \t M_C2 \t r_obs(comb) \t r_exp(comb) \t\t\t Combination \t\t\t  \t max_r_obs \t Analysis \t max_r_exp \t Analysis')
-        for i in range(len(files)):
+        out.write('#SLHA_file\t M_N1\t M_N2\t M_C1\t M_N3\t M_N4\t M_C2\t r_obs(comb)\t r_exp(comb)\t Combination_of_Analyses\t max_r_obs\t Most_Constraining_Analysis\t max_r_exp\t Most_Senitive_Analysis\t Time Taken')
+        for i in range(10):
+            #process_item = queue.get()
+            
             item = queue.get()
             print(item)
-            out.write('\n {}, \t\t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t\t\t {}, \t\t\t {}, \t {}, \t {}, \t {}'.format(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11],item[12],item[13]))
+            out.write('\n{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11],item[12],item[13], item[14]))
         
         
             #sm.getBestCombination(file)
