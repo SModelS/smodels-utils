a2dfa39c590a25f371283f5320cefefafde27ae7
APMDSLHC <timothee.pascal@lpsc.in2p3.fr>
2023-07-10 00:05:39 +0200

    path fix

--- combinations/combination_tim/runFullScan_23.sh
+++ combinations/combination_tim/runFullScan_23.sh
@@ -3,7 +3,7 @@
 source ~/.bashrc
 
 callmyprogram(){
-  python -i -c "import runFullScan;runFullScan.main('2ndFilter_slha_nlo_far_r_23',23,'outputFullScan_nlo_2p3_mmg05_far_r_23')"
+  python -i -c "import runFullScan;runFullScan.main('2ndFilter_slha_nlo/2ndFilter_slha_nlo_far_r/2ndFilter_slha_nlo_far_r_23',23,'outputFullScan_nlo_2p3_mmg05_far_r_23')"
 }
 
 callmyprogram
