e1f4af1e4698c51d92efa05c3a392979b30ee518
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-09-06 14:18:48 +0200

    merged

--- clip/container.sh
+++ clip/container.sh
@@ -17,7 +17,7 @@ ml unload gcc/10.2.0
 ml unload build-env/f2021
 ml unload anaconda3/2021.11
 
-singularity shell --env-file /users/wolfgan.waltenberger/.containerrc -c -B /groups/hephy,/scratch-cbe/users/wolfgan.waltenberger:/local/wwaltenberger,/scratch-cbe/users/wolfgan.waltenberger:/home/walten,/scratch-cbe/users/wolfgan.waltenberger/tmp:/tmp,/run,/scratch -s /bin/bash -H /scratch-cbe/users/wolfgan.waltenberger /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
+singularity shell --cleanenv --env-file /users/wolfgan.waltenberger/.containerrc -c -B /groups/hephy,/scratch-cbe/users/wolfgan.waltenberger:/local/wwaltenberger,/scratch-cbe/users/wolfgan.waltenberger:/home/walten,/scratch-cbe/users/wolfgan.waltenberger/tmp:/tmp,/run,/scratch -s /bin/bash -H /scratch-cbe/users/wolfgan.waltenberger /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
 # singularity shell --env 'PS1="[sing] \[\e[32;11m\]\h \[\e[0;33;11m\]\w> \[\033k\033\134\033 \e[37;0m\]"' -c -B /groups/hephy,/scratch-cbe/users/wolfgan.waltenberger:/local/wwaltenberger,/scratch-cbe/users/wolfgan.waltenberger:/home/walten,/scratch-cbe/users/wolfgan.waltenberger/tmp:/tmp,/run,/scratch -s /bin/bash -H /scratch-cbe/users/wolfgan.waltenberger /scratch-cbe/users/wolfgan.waltenberger/container/current.simg
 type -t ml 1>/dev/null && ml -q load build-env/f2022
 type -t ml 1>/dev/null && ml load anaconda3/2023.03
