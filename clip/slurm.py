d80e7f059fadbee1c25098110b3b0da9e7260179
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-10-23 16:27:08 +0200

    setup

--- clip/slurm.py
+++ clip/slurm.py
@@ -12,15 +12,24 @@ def queryStats ( maxsteps = None ):
             running_stats.running_stats()
             print()
 
+def describeSetup():
+    import scipy
+    print ( f"scipy {scipy.__version__}" )
+
 def main():
     import argparse
     argparser = argparse.ArgumentParser(description="slurm-run a walker")
     argparser.add_argument ( '-q','--query',
             help='query status, dont actually run (use -M to query repeatedly)',
             action="store_true" )
+    argparser.add_argument ( '-s','--setup',
+            help='check the setup',
+            action="store_true" )
     args=argparser.parse_args()
     if args.query:
         queryStats ( )
+    if args.setup:
+        describeSetup()
 
 if __name__ == "__main__":
     main()
