f6f7c27e61b241cb2a12f33e3377e2a6321a7d5e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-12-07 12:12:58 +0100

    no pcl file

--- validation/runValidation.py
+++ validation/runValidation.py
@@ -457,7 +457,8 @@ def main(analysisIDs,datasetIDs,txnames,dataTypes,kfactorDict,slhadir,databasePa
                        subpickle = True )
         if not "validation.pcl" in buPath: # ok so we create a new pickle
             currentPickle = os.path.join ( buPath, "db30.pcl" )
-            if os.path.exists ( currentPickle ) and True:
+            if os.path.exists ( currentPickle ) and False:
+                # per default we do not do this
                 shutil.copyfile ( currentPickle, os.path.join ( databasePath, "validation.pcl" ) )
     except Exception as e:
         logger.error("Error loading database at %s" % ( databasePath ) )
@@ -649,8 +650,8 @@ if __name__ == "__main__":
                 "pngPlots": True, ## also png plots?
                 "recordPlotCreation": False, ## record the plot creation?
                 "pdfPlots": True, ## also pdf plots?
-                # "expectationType": "posteriori",
-                "expectationType": "prior", # the expectation type used for eULs
+                "expectationType": "posteriori",
+                # "expectationType": "prior", # the expectation type used for eULs
                 "minmassgap": 2.0, ## the min mass gap in SModelS
                 "sigmacut": 0.000000001, ## sigmacut in SModelS
                 "maxcond": 1.0, ## maximum allowed condition violation in SModelS
