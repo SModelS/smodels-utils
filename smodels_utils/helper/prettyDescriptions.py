49577e408fa0f6503a64658c67885956a6bf5577
Andre Lessa <lessa.a.p@gmail.com>
2024-01-18 16:30:40 -0300

    Added TRPVM2jjj

--- smodels_utils/helper/prettyDescriptions.py
+++ smodels_utils/helper/prettyDescriptions.py
@@ -297,7 +297,8 @@ decayDict = { 'T1': 'gluino  --> quark antiquark  lsp ' ,
     'TRV1': 'ZPrime --> 2*MET',
     'TRV1jj': 'ZPrime --> 2*MET',
     'TRS1': 'H0 --> 2*MET',
-    'TRPVM1jjj' : 'chargino^mp_1/neutralino_2/neutralino_1 --> q q q'
+    'TRPVM1jjj' : 'chargino^mp_1/neutralino_2/neutralino_1 --> quark antiquark quark',
+    'TRPVM2jjj' : 'gluino --> neutralino_1 quark antiquark, neutralino_1 --> quark antiquark quark'    
 }
 
 #Name of mother particles
@@ -504,7 +505,8 @@ motherDict = {"T1" :  "gluino",
     "TRS1": "H0",
     "TRV1": "ZPrime",
     "TRV1jj": "ZPrime",
-    "TRPVM1jjj" : "chargino^mp_1/neutralino_2/neutralino_1 chargino^mp_1/neutralino_2/neutralino_1"
+    "TRPVM1jjj" : "chargino^mp_1/neutralino_2/neutralino_1 chargino^mp_1/neutralino_2/neutralino_1",
+    "TRPVM2jjj" : "gluino"    
 }
 
 
