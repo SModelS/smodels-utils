74a17ffce41179746a26fe12079bbf6b646a9655
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-01-25 13:30:00 +0100

    refactor(slurm_walk.py): improve readability and maintainability of the code by adding line breaks and aligning arguments in the updateHiscores.loop function call refactor(slurm_walk.py): improve cleanliness and clarity of the clean_dirs function by breaking the long command into multiple lines and adding comments to explain the purpose of each line

--- clip/slurm_walk.py
+++ clip/slurm_walk.py
@@ -351,8 +351,8 @@ def runUpdater( dry_run : bool, time : float, rundir : os.PathLike,
         f.write ( "from ptools import updateHiscores\n" )
         f.write ( 'batchjob="SLURM_JOBID" in os.environ\n' )
         f.write ( f'did_combine=updateHiscores.didCombine ( rundir )\n' )
-        f.write ( f"updateHiscores.loop ( rundir=rundir,\n" )
-        f.write ( f"    maxruns={maxiterations}, doPlots=not batchjob, uploadTo='{uploadTo}', do_combine=did_combine, dbpath='{dbpath}' )\n" )
+        f.write ( f"updateHiscores.loop ( rundir=rundir, maxruns={maxiterations}, createPlots=not batchjob,\n" )
+        f.write ( f"    uploadTo='{uploadTo}', do_combine=did_combine, dbpath='{dbpath}' )\n" )
     os.chmod( runner, 0o755 ) # 1877 is 0o755
     cmd = [ "sbatch", "--mem", "25G" ]
     if maxiterations > 5:
@@ -383,7 +383,7 @@ def clean_dirs( rundir, clean_all = False, verbose=True ):
     cmd = "cd %s; rm -rf old*hi .*slha H*hi ssm*pcl *old *png decays* states.dict hiscore.hi Kold.conf Zold.conf RUN* xsec* llhdscanner*sh walker*log $OUTPUTS" % rundir
     if clean_all:
         # cmd = "cd %s; rm -rf old*pcl H*hi hiscore*hi .cur* .old* .tri* .*slha M*png history.txt pmodel-*py pmodel.py llhd*png decays* RUN*.sh ruler* rawnumb* *tex hiscore.log hiscore.slha *html *png *log RUN* walker*log training*gz Kold.conf Zold.conf ../outputs/slurm-*.out" % rundir
-        cmd = f"cd {rundir}; rm -rf old*pcl H*hi hiscore*hi .cur* .old* .tri* .*slha M*png history.txt pmodel-*py pmodel.py llhd*png decays* RUN*.sh ruler* rawnumb* *tex hiscore.log hiscore.slha *html *png *log RUN* walker*log training*gz Kold.conf Zold.conf xsec* llhdscanner*sh hiscores.dict Kold.conf Kmin.conf old_hiscore.hi log.txt run.dict $(OUTPUTS)/walk-*.out"
+        cmd = f"cd {rundir}; rm -rf old*pcl H*hi hiscore*hi .cur* .old* .tri* .*slha M*png history.txt pmodel-*py pmodel.py llhd*png decays* RUN*.sh ruler* rawnumb* *tex hiscore.log hiscore.slha *html *png *log RUN* walker*log training*gz Kold.conf Zold.conf xsec* llhdscanner*sh hiscores.dict Kold.conf Kmin.conf old_hiscore.hi log.txt run.dict llhd*pcl L*sh S*sh $(OUTPUTS)/walk-*.out"
     if verbose:
         print ( "[slurm.py] %s" % cmd )
     o = subprocess.getoutput ( cmd )
