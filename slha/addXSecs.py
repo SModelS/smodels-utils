2f6028d5a24a6fb1cbb0dda7115e5459dbbe69c3
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-03-20 14:15:31 +0100

    updated T2

--- slha/addXSecs.py
+++ slha/addXSecs.py
@@ -30,7 +30,7 @@ def unlock ( f ):
         f = f[p1+1:]
     cmd = f"rm -rf .{f}.lock"
     subprocess.getoutput ( cmd )
-    
+
 def isLocked ( f ):
     if "/" in f:
         p1 = f.rfind("/")
@@ -39,6 +39,8 @@ def isLocked ( f ):
 
 def addXSecs( dirname, pretend = False, parallel : bool = False ):
     files = [ dirname ]
+    if "*" in dirname:
+        files = glob.glob ( dirname )
     if os.path.isdir ( dirname ):
         files = glob.glob( dirname+"T*slha" )
     ctr=0
@@ -47,15 +49,15 @@ def addXSecs( dirname, pretend = False, parallel : bool = False ):
     for f in files:
         has = hasXSecs ( f )
         if has:
-            print ( f"[addXSecs] skipping {f}: has xsecs" ) 
             hasXS += 1
+            print ( f"[addXSecs] skipping ({hasXS}) {f}: has xsecs" )
             continue
         if isLocked ( f ):
-            print ( f"[addXSecs] skipping {f}: is locked" ) 
             locked += 1
+            print ( f"[addXSecs] skipping ({locked}) {f}: is locked" )
             continue
         else:
-            print ( f"[addXSecs] computing for {f}" ) 
+            print ( f"[addXSecs] computing for {f}" )
             ctr+=1
             if ctr < 40:
                 cmd = f"../../smodels/smodelsTools.py xseccomputer -f {f} -8 -N -P -e 200000 -v info -c 1 -s 8 13 "
@@ -64,7 +66,7 @@ def addXSecs( dirname, pretend = False, parallel : bool = False ):
                     pid = os.fork()
                 if pid == 0:
                     lock ( f )
-                    print ( "cmd", cmd )
+                    print ( f"[addXSecs] cmd: {cmd}" )
                     o = ""
                     if not pretend:
                         o = subprocess.getoutput ( cmd )
@@ -75,7 +77,5 @@ def addXSecs( dirname, pretend = False, parallel : bool = False ):
     # print ( f"{ctr}/{len(files)} processing, {hasXS} have xsecs, {locked} locked" )
 
 if __name__ == "__main__":
-    files = glob.glob ( "T*.slha" )
-    # files = glob.glob ( "tmp*/" )
-    for f in files:
-        addXSecs( f, pretend = False, parallel = False )
+    addXSecs( "T*.slha", pretend = False, parallel = False )
+#        addXSecs( "tmp*/T*.slha", pretend = False, parallel = False )
