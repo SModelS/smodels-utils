d35d384ac673c22b54c6864595fa77cfcf2e0636
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-03-20 13:53:30 +0100

    updated addxsecs needed it for T2

--- slha/addXSecs.py
+++ slha/addXSecs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+""" add xsecs to slha files """
+
 import glob, os, subprocess, sys
 
 def hasXSecs( filename ):
@@ -8,7 +10,9 @@ def hasXSecs( filename ):
         f.close()
     for line in lines:
         if line.startswith ( "XSEC" ):
-            return True
+            # check for 13 tev xsecs
+            if " 1.30E+04 " in line or " 13000 " in line:
+                return True
     return False
 
 def lock ( f ):
@@ -33,23 +37,31 @@ def isLocked ( f ):
         f = f[p1+1:]
     return os.path.exists ( f".{f}.lock" )
 
-def addXSecs( dirname, pretend = False ):
-    files = glob.glob( dirname+"T*slha" )
+def addXSecs( dirname, pretend = False, parallel : bool = False ):
+    files = [ dirname ]
+    if os.path.isdir ( dirname ):
+        files = glob.glob( dirname+"T*slha" )
     ctr=0
     hasXS = 0
     locked = 0
     for f in files:
-        if hasXSecs ( f ):
+        has = hasXSecs ( f )
+        if has:
+            print ( f"[addXSecs] skipping {f}: has xsecs" ) 
             hasXS += 1
             continue
         if isLocked ( f ):
+            print ( f"[addXSecs] skipping {f}: is locked" ) 
             locked += 1
             continue
         else:
+            print ( f"[addXSecs] computing for {f}" ) 
             ctr+=1
             if ctr < 40:
-                cmd = f"../../smodels/smodelsTools.py xseccomputer -f {f} -8 -N -P -e 200000 -v info -c 1 -s 13.6 "
-                pid = os.fork()
+                cmd = f"../../smodels/smodelsTools.py xseccomputer -f {f} -8 -N -P -e 200000 -v info -c 1 -s 8 13 "
+                pid = 0
+                if parallel:
+                    pid = os.fork()
                 if pid == 0:
                     lock ( f )
                     print ( "cmd", cmd )
@@ -58,10 +70,12 @@ def addXSecs( dirname, pretend = False ):
                         o = subprocess.getoutput ( cmd )
                     print ( "o", o )
                     unlock ( f )
-                    sys.exit()
-    print ( f"{ctr}/{len(files)} processing, {hasXS} have xsecs, {locked} locked" )
+                    if parallel:
+                        sys.exit()
+    # print ( f"{ctr}/{len(files)} processing, {hasXS} have xsecs, {locked} locked" )
 
 if __name__ == "__main__":
-    files = glob.glob ( "tmp*/" )
+    files = glob.glob ( "T*.slha" )
+    # files = glob.glob ( "tmp*/" )
     for f in files:
-        addXSecs( f, pretend = False )
+        addXSecs( f, pretend = False, parallel = False )
