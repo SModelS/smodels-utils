73768744a91c680fa0dec2ec85ca8793177d8ad3
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-04-19 16:09:05 +0200

    towards cms-sus-19-012

---
+++ smodels_utils/dataPreparation/pdfconfigs/CMS-SUS-19-012-Figure-019.py
@@ -0,0 +1,9 @@
+{
+    'name': 'CMS-SUS-19-012_Figure_019',
+    'x':{'limits': (100, 800, 20) },
+    # 'y':{'limits': (0, 3000, 25) },
+    # 'y':{'limits': (125, 2275, 25) },
+    'y':{'limits': (0, 600, 25) },
+    'z':{'limits': ( 1., 2e3 ), 'log':True},
+    'topology': 'TChiWZ'
+}
