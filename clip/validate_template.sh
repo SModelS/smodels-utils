aa9f980b14e367cdfe48bf326cf52ca2913c1483
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-04-19 12:07:13 +0200

    change to matching texlive

--- clip/validate_template.sh
+++ clip/validate_template.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 ## this is the validation script for @@ANALYSES@@ @@TOPO@@
 ## the template inifile was @@ORIGINIFILE@@
 
 SCRIPT=$(readlink -f $0)
 
-ml load texlive/20210324-gcccore-10.2.0
+# ml load texlive/20210324-gcccore-10.2.0
+ml load texlive/20220321-gcc-12.2.0
 
 cd /scratch-cbe/users/wolfgan.waltenberger/git/smodels-utils/validation
 ./runValidation.py -p @@INIFILE@@ @@KEEP@@
