69823e82e243ea713ce0cca0f7f7ea2f11addd99
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-07-02 10:24:17 +0200

    run_smodels

---
+++ gambit/run_smodels.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" a first version of a backend for gambit """
+
+import os
+
+def run_smodels ( slhafile : os.PathLike ):
+    pass
+
+if __name__ == "__main__":
+    import argparse
+    run_smodels ( slhafile )
