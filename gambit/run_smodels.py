cae3bf6925b78c026df5ae81f73ca881d7a9f1c8
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-07-02 18:33:42 +0200

    dict

--- gambit/run_smodels.py
+++ gambit/run_smodels.py
@@ -3,11 +3,20 @@
 """ a first version of a SModelS backend for gambit/colliderbit """
 
 import os
+from typing import Dict
 
-def run_smodels ( slhafile : os.PathLike ):
+def run_smodels ( slhafile : os.PathLike ) -> Dict:
+    """ given the path to an slha file, this little script runs
+    SModelS, returns the results as dictionary.
+
+    :returns: dictionary with analysis Ids as keys, and (negative log) 
+    likelihoods, and r-values 
+    (r := predicted cross section / upper limit on cross section ) 
+    as values
+    """
     if not os.path.exists ( slhafile ):
         print ( f"[run_smodels] slha file {slhafile} not found" )
-        return
+        return {}
     from smodels.base.model import Model
     from smodels.share.models.SMparticles import SMList
     from smodels.particlesLoader import load
@@ -42,10 +51,11 @@ def run_smodels ( slhafile : os.PathLike ):
             tp_dict["nll_SM"]=theoryPrediction.lsm ( return_nll = True )
             tp_dict["nll_min"]=theoryPrediction.lmax ( return_nll = True )
         return_dict[tpId]=tp_dict
-
     return return_dict
 
+
 if __name__ == "__main__":
+    ## simple executable to run this
     import argparse
     ap = argparse.ArgumentParser(description=\
             "first candidate for gambit/colliderbit SModelS backend")
