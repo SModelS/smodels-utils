b1e8ad775b5533bbd33f506e5b12b27645285297
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-07-10 13:45:10 +0200

    x

--- validation/createWikiPage.py
+++ validation/createWikiPage.py
@@ -149,7 +149,7 @@ the SMS results database that can be validated against official results.
 %s. The list has been created from the
 database version %s, including the Fastlim tarball that is shipped separately.
 There is also a [list of all analyses](ListOfAnalyses%s), 
-a list of [all SMS topologies](SmsDictionary%s), and an explicit [comparison](ExclComparison%s) of best-SR vs SR exclusion lines.
+a list of [all SMS topologies](SmsDictionary%s), and an explicit [comparison of best-SR vs combined-SR](ExclComparison%s) exclusion lines.
 
 The validation procedure for upper limit maps used here is explained in [arXiv:1312.4175](http://arxiv.org/abs/1312.4175),  [EPJC May 2014, 74:2868](http://link.springer.com/article/10.1140/epjc/s10052-014-2868-5), section 4. For validating efficiency maps, a very similar procedure is followed. For every input point, the best signal region is chosen. If a covariance matrix has been published, we present the combined limit of all signal regions. The experimental upper limits are compared with the theoretical predictions for that signal region.
 
