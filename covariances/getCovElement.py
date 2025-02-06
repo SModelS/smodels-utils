dea86bbfc75a93b06dd061ea9019340681c1129c
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-02-06 16:30:04 +0100

    working

--- covariances/getCovElement.py
+++ covariances/getCovElement.py
@@ -4,7 +4,7 @@
 
 from smodels.experiment.databaseObj import Database
 from smodels.base.smodelsLogging import setLogLevel
-import math
+import math, sys
 
 def getVariance( expRes, srName : str ):
     """ retrieve the variance of srName in expRes """
@@ -12,12 +12,13 @@ def getVariance( expRes, srName : str ):
     cov = expRes.globalInfo.covariance
     poses = [ i for i,x in enumerate(dsOrder) if x == srName ]
     pos = poses[0]
-    print ( dsOrder )
-    print ( f"we want the {pos} element" )
-    print ( f"cov({pos},{pos})={cov[pos][pos]}" )
-    print ( f"sigma({pos},{pos})={math.sqrt(cov[pos][pos])}" )
+    print ( f"dsOrder={dsOrder}" )
+    print ( f"we want the {pos}th element ({srName} of {expRes.globalInfo.id})" )
+    print ( f"obs({pos})={expRes.datasets[pos].dataInfo.observedN}" )
+    print ( f"cov({pos},{pos})={cov[pos][pos]:.3f}" )
+    print ( f"sigma({pos},{pos})={math.sqrt(cov[pos][pos]):.3f}" )
 
-def get():
+def getExample():
     anaId =  "CMS-SUS-21-002" 
     sr = "H_SR0"
     setLogLevel("info")
@@ -26,6 +27,25 @@ def get():
                         dataTypes = [ "efficiencyMap" ] )
     print ( f"{len(e)} results" )
     getVariance ( e[0], sr )
+
+def get():
+    import argparse
+    ap = argparse.ArgumentParser( description="retrieve individual covariance matrix entries" )
+    ap.add_argument('-s', '--sr', type=str, default="H_SR0",
+                    help='signal region name [H_SR0]' )
+    ap.add_argument('-a', '--analysisId', type=str, default="CMS-SUS-21-002",
+                    help='analysis id [CMS-SUS-21-002]' )
+    ap.add_argument('-d', '--dbpath', type=str, default="../../smodels-database",
+                    help='database path [../../smodels-database]' )
+    args=ap.parse_args()
+    d = Database ( args.dbpath )
+    e=d.getExpResults ( analysisIDs = [ args.analysisId ], 
+                        dataTypes = [ "efficiencyMap" ] )
+    if len(e) == 0:
+        print ( f"could not find {args.analysisId} in {args.dbpath}" )  
+        sys.exit()
+
+    getVariance ( e[0], args.sr )
     
 if __name__ == "__main__":
     get()
