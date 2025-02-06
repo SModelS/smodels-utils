e4ee02a6389dcbafe3587e4d317f5d0a680ab176
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-02-06 16:18:52 +0100

    improving getCovElement

--- covariances/getCovElement.py
+++ covariances/getCovElement.py
@@ -6,6 +6,17 @@ from smodels.experiment.databaseObj import Database
 from smodels.base.smodelsLogging import setLogLevel
 import math
 
+def getVariance( expRes, srName : str ):
+    """ retrieve the variance of srName in expRes """
+    dsOrder = expRes.globalInfo.datasetOrder
+    cov = expRes.globalInfo.covariance
+    poses = [ i for i,x in enumerate(dsOrder) if x == srName ]
+    pos = poses[0]
+    print ( dsOrder )
+    print ( f"we want the {pos} element" )
+    print ( f"cov({pos},{pos})={cov[pos][pos]}" )
+    print ( f"sigma({pos},{pos})={math.sqrt(cov[pos][pos])}" )
+
 def get():
     anaId =  "CMS-SUS-21-002" 
     sr = "H_SR0"
@@ -14,15 +25,7 @@ def get():
     e=d.getExpResults ( analysisIDs = [ anaId ], 
                         dataTypes = [ "efficiencyMap" ] )
     print ( f"{len(e)} results" )
-    dsOrder = e[0].globalInfo.datasetOrder
-    cov = e[0].globalInfo.covariance
-    poses = [ i for i,x in enumerate(dsOrder) if x == sr ]
-    pos = poses[0]
-    print ( dsOrder )
-    print ( f"we want the {pos} element" )
-    print ( f"cov({pos},{pos})={cov[pos][pos]}" )
-    print ( f"sigma({pos},{pos})={math.sqrt(cov[pos][pos])}" )
+    getVariance ( e[0], sr )
     
-
 if __name__ == "__main__":
     get()
