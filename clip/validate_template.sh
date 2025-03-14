a783fc3cf4b1192b52fd4e1b0c9d19732085883e
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-03-14 16:31:44 +0100

    remove hard coded path

--- clip/validate_template.sh
+++ clip/validate_template.sh
@@ -10,7 +10,7 @@ SCRIPT=$(readlink -f $0)
 # ml load texlive/20210324-gcccore-10.2.0
 ml load texlive/20220321-gcc-12.2.0
 
-cd /scratch-cbe/users/$USER/git/smodels-utils/validation
+cd @@CODEDIR@@/smodels-utils/validation
 ./runValidation.py -p @@INIFILE@@ @@KEEP@@
 # sleep 5
 # rm @@INIFILE@@
