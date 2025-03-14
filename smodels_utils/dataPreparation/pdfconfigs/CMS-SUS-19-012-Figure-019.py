e8e11f531c0be4653183e7a08fefd74e2d198a4b
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-03-14 09:30:07 +0100

    mod

--- smodels_utils/dataPreparation/pdfconfigs/CMS-SUS-19-012-Figure-019.py
+++ smodels_utils/dataPreparation/pdfconfigs/CMS-SUS-19-012-Figure-019.py
@@ -3,7 +3,7 @@
     'x':{'limits': (100, 800, 20) },
     # 'y':{'limits': (0, 3000, 25) },
     # 'y':{'limits': (125, 2275, 25) },
-    'y':{'limits': (0, 600, 25) },
+    'y':{'limits': (0, 600, 20) },
     'z':{'limits': ( 1., 2e3 ), 'log':True},
     'topology': 'TChiWZ'
 }
