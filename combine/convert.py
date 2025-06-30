c521033b9a8ee809ea4fafca87c48e4077396211
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-06-30 17:21:09 +0200

    rates

--- combine/convert.py
+++ combine/convert.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+__all__ = [ "DatacardConverter", "main" ]
+
 """
 .. module:: convertCountingAnalysis.py
    :synopsis: Here I am attempting to write a combine2pyhf converter for counting-only analyses. lets see.
@@ -17,17 +19,53 @@ class DatacardConverter:
         print ( f"converting {datacard}" )
         import DatacardParser
         with open( datacard, 'r') as f:
-            opts = type("opts", (object,), dict(bin=True, noJMax=False, stat=False, nuisancesToExclude=[], allowNoSignal=True, allowNoBackground=True))
+            opts = type("opts", (object,), dict(bin=True, noJMax=False, stat=False, nuisancesToExclude=[], 
+                                                allowNoSignal=True, allowNoBackground=True))
             self.datacard = DatacardParser.parseCard(f, opts)
-            signalRegions = list ( self.datacard.obs.keys() )
-            signalRegions.sort()
+            # signalRegions = list ( self.datacard.obs.keys() )
+            # signalRegions.sort() # alphabetical order?
+            signalRegions = self.datacard.bins # keep order from combine?
             self.signalRegions = signalRegions
 
+    def writeMeasurements ( self ):
+        """ write the measurement section of the json file """
+        self.fhandle.write ( """    "measurements": [
+        {
+            "config": {
+                "parameters": [
+                    {
+                        "auxdata": [
+                            1.0
+                        ],
+                        "bounds": [
+                            [
+                                0.915,
+                                1.085
+                            ]
+                        ],
+                        "fixed": true,
+                        "inits": [
+                            1.0
+                        ],
+                        "name": "lumi",
+                        "sigmas": [
+                            0.017
+                        ]
+                    }
+                ],
+                "poi": "mu_SIG"
+            },
+            "name": "NormalMeasurement"
+        }
+    ],
+""" )
 
     def writeObservations ( self ):
+        """ write out the observed data """
         self.fhandle.write ( '    "observations": [\n' )
         for i,sr in enumerate(self.signalRegions):
-            value = int ( self.datacard.obs[sr] )
+            exps = self.datacard.exp[sr]
+            value = exps["sig"]
             self.fhandle.write ( '        {\n' )
             self.fhandle.write ( '            "data": [\n' )
             self.fhandle.write ( f'                {value}\n' )
@@ -37,10 +75,30 @@ class DatacardConverter:
             self.fhandle.write ( f'        }}{comma}\n' )
         self.fhandle.write ( '    ],\n' )
 
+    def writeChannels ( self ):
+        """ write out the channel info """
+        self.fhandle.write ( '    "channels": [\n' )
+        for i,sr in enumerate(self.signalRegions):
+            value = int ( self.datacard.obs[sr] )
+            self.fhandle.write ( '        {\n' )
+            self.fhandle.write ( f'            "name": "{sr}",\n' )
+            self.fhandle.write ( '            "samples": [ \n' )
+            self.fhandle.write ( '                {\n' )
+            self.fhandle.write ( '                    "data": [\n' )
+            self.fhandle.write ( f'                        {value}\n' )
+            self.fhandle.write ( '                    ]\n' )
+            self.fhandle.write ( '                }\n' )
+            self.fhandle.write ( '            ]\n' )
+            comma = "," if i+1 < len(self.datacard.obs) else ""
+            self.fhandle.write ( f'        }}{comma}\n' )
+        self.fhandle.write ( '    ],\n' )
+
     def writeJsonFile ( self, outfile : os.PathLike ):
         self.outfile = outfile
         self.fhandle = open ( outfile, "w" )
         self.fhandle.write ( "{\n" )
+        self.writeChannels()
+        self.writeMeasurements()
         self.writeObservations()
         self.fhandle.write ( '    version: "1.0.0"\n' )
         self.fhandle.write ( "}\n" )
