16223b6e946f4ecb35c9df02eabf880fcd77e0c9
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-07-10 19:48:54 +0200

    only write our observed and self.scale == 1

--- pyhfintegration/patchesFromSModelS.py
+++ pyhfintegration/patchesFromSModelS.py
@@ -2,21 +2,21 @@
 
 """
 .. module:: patchesFromSModelS
-   :synopsis: neat trick to patch into PyhfUpperLimitComputer to force it
+   :synopsis: neat trick to hack into PyhfUpperLimitComputer to force it
    to write out the patched json files. use like runSModelS.py!
 
 .. moduleauthor:: Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
 """
 
-from smodels.statistics.pyhfInterface import PyhfUpperLimitComputer
+from smodels.statistics.pyhfInterface import PyhfUpperLimitComputer, PyhfData
 
-def writePatchedJsons ( computer : PyhfUpperLimitComputer, workspaces ) -> bool:
+def writePatchedJsons ( data : PyhfData, workspaces ) -> bool:
     """ debug method to write out patched json files.
     :param computer: the upper limit computer
     :returns: true if something was written
     """
     import json
-    for workspace,(jsonFile,SRs) in zip(workspaces,computer.data.jsonFiles.items()):
+    for workspace,(jsonFile,SRs) in zip(workspaces,data.jsonFiles.items()):
         filename = jsonFile.replace(".json","_patched.json")
         # filename = f"{self.data.globalInfo.id}_{filename}"
         print ( f"[patchesFromSModelS] dumping patched version of {jsonFile} to {filename}" )
@@ -28,9 +28,18 @@ def writePatchedJsons ( computer : PyhfUpperLimitComputer, workspaces ) -> bool:
 
 oldWsMaker = PyhfUpperLimitComputer.wsMaker
 
-def newWsMaker ( self, apriori = False ): 
+def newWsMaker ( self, apriori : bool = False ) -> list: 
+    """ this is our replacement wsMaker method. it calls the old wsMaker method,
+    but then (potentially) writes out the workspaces.
+    """
+    if apriori:
+        # for now lets not write out the a priori versions
+        return
     ws = oldWsMaker ( self, apriori )
-    writePatchedJsons ( self, ws )
+    if not hasattr ( self, "scale" ): #  or abs ( self.scale - 1 ) < 1e-5:
+        # we want to write out only the first patched version, the others are due to rescaling
+        # (i think)
+        writePatchedJsons ( self.data, ws )
     return ws
 
 PyhfUpperLimitComputer.wsMaker = newWsMaker
