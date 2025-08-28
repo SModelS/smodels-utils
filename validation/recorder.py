6bb992c82c1003b5e9a1cf33193ac0e7a417a37d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2025-08-28 15:24:48 +0200

    dont git-commit recorder.py

--- validation/recorder.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#
-# a python script that recorded the plotting statements,
-# so we can reproduce the plotting
-
-from matplotlib import pyplot as plt
-from plottingFuncs import getColormap
-import numpy as np
-from numpy import array
