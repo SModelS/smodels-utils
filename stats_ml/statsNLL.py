c97656a6e77022f6f4a8d490c3afb815401f4c63
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-04-21 12:27:06 +0200

    up

--- stats_ml/statsNLL.py
+++ stats_ml/statsNLL.py
@@ -11,6 +11,7 @@ sys.path.insert(0,"../../" )
 from smodels.share.models.SMparticles import SMList
 from smodels.matching.theoryPrediction import theoryPredictionsFor
 from smodels.statistics.basicStats import observed, apriori, aposteriori
+from smodels.statistics.nnInterface import NNUpperLimitComputer
 import warnings
 
 warnings.filterwarnings(
@@ -38,7 +39,7 @@ def createSLHAFile() -> os.PathLike:
     masses = { 1000022: mLSP, 1000023: mC1,
                1000024: mC1 }
     decays = { 1000024: { ( 1000022, 24 ): 1 },
-               1000023: { ( 1000022, 23 ): 1 },
+               1000023: { ( 1000022, 23 ): .5, ( 1000022, 25 ): .5 },
                1000022: {} }
     ssms = { ( 1000023, 1000024 ) : 1, (-1000024, 1000023 ) : 1,
              ( 1000023, 1000023 ) : 1, (-1000024, 1000024 ) : 1 }
@@ -68,7 +69,7 @@ def readStats():
 
 def createOnePoint( db ):
     s = createSLHAFile()
-    print ( f"[statsNLL] created for {s['key']}" )
+    print ( f"[statsNLL] starting {s['key']}" )
     slhafile = s["file"]
     runtime.modelFile = "smodels.share.models.mssm"
     BSMList = load()
@@ -76,15 +77,17 @@ def createOnePoint( db ):
     model.updateParticles(inputFile=slhafile,
             ignorePromptQNumbers = ['eCharge','colordim','spin'])
 
-    topDict = decomposer.decompose(model, sigmacut=0.001,
+    topDict = decomposer.decompose(model, sigmacut=0.00,
                            massCompress=True, invisibleCompress=True,
                            minmassgap=5*GeV)
-    print ( f"[statsNLL] decomposed to {len(topDict)} elements" )
+    key = list(topDict.keys())[0]
+    print ( f"[statsNLL] decomposed to {len(topDict[key])} elements" )
     t0 = time.time()
     allPreds = theoryPredictionsFor( db, topDict, 
             combinedResults=True )
     t1 = time.time()
-    print ( f"[statsNLL] we have {len(allPreds)} predictions from ML in {t1-t0:.2f}s" )
+    sanas = ", ".join ( [ x.dataset.globalInfo.id for x in allPreds ] )
+    print ( f"[statsNLL] found {len(allPreds)} preds in {t1-t0:.1f}s: {sanas}" )
     res = {}
     for p in allPreds:
         if p.dataType() != "combined":
@@ -93,38 +96,45 @@ def createOnePoint( db ):
         anaId = p.dataset.globalInfo.id
         isOrig = True if "-orig" in anaId else False
         nlls = { }
-        anaId = anaId.replace("-orig","")
+        # print ( f"@@X anaId is {anaId} computer is {type(p.statsComputer.upperLimitComputer)} isNN {type(p.statsComputer.upperLimitComputer)==NNUpperLimitComputer}" )
         if isOrig:
             nlls["orig"]=nll
         else:
             nlls["center"]=nll
-        try:
+        if type(p.statsComputer.upperLimitComputer)==NNUpperLimitComputer:
             nll_p1 = p.statsComputer.upperLimitComputer.nll ( 1.,
                         pmSigma = 1 )
-            nlls["p1"] = float ( nll_p1 )
+            if nll_p1 == None:
+                print ( f"nll_p1 is None" )
+            if nll_p1 != None:
+                nlls["p1"] = float ( nll_p1 )
+        short_anaId = anaId.replace("-orig","")
+        if short_anaId in res:
+            res[short_anaId].update ( nlls )
+        else:
+            res[short_anaId]=nlls
+    if len(res)==0:
+        return
+    cleaned = {}
+    for anaId, nlls in res.items():
+        if "p1" in nlls and "orig" in nlls:
             sigma = nlls["p1"]-nlls["center"]
             delta = nlls["center"]-nlls["orig"]
             pull = delta / sigma
             nlls["pull"] = pull
-        except TypeError as e:
-            pass
-        if not "p1" in nlls:
-            continue ## missing sigma
-        if anaId in res:
-            res[anaId].update ( nlls )
-        else:
-            res[anaId]=nlls
-    print ( res )
+            cleaned[anaId]=nlls
+        
+    print ( f"[statsNLL] found {cleaned}: {res}" )
     from ptools.helpers import py_dumps
     import shutil
     if os.path.exists ( "stats" ):
         shutil.copyfile ( "stats", "stats.all" )
     key = s["key"]
     stats = readStats()
-    if len(res)>0:
-        stats[key]=res
+    if len(cleaned)>0:
+        stats[key]=cleaned
     ds = py_dumps ( stats ) + "\n"
-    d1 = py_dumps ( res ) + "\n"
+    d1 = py_dumps ( cleaned ) + "\n"
     with open ( f"results/{key}", "wt" ) as f:
         f.write ( d1 )
     with open ( "stats", "wt" ) as f:
@@ -134,7 +144,7 @@ def loop():
     print ( f"[statsNLL] Instantiate the database" )
     db = Database ( "../../smodels-database/" )
     print ( f"[statsNLL] Lets go" )
-    db.getExpResults()
+    db.getExpResults( dataTypes = "efficiencyMap" )
     while True:
         try:
             createOnePoint( db )
@@ -142,9 +152,16 @@ def loop():
             print ( f"[statsNLL] {e} -- ignoring" )
 
 def create():
+    for path in [ "results", "slhafiles" ]:
+        if not os.path.exists ( path ):
+            os.mkdir ( path )
+    nprocesses = 5
+    # nprocesses = 1
+    if nprocesses == 1:
+        loop()
     from multiprocessing import Process
     processes = []
-    for i in range(5):
+    for i in range(nprocesses):
         p = Process ( target = loop )
         p.start()
         processes.append ( p )
