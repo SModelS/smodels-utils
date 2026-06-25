ab526d87542544453357b3e18e7031e907dcbf60
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-06-25 14:21:08 +0200

    fudge

--- bin/listOfAnalyses.py
+++ bin/listOfAnalyses.py
@@ -77,12 +77,13 @@ class Lister:
         if os.path.exists ( poptions["dictfile"] ) and not self.keep:
             os.unlink ( poptions["dictfile"] )
         if self.fudged:
-            options["fudge"]=0.7
+            fudge = 0.7
+            options["fudge"]=fudge
             options["suffix"]="fudge"
             options["outfile"]="none"
             del plotter
             del modifier
-            print ( f"[listOfAnalyses] instantiating fudged ExpResModifier" )
+            print ( f"[listOfAnalyses] instantiating fudged (f={fudge:.1f}) ExpResModifier" )
             modifier = expResModifier.ExpResModifier ( options )
             print ( f"[listOfAnalyses] done instantiating fudged ExpResModifier" )
             from protomodels.plotting import plotDBDict
