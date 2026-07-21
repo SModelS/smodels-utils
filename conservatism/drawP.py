e6f6f7409f75be41f9e5cd58eb8eb8da10b0a9a4
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-07-21 14:24:21 +0200

    quenched warnings

--- conservatism/drawP.py
+++ conservatism/drawP.py
@@ -5,6 +5,7 @@ namely it draws distributions of p-values, but for various
 fudge factors """
 
 import os
+os.environ["QT_QPA_PLATFORM"]="xcb"
 os.environ.pop("XDG_SESSION_TYPE",None)
 import sys
 from matplotlib import pyplot as plt
