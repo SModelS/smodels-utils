823395989cd1dcc9e894a98510a6eaf3e7e7400d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-07-24 13:19:52 +0200

    full

--- stats_ml/slurm_yields.py
+++ stats_ml/slurm_yields.py
@@ -75,9 +75,11 @@ def submit ( mN2, mC1, mN1, options ):
     for m in [ "mN1", "mC1", "mN2" ]:
         options.pop(m,None)
     cmd = []
+    jobname = f"{options['txname']}_{mN2}_{mC1}_{mN1}"
+    if options["enable_full"]:
+        jobname = f"f{jobname}"
     if shutil.which ( "sbatch" ) != None:
         cmd = [ "sbatch", "-c", "6", "--time", "479" ]
-        jobname = f"{options['txname']}_{mN2}_{mC1}_{mN1}"
         cmd += [ "--job-name", jobname ]
         #cmd = [ "sbatch", "-c", "2", "--time", "479" ]
         cmd += [ "--error", f"./slurm_logs/%j.out",
